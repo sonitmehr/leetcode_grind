1f8c01af90c1e700ddf1e656d9e9289a71776457
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2021-12-09 04:27:58 +0000

    [LeetCode Sync] Runtime - 5 ms (14.22%), Memory - 10.2 MB (100.00%)

---
+++ my-folder/0349-intersection-of-two-arrays/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> m;// m = [1]        
+        vector<int> v;
+        for(int i = 0;i<nums1.size();i++){
+            m.insert(nums1[i]);
+        }for (int j = 0;j<nums2.size();j++){
+            if(m.find(nums2[j])!=m.end()){
+                v.push_back(nums2[j]);
+                m.erase(nums2[j]);
+            }
+        }return v;
+        
+        
+    }
+};
