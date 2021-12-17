616d7e0c12c4007234fe19dd81993e96074384b2
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2021-12-17 08:13:45 +0000

    [LeetCode Sync] Runtime - 27 ms (41.06%), Memory - 12.2 MB (99.99%)

--- my-folder/0001-two-sum/solution.cpp
+++ my-folder/0001-two-sum/solution.cpp
@@ -1,23 +1,20 @@
 #include<vector>
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        int diff;
-        std::vector<int> result;
+    
+    vector < int> twoSum(vector< int>& nums, int target) {
         unordered_map<int,int> m;
-        for (int i = 0;i<n;i++){
-            diff = target - nums[i]; // difference
-            // check if difference is same as current ele or not
-            // if not then proceed.
-            if(m.find(diff)  != m.end() && m.find(diff) -> second != i){
-                result.push_back(i);
-                result.push_back(m.find(diff) -> second);
-                return result;
-                
-            }m[nums[i]] = i; // if doesnt enter if condition, add value to map
-        }return result; 
+        vector<int> v;
+        for(int i = 0;i<nums.size();i++){
+            int diff = target - nums[i];
+            if(m.find(diff) == m.end()){// Did not find
+               m[nums[i]] = i; 
+            }else{
+                v.push_back(i);
+                v.push_back(m.find(diff)->second);
+            }
+        }return v;
     }
-            
+
         
 };
