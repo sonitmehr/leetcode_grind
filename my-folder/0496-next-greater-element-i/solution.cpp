4d6d95af7a5e9d55a8079eed97b77b063317653c
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2021-12-22 05:32:07 +0000

    [LeetCode Sync] Runtime - 4 ms (24.56%), Memory - 8.7 MB (100.00%)

---
+++ my-folder/0496-next-greater-element-i/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums2.size();
+        stack<int>st;
+        vector<int>ans;
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            mp[nums2[i]]=-1;
+        }
+        for(int i=0;i<n;i++){
+            while(!st.empty() && st.top()<nums2[i]){
+                mp[st.top()]=nums2[i];
+                st.pop();
+            }
+            st.push(nums2[i]);
+        }
+        for(int i=0;i<nums1.size();i++){
+            ans.push_back(mp[nums1[i]]);
+        }
+        return ans;
+    }
+};
