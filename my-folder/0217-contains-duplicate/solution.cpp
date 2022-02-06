fa1c12ad325a4a933912612145f47146bbfc2217
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-02-06 05:51:02 +0000

    [LeetCode Sync] Runtime - 195 ms (5.33%), Memory - 53.6 MB (100.00%)

--- my-folder/0217-contains-duplicate/solution.cpp
+++ my-folder/0217-contains-duplicate/solution.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
         unordered_map<int,int> m;
-        for(int i = 0;i<nums.size();i++){
+        
+        for (int i = 0;i<n;i++){
             m[nums[i]]++;
         }
-        for(int i = 0;i<m.size();i++){
-            if(m[nums[i]] > 1){
+        for (int i =0;i<m.size();i++){
+            if(m[nums[i]]>1){
                 return true;
-            } 
-        }return false;
+            }
+        }
+        return false;
     }
 };
