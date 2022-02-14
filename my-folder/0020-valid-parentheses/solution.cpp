9602de2d4b732a770da9f09d51f211a965f1b8b1
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-02-14 03:54:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.5 MB (100.00%)

--- my-folder/0020-valid-parentheses/solution.cpp
+++ my-folder/0020-valid-parentheses/solution.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        static unordered_map<char, char> m = {{'(', ')'}, {'{', '}'}, {'[', ']'}};
-        
-        for (char x : s) {
-            if (m.find(x) != m.end()) {
-              st.push(x);  
-            } else if (st.size() && m[st.top()] == x) {
+        unordered_map<char,char> m = {{'(',')'},{'{','}'},{'[',']'}};
+        for(char x : s){
+            if(m.find(x)!=m.end()){
+                st.push(x);
+            }else if(st.size() && m[st.top()]==x){
                 st.pop();
-            } else {
+            }else{
                 return false;
             }
+            
         }
-        
         return st.empty();
     }
 };
