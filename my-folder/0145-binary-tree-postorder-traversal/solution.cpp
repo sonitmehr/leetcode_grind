d7534fef7611d34a36aace112dc6988369cbf3fd
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-02-15 07:43:14 +0000

    [LeetCode Sync] Runtime - 7 ms (0.02%), Memory - 10.2 MB (100.00%)

--- my-folder/0145-binary-tree-postorder-traversal/solution.cpp
+++ my-folder/0145-binary-tree-postorder-traversal/solution.cpp
@@ -13,10 +13,8 @@ class Solution {
 public:
     vector<int> v;
     vector<int> postorderTraversal(TreeNode* root) {
-        if(root!=NULL){
-            
+        if(root !=NULL){
             postorderTraversal(root->left);
-            
             postorderTraversal(root->right);
             v.push_back(root->val);
         }
