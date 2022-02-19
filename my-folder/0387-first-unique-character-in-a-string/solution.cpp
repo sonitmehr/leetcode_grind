a59d89cfff6dfc2cde44f8fb76e0f79561929b37
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-02-19 05:06:45 +0000

    [LeetCode Sync] Runtime - 20 ms (25.88%), Memory - 10.5 MB (100.00%)

--- my-folder/0387-first-unique-character-in-a-string/solution.cpp
+++ my-folder/0387-first-unique-character-in-a-string/solution.cpp
@@ -6,9 +6,9 @@ public:
         for(int i=0;i<s.size();i++){
             arr[s[i] -'a']++;
         }
-        for(int i=0;i<s.size();i++){
-            if(arr[s[i] - 'a']==1)
-                return i;
+        cout << arr[0]; 
+        for(int i = 0;i<s.size();i++){
+            if(arr[s[i] - 'a']==1)return i;
         }
         return -1;
     }
