d560176b415938f41ab2b3449e647f4417bb56fd
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-02-25 14:46:34 +0000

    [LeetCode Sync] Runtime - 15 ms (41.42%), Memory - 12.2 MB (99.99%)

--- my-folder/0001-two-sum/solution.cpp
+++ my-folder/0001-two-sum/solution.cpp
@@ -3,16 +3,16 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> m;
         vector<int> v;
+        
         for(int i =0;i<nums.size();i++){
             int diff = target - nums[i];
-            if(m.find(diff)!=m.end()){
-                v.push_back(i);
+            if(m.find(diff)==m.end())m[nums[i]]=i;
+            else{
                 v.push_back(m.find(diff)->second);
-            }else{
-            m[nums[i]] = i;
+                v.push_back(i);
             }
-            
         }
         return v;
+            
     }
 };
