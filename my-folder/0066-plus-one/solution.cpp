7ca0e994bcbadb5b4d86656abc182fc9a9937a9b
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-02-25 15:08:35 +0000

    [LeetCode Sync] Runtime - 4 ms (0.62%), Memory - 8.5 MB (99.97%)

--- my-folder/0066-plus-one/solution.cpp
+++ my-folder/0066-plus-one/solution.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n = digits.size();
-        int ind = n-1; 
-        while(ind>=0){
-            digits[ind]++;
-            if(digits[ind] == 10){ // this will result in i = -1;
-                digits[ind]%=10;
-                ind--;
-                
-            }else{
+        int n= digits.size();
+        // [9] = [1,0]
+        // [7,9] = [7,0]
+        // [9,9,9,9,9,9,9,9,9] = [1,0,0]
+        // vectors.insert(v.begin(),1);
+        int i = n-1;
+        while(i>=0){
+            digits[i]++;
+            if(digits[i] == 10){
+                digits[i]=0;
+                i--;
+            }
+            else{
                 break;
             }
         }
-        if(ind == -1){
-            digits.insert(digits.begin(),1);
-        }
+        if(i == -1)digits.insert(digits.begin(),1);
         return digits;
+            
     }
-    
 };
