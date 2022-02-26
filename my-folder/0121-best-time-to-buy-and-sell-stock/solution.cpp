94843648b6e45e4bcf2f13b23cf47b211fee4982
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-02-26 07:39:36 +0000

    [LeetCode Sync] Runtime - 173 ms (6.56%), Memory - 93.8 MB (99.99%)

--- my-folder/0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ my-folder/0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        int cp[n+2];
-        cp[0] = prices[0]; // store initial cost of stock on Day 1. 
-        
-        int maxProfit = INT_MIN;
-        
+        int cost[n];
+        int maxi = INT_MIN;
+        cost[0] = prices[0];
         for(int i = 1;i<n;i++){
-            if(prices[i] - cp[i-1] > 0 ){ // IF profit is positive
-                cp[i] = cp[i-1];
-                maxProfit = max(maxProfit,prices[i] - cp[i-1]);
-            }else{
-                cp[i] = prices[i];
+            if(prices[i] - cost[i-1] > 0){
+                maxi = max(maxi,prices[i] - cost[i-1]);
+                cost[i] = cost[i-1];
             }
+            else{
+                cost[i] = prices[i]; 
+            }
+            
         }
-        if(maxProfit == INT_MIN){
-            return 0;
-        }
-        return maxProfit;
+        if(maxi == INT_MIN)return 0;
+        return maxi;
     }
 };
