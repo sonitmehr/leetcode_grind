7a8ac1c3a75013219eb05a6488647bd8fabbe0be
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-02-26 08:56:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.7 MB (100.00%)

--- my-folder/0119-pascals-triangle-ii/solution.cpp
+++ my-folder/0119-pascals-triangle-ii/solution.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<vector<int>> r;
-        r.push_back({1});
-        r.push_back({1,1});
-        for (int i = 2;i<rowIndex+1;i++){
-            vector<int> temp(r[r.size()-1].size()+1);
-            
-            temp[0] = 1;
-            temp[temp.size()-1] = 1;
-            
-                                       
-            for(int j = 1;j<temp.size()-1;j++){
-                temp[j] = r[i-1][j-1] + r[i-1][j];
-                
-                
-            }r.push_back(temp);
-        }return r[rowIndex];
+        vector<vector<int>> v;
+        int numRows = rowIndex;
+        v.push_back({1});
+        if(numRows ==0)return v[numRows];
+        v.push_back({1,1});
+        if(numRows == 1)return v[numRows];
         
+        for(int i = 2;i<numRows+1;i++){
+            vector<int> col(v[v.size()-1].size()+1);
+            col[0]=1;
+            col[col.size()-1]=1;
+            for(int j = 1;j<col.size()-1;j++){
+                col[j] = v[i-1][j-1]+v[i-1][j];
+            }
+            v.push_back(col);           
+                
+        }
+        return v[numRows];
+            
     }
 };
