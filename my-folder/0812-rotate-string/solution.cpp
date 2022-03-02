f6f8654f83e6759d9476c56265d67439a50bc226
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-03-02 10:47:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.1 MB (100.00%)

--- my-folder/0812-rotate-string/solution.cpp
+++ my-folder/0812-rotate-string/solution.cpp
@@ -1,16 +1,14 @@
-#include <iostream>
-#include <string>
 class Solution {
 public:
     bool rotateString(string s, string goal) {
         int n = s.size();
-        for(int i=0;i<n;i++){
-            char str1 = s[0];
+        string s1 =""; // abcde , n = 5
+        
+        for(int i = 1;i<n;i++){
+            char t = s[0];
             s.erase(0,1);
-            s.push_back(str1);
-            if(s==goal){
-                return true;
-            }
+            s.push_back(t);
+            if(s == goal)return true;
         }
         return false;
     }
