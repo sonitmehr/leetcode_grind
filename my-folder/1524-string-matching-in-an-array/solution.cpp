f8c103fcf45d1bef304431a540498f500163d455
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-03-02 11:02:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.8 MB (100.00%)

--- my-folder/1524-string-matching-in-an-array/solution.cpp
+++ my-folder/1524-string-matching-in-an-array/solution.cpp
@@ -4,26 +4,26 @@ public:
     static bool compare(string a,string b){
         if(a.size()>b.size()){
             return true;
+            
         }
         return false;
+            
     }
-    
+//      ["superhero","^","mass","as","ero"]  
     vector<string> stringMatching(vector<string>& words) {
+        int n = words.size();
         sort(words.begin(),words.end(),compare);
-        vector<string> v;
-        for (int i = 0;i<words.size()-1;i++){
-            if(words[i] != "#")
-            {
-                for(int j = i+1;j<words.size();j++)
-                    // Checking ahead of it.
-                {
+        vector<string>v;
+        for(int i = 0;i<n-1;i++){
+            if(words[i]!="^"){
+                for(int j =i+1;j<words.size();j++){
                     if(words[i].find(words[j])!=string::npos){
                         v.push_back(words[j]);
-                        words[j]= "#";
+                        words[j] = "^";
                     }
+
                 }
             }
-            
         }
         return v;
     }
