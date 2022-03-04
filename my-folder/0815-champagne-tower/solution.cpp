66959c082794157bb313b387ab25310f3fd48464
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-03-04 04:41:50 +0000

    [LeetCode Sync] Runtime - 44 ms (5.44%), Memory - 43.7 MB (32.63%)

---
+++ my-folder/0815-champagne-tower/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<vector<double>> v(102,vector<double>(102,0.0));
+        v[0][0] = poured;
+        for(int i = 0;i<query_row;i++){
+            for(int j = 0;j<=i;j++){
+                double flow = (v[i][j]-1)/2.0;
+                if(v[i][j] >=1){
+                    v[i+1][j]+=flow;
+                    v[i+1][j+1]+=flow;
+                    v[i][j] = 1;
+                }
+            }
+        }
+        return min(1.0,v[query_row][query_glass]);
+        
+    }
+};
