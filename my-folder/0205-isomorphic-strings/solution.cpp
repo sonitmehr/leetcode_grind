4bec4fa3e783570825be8ae97a73c092342316b3
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-03-07 09:51:21 +0000

    [LeetCode Sync] Runtime - 17 ms (7.58%), Memory - 6.9 MB (100.00%)

--- my-folder/0205-isomorphic-strings/solution.cpp
+++ my-folder/0205-isomorphic-strings/solution.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if(s.size()!=t.size())return false;
-        unordered_map<char,char> m;
         unordered_map<char,char> m1;
+        unordered_map<char,char> m2;
+        int s1 = s.size(),t1 = t.size();
+        if(s1!=t1)return false;
         
-        for(int i = 0;i<s.size();i++){
-            if(m.find(s[i]) !=m.end() && m[s[i]]!=t[i]){
-                return false;
-            }
-            if(m1.find(t[i]) !=m1.end() && m1[t[i]]!=s[i]){
-                return false;
-            }
-            m[s[i]] = t[i];
-            m1[t[i]] = s[i];
+        for(int i = 0;i<s1;i++){
+            if(m1.find(s[i])!=m1.end() && m1[s[i]]!=t[i])return false;
+            if(m2.find(t[i])!=m2.end() && m2[t[i]]!=s[i])return false;
+            
+            
+            m1[s[i]] = t[i];
+            m2[t[i]] = s[i];
+            
         }
         return true;
-        
     }
 };
