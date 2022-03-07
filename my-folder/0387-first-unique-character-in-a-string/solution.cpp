a058828b9ec5afd0ed2e18228d0d5eab3d560281
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-03-07 10:10:29 +0000

    [LeetCode Sync] Runtime - 16 ms (38.93%), Memory - 10.7 MB (100.00%)

--- my-folder/0387-first-unique-character-in-a-string/solution.cpp
+++ my-folder/0387-first-unique-character-in-a-string/solution.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-	//taking array size of 123 as ascii value of lower alphabets lies between 97 to 123--
-        int arr[26]={0};
-        for(int i=0;i<s.size();i++){
-            arr[s[i] -'a']++;
+        int a[26] = {0},n = s.size();
+        
+        for(int i=0;i<n;i++){
+            a[s[i]-'a']++;
         }
-        cout << arr[0]; 
-        for(int i = 0;i<s.size();i++){
-            if(arr[s[i] - 'a']==1)return i;
+        for(int i = 0;i<n;i++){
+            if(a[s[i]-'a']==1)return i;
         }
         return -1;
     }
