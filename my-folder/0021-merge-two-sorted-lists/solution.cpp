b9c09901fb6f28fcb3ceaa49fa65963434562b0a
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-03-07 04:50:38 +0000

    [LeetCode Sync] Runtime - 16 ms (0.97%), Memory - 14.8 MB (99.98%)

--- my-folder/0021-merge-two-sorted-lists/solution.cpp
+++ my-folder/0021-merge-two-sorted-lists/solution.cpp
@@ -11,27 +11,19 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        
-        ListNode* dummy = new ListNode(0);
+        ListNode * dummy = new ListNode(0);
         ListNode* temp = dummy;
-        
         while(list1!=NULL && list2!=NULL){
-            if(list1 -> val <= list2 -> val){
-                dummy -> next = list1;
-                list1= list1-> next;
-            }
-            else if(list1 -> val > list2 -> val){
-                dummy -> next = list2;
-                list2= list2-> next;
-            }
-            dummy = dummy ->  next;
-            
+            if(list1->val < list2->val){
+                dummy -> next = list1; 
+                list1 = list1->next;
+            }else {
+                dummy ->  next = list2;
+                list2 = list2 -> next;
+            }dummy = dummy -> next;
         }
-        if(list1!=NULL){
-            dummy->next = list1;
-        }else dummy -> next = list2;
-        
-        return temp -> next;
-        
+        if(list1!=NULL)dummy -> next = list1;
+        else dummy -> next = list2;
+        return temp-> next;
     }
 };
