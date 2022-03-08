086d027776da4281cdbc36e94b74b3ce6587cd01
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-03-08 06:01:42 +0000

    [LeetCode Sync] Runtime - 16 ms (9.21%), Memory - 8.1 MB (99.99%)

--- my-folder/0141-linked-list-cycle/solution.cpp
+++ my-folder/0141-linked-list-cycle/solution.cpp
@@ -9,20 +9,18 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head == NULL)return false;
-        if(head -> next == NULL){
-            return false;
-        }
-        if(head->next == head)return true;
+        if(head==NULL)return false;
         ListNode* slow = head;
-        ListNode* fast = head -> next;
+        ListNode* fast = head->next;
         
         while(slow!=fast){
-            if(fast == NULL || fast ->next == NULL)return false;
+            
+            if(fast==NULL || fast->next==NULL)return false;
             slow = slow->next;
-            fast = fast ->next->next;
+            fast = fast-> next->next;
+            
         }
         return true;
+        
     }
-    
 };
