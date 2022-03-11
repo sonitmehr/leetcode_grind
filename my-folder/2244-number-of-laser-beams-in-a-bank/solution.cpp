7df2aa56afa6d1e1443f8a914c3d0e0283e22637
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-03-11 10:24:54 +0000

    [LeetCode Sync] Runtime - 163 ms (5.15%), Memory - 23.1 MB (100.00%)

--- my-folder/2244-number-of-laser-beams-in-a-bank/solution.cpp
+++ my-folder/2244-number-of-laser-beams-in-a-bank/solution.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        int prevFreq = 0,devices=0,lasers = 0;
-        int rows = bank.size();
-        int col  = bank[0].size();
+        int n = bank.size();
+        int m = bank[0].size();
+        int count,prev=0,finalSum=0;
         
-        for(int i = 0;i<rows;i++){
-            devices=0;
-            for(int j = 0;j<col;j++){
-                if(bank[i][j] == '1') devices++;
-            }if(devices == 0) continue;
-            else{
-                lasers += prevFreq*devices;
-                prevFreq = devices;
+        for(int i = 0;i<n;i++){
+            count = 0;
+            for(int j = 0;j<m;j++){
+                if(bank[i][j]=='1'){
+                    count++;
+                }
             }
-            
+            finalSum += (prev*count);
+            if(count!=0)prev = count;
         }
-        return lasers;
+        return finalSum;
     }
 };
