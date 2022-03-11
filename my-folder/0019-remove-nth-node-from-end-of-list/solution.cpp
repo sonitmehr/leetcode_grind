b987ff0c0c6c5c0213a81480d1630cd2ce088e5d
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-03-11 10:12:05 +0000

    [LeetCode Sync] Runtime - 3 ms (1.67%), Memory - 10.8 MB (99.99%)

--- my-folder/0019-remove-nth-node-from-end-of-list/solution.cpp
+++ my-folder/0019-remove-nth-node-from-end-of-list/solution.cpp
@@ -11,23 +11,20 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* dummy = new ListNode() ; // Need to initialize.
-        dummy->next = head; // Dummy is one step behind head.
-        
+        ListNode * dummy = new ListNode();
+        dummy -> next = head;
         ListNode* fast = dummy;
-        ListNode* slow = dummy; 
+        ListNode* slow = dummy;
         
-        // [dummy(fast)(left),1,2,3,4,5]
-        
-        for(int i = 0;i<=n;i++){//  Runs 'n+1' times.
-            fast = fast -> next;
+        for(int i = 0; i<=n;i++){
+            fast = fast->next;
             
-        }// [dummy(left),1, 2,3(fast),4,5]  Fast reaches 1 step behind node to be deleted.
-        while(fast !=NULL){
-            slow = slow -> next;
-            fast = fast -> next;
         }
-        slow -> next = slow -> next -> next;
+        while(fast!=NULL){
+            fast = fast -> next;
+            slow = slow ->next;
+        }
+        slow ->next = slow -> next -> next;
         return dummy -> next;
     }
 };
