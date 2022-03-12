f066897865b3e24b6365b2873292c4e361cbcab9
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-03-12 15:09:10 +0000

    [LeetCode Sync] Runtime - 164 ms (5.82%), Memory - 40.5 MB (98.15%)

--- my-folder/2279-maximum-split-of-positive-even-integers/solution.cpp
+++ my-folder/2279-maximum-split-of-positive-even-integers/solution.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     vector<long long> maximumEvenSplit(long long finalSum) {
-        
-        // VERY GOOD LOGIC. TOPPER CODE.
-        
-        if (finalSum % 2) return {};
-        vector<long long> v;
-        for (long long i = 2; i <= finalSum; i += 2) { // final = 8
-                                                       // v = [2,4,6,16]
+        vector<long long>v;
+        if(finalSum %2==1)return v;
+        for(long long i = 2;i<=finalSum;i+=2){
             v.push_back(i);
-            finalSum -= i;
+            finalSum-=i;
         }
         v.back() += finalSum;
         return v;
