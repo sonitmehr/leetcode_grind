3a39755696664ac623304d8843d642d01a1b6721
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-03-12 14:25:20 +0000

    [LeetCode Sync] Runtime - 6 ms (50.11%), Memory - 7.2 MB (100.00%)

--- my-folder/0567-permutation-in-string/solution.cpp
+++ my-folder/0567-permutation-in-string/solution.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int n_size = s2.size(),m_size = s1.size();
-        if(m_size>n_size)return false;
-        vector<int> m(26,0);
-        vector<int> m1(26,0);
-        int l = 0,r=0;
+        int n1 = s1.size(),n2 = s2.size();
+        if(n1>n2)return false;
+        int l = 0,r=n1-1;
         
-        // Takes in size of all indexes in s1
-        for(int i = 0;i<m_size;i++){
-            m[s1[i] - 'a']++;
-            m1[s2[i]-'a']++;
+        vector<int> v1(26,0);
+        vector<int> v2(26,0);
+        
+        for(int i = 0;i<n1;i++){
+            v1[s1[i]-'a']++;
+            v2[s2[i]-'a']++;
         }
-        r = m_size-1;
-        while(r<n_size){
-            if(m1 == m)return true;
+        while(r<n2){
+            if(v1==v2)return true;
             
-            r+=1; // Go to next element.
+            r++;
+            if(r!=n2)v2[s2[r]-'a']++;
+            v2[s2[l]-'a']--;
             
-            if(r!=n_size){
-                m1[s2[r]-'a']++;
-            }
-            m1[s2[l]-'a']--;
             l++;
         }
-        
         return false;
+        
     }
 };
+
+/*
+s2 = "eidbaooo" m1 = 
+s1 = "ab"
+r = 2 
+l = 0;
+
+*/
