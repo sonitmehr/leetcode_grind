5f7861b4041eada872067c757899a8ec0d407e07
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-03-19 04:40:23 +0000

    [LeetCode Sync] Runtime - 7 ms (47.74%), Memory - 7.2 MB (100.00%)

--- my-folder/0567-permutation-in-string/solution.cpp
+++ my-folder/0567-permutation-in-string/solution.cpp
@@ -2,34 +2,22 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         int n1 = s1.size(),n2 = s2.size();
-        if(n1>n2)return false;
-        int l = 0,r=n1-1;
-        
-        vector<int> v1(26,0);
-        vector<int> v2(26,0);
+        if(n1 > n2)return false;
+        vector<int>a1(26,0);
+        vector<int>a2(26,0);
+        int l = 0,r= n1-1;
         
         for(int i = 0;i<n1;i++){
-            v1[s1[i]-'a']++;
-            v2[s2[i]-'a']++;
+            a1[s1[i]-'a']++;
+            a2[s2[i]-'a']++;
         }
-        while(r<n2){
-            if(v1==v2)return true;
+        for(int i = n1;i<=n2;i++){
+            if(a1==a2)return true;
             
-            r++;
-            if(r!=n2)v2[s2[r]-'a']++;
-            v2[s2[l]-'a']--;
-            
+            if(i!=n2)a2[s2[i]-'a']++;
+            a2[s2[l]-'a']--;
             l++;
         }
         return false;
-        
     }
 };
-
-/*
-s2 = "eidbaooo" m1 = 
-s1 = "ab"
-r = 2 
-l = 0;
-
-*/
