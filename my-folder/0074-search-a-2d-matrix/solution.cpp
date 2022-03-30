fa2bedb2e008ff3ede97329291648cfadd1c651c
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-03-30 15:11:12 +0000

    [LeetCode Sync] Runtime - 3 ms (1.33%), Memory - 9.6 MB (100.00%)

--- my-folder/0074-search-a-2d-matrix/solution.cpp
+++ my-folder/0074-search-a-2d-matrix/solution.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int rows = matrix.size();
-        int col = matrix[0].size();
-        int temp = -1;
-        /*if(rows*col == 1){
-            if(matrix[0][0] == target)return true;
-            return false;
-        }*/
-        for(int i = 0;i<rows;i++){
-            if(target > matrix[i][0] && target < matrix[i][col-1]) temp = i;
-            else if(matrix[i][0] == target || matrix[i][col-1] == target) return true;
-        }
-        if(temp!=-1){
-            for(int i = 0;i<col;i++){
-                if(matrix[temp][i] == target)return true;
+		// running a forloop till size of matrix
+        for(int i  = 0; i<matrix.size(); ++i)
+        {
+		// nested for for ietrating each row element
+            for(int j = 0; j<matrix[0].size(); ++j)
+            {
+				// if found return true
+                if(matrix[i][j] == target)
+                    return true;
             }
         }
+		// after traversal if not found
+		// return false
         return false;
     }
+	// for github repository link go to my profile.
 };
