6db63c76e74bee4546eea33b1e7a2692b55c9abd
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-04-01 02:42:09 +0000

    [LeetCode Sync] Runtime - 24 ms (2.84%), Memory - 23.3 MB (100.00%)

--- my-folder/0344-reverse-string/solution.cpp
+++ my-folder/0344-reverse-string/solution.cpp
@@ -1,16 +1,7 @@
 class Solution {
 public:
-    
-    
-    
-    
     void reverseString(vector<char>& s) {
-        int n = s.size();
-        int i =0,j=n-1;
-        while(i<j){
-            swap(s[i],s[j]);
-            i++;j--;
-        }
-        
+        reverse(s.begin(),s.end());
+
     }
 };
