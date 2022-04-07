5d7966ab575d101eaf8b930e9850a41064ead054
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-04-07 04:08:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.6 MB (100.00%)

---
+++ my-folder/1127-last-stone-weight/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int n = stones.size();
+        sort(stones.begin(),stones.end());
+        
+        for(int i = n-1;i>0;i--){
+            stones[i-1] = stones[i] - stones[i-1];
+            sort(stones.begin(),stones.end());
+        }
+        return stones[0];
+    }
+};
