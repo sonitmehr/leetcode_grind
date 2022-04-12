5c484e36751f4b0b9455739c731a94ea12f744b9
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-04-12 06:17:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.2 MB (100.00%)

--- my-folder/0886-score-of-parentheses/solution.cpp
+++ my-folder/0886-score-of-parentheses/solution.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int scoreOfParentheses(string s) {
-        // By observation I guess.
+        int n = s.size();
         stack<int> stack;
         int count = 0;
-        
         for(auto i : s){
-            if(i == '('){
+            if(i=='('){
                 stack.push(count);
                 count = 0;
-                
-            }else {
+            }else{
                 count = stack.top() + max(2*count,1);
                 stack.pop();
             }
+                
         }
         return count;
     }
