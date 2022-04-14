9c67d2da0afa655e2d0d3bd42c9890399007306e
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-04-14 06:07:50 +0000

    [LeetCode Sync] Runtime - 48 ms (0.07%), Memory - 34.7 MB (99.80%)

--- my-folder/0783-search-in-a-binary-search-tree/solution.cpp
+++ my-folder/0783-search-in-a-binary-search-tree/solution.cpp
@@ -11,25 +11,12 @@
  */
 class Solution {
 public:
-    struct TreeNode* searchBST(struct TreeNode* root, int val){
-    if (!root) {
-        return NULL;
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == NULL)return NULL;
+        if(root->val == val)return root;
+        TreeNode* search_l = searchBST(root->left,val);
+        TreeNode* search_r = searchBST(root->right,val);
+        if(search_l!=NULL)return search_l;
+        return search_r;
     }
-    if (root->val == val) {
-        return root;
-    }
-    
-    struct TreeNode* root_l = searchBST(root->left, val);
-    struct TreeNode* root_r = searchBST(root->right, val);
-    
-    if (root_l) {
-        return root_l;
-    }
-    
-    else if (root_r) {
-        return root_r;
-    }
-    
-    return NULL;
-}
 };
