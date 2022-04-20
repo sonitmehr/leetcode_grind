75d764e6aab3076ca67c1a10f750d686f556e297
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-04-20 05:07:44 +0000

    [LeetCode Sync] Runtime - 60 ms (5.28%), Memory - 24.2 MB (100.00%)

---
+++ my-folder/0173-binary-search-tree-iterator/solution.cpp
@@ -0,0 +1,32 @@
+class BSTIterator {
+public:
+    stack<TreeNode*> s;
+    BSTIterator(TreeNode* root) {
+        partialInorder(root);
+    }
+    
+    void partialInorder(TreeNode* root){
+        while(root != NULL){
+            s.push(root);
+            root = root->left;
+        }
+    }
+    
+    int next() {
+        TreeNode* top = s.top();
+        s.pop();
+        partialInorder(top->right);
+        return top->val;
+    }
+    
+    bool hasNext() {
+        return !s.empty();
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
