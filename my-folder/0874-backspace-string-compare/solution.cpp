d5b4688aad88c7a38f4e154dcaf3bcb28d448eb4
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-05-01 05:57:18 +0000

    [LeetCode Sync] Runtime - 7 ms (0.60%), Memory - 6.2 MB (100.00%)

---
+++ my-folder/0874-backspace-string-compare/solution.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    // WHEN I TRIED THE SAME LOGIC WITHOUT O(1) space, it didn't work.
+    bool backspaceCompare(string s, string t) {
+        int k=0,p=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='#')
+            {
+                k--;
+                 k=max(0,k);
+            }
+            
+           else
+           {
+               s[k]=s[i];
+               k++;
+           }
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            if(t[i]=='#')
+            {
+                p--;
+                 p=max(0,p);
+            }
+            
+           else
+           {
+               t[p]=t[i];
+               p++;
+           }
+        }
+        if(k!=p)
+            return false;
+        else
+        {
+            for(int i=0;i<k;i++)
+            {
+                if(s[i]!=t[i])
+                    return false;
+            }
+            return true;
+        }
+        
+    }
+};
