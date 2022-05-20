ff8ca81dc7dcda13c75bb13049a0219774af61cd
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-05-20 13:19:31 +0000

    [LeetCode Sync] Runtime - 3 ms (0.46%), Memory - 6.1 MB (100.00%)

--- my-folder/0070-climbing-stairs/solution.cpp
+++ my-folder/0070-climbing-stairs/solution.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
+    
+    
+    int f(int ind,vector<int> &dp){
+        if(ind == 0)return 1;
+        if(ind == 1)return 1;
+        if(dp[ind]!=-1)return dp[ind];
+        
+        int left = f(ind-1,dp);
+        int right = f(ind-2,dp);
+        return dp[ind] = left+right;
+    }
+    
     int climbStairs(int n) {
-        int dp[n+1];
-        dp[0]=1; // number of ways to climb the zeroth step will be one
-        dp[1]=1; // number of ways to climb the first step will be one
-        for(int i=2;i<=n;i++){
-           dp[i] = dp[i-1]+dp[i-2]; //for finding the number of ways to climb the second step we will add the number of ways to climb the zeroth and first step
-        }
-        return dp[n];// returning the number of ways to climb 5 steps will be the addition of number of ways to climb step 4 and 3
+        vector<int> dp(n+1,-1);
+        return f(n,dp);
     }
 };
