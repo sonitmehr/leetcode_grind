8bc94c9b36c30e21ff233717127bb9e5f1d8d9f4
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-05-20 15:04:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.3 MB (100.00%)

--- my-folder/0118-pascals-triangle/solution.cpp
+++ my-folder/0118-pascals-triangle/solution.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
+    #define pb push_back;
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> v;
-        v.push_back({1});
-        if(numRows == 1)return v;
-        v.push_back({1,1});
-        if(numRows == 2)return v;
         
-        for(int i = 2;i<numRows;i++){
-            vector<int> v1(i+1);
-            v1[0]=1;
-            v1[i]=1;
-            for(int j = 1;j<i;j++){
-                v1[j] = v[i-1][j-1]+ v[i-1][j];
-            }
-            v.push_back(v1);
+        
+        vector<vector<int>> ans;
+        
+        int n = numRows;
+        ans.push_back({1});
+        if(n==1){
+            return ans;
+        }ans.push_back({1,1});
+        if(n==2){
+            return ans;
         }
-        return v;
-        
+        for(int i =2;i<n;i++){
+            vector<int> v;
+            v.push_back(1);
+            for(int j=1;j<i;j++){
+                v.push_back(ans[i-1][j-1]+ans[i-1][j]);
+            }
+            v.push_back(1);
+            ans.push_back(v);
+        }
+        return ans;
     }
 };
