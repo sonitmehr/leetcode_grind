181621e6cbaa1bfabb31281ae682db82f38779aa
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-05-21 07:05:20 +0000

    [LeetCode Sync] Runtime - 121 ms (5.02%), Memory - 24.4 MB (24.78%)

---
+++ my-folder/1951-find-the-winner-of-the-circular-game/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        queue<int> q;
+        for(int i = 1;i<=n;i++){
+            q.push(i);
+        }
+        while(q.size()!=1){
+            for(int i = 0;i<k-1;i++){
+                int temp = q.front();
+                q.pop();
+                q.push(temp);
+            }
+            q.pop();
+        }
+        return q.front();
+    }
+};
