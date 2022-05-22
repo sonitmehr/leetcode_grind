127668bb88204cfd8bc7a8b376fcccb254636a28
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-05-22 05:49:38 +0000

    [LeetCode Sync] Runtime - 6 ms (33.26%), Memory - 10.8 MB (100.00%)

--- my-folder/0039-combination-sum/solution.cpp
+++ my-folder/0039-combination-sum/solution.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    void f(int i,int n,vector<int> &v,vector<vector<int>> &ds,int tar,vector<int> &a){
-        if(i == n){
-            if(tar == 0) ds.push_back(v);return;
-        }
-        if(a[i] <= tar){
-            v.push_back(a[i]);
-            f(i,n,v,ds,tar-a[i],a);
-            v.pop_back();
+    void f(vector<int> &ds,vector<int> &a,int ind,int n,int t,vector<vector<int>> &ans){
+        if(ind == n){
+            if(t == 0){
+                ans.push_back(ds);
+                
+            }
+            return;
             
         }
-        f(i+1,n,v,ds,tar,a);
+        if(a[ind] <= t){
+            ds.push_back(a[ind]);
+            f(ds,a,ind,n,t-a[ind],ans);
+            ds.pop_back();
+            
+        }f(ds,a,ind+1,n,t,ans);
+        
     }
     
     
-    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ds;
-        vector<int> v;
-        f(0,candidates.size(),v,ds,target,candidates);
-        return ds;
+        vector<vector<int>> ans;
+        vector<int> ds;
+        int n = candidates.size();
+        f(ds,candidates,0,n,target,ans);
+        return ans;
     }
 };
