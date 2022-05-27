402e3d800c502bdf0947e71c7e387fb81471c2af
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-05-27 14:19:07 +0000

    [LeetCode Sync] Runtime - 30 ms (9.01%), Memory - 10.7 MB (100.00%)

--- my-folder/0387-first-unique-character-in-a-string/solution.cpp
+++ my-folder/0387-first-unique-character-in-a-string/solution.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        int a[26] = {0},n = s.size();
+        int n = s.size();
         
-        for(int i=0;i<n;i++){
-            a[s[i]-'a']++;
-        }
+        int a[26] = {0};
+        if(n==1)return 0;
         for(int i = 0;i<n;i++){
-            if(a[s[i]-'a']==1)return i;
+            if(a[s[i]-'a'] > 0){
+                a[s[i]-'a']=-1;
+            }
+            if(a[s[i]-'a']!=-1)a[s[i]-'a']++;
+        }
+        for(int i =0;i<n;i++){
+            if(a[s[i]-'a'] == 1)return i;
         }
         return -1;
     }
