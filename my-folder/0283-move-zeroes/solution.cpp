c07fe3b49090fcdbfa52f1bfc71c3852abf30480
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-05-27 14:26:18 +0000

    [LeetCode Sync] Runtime - 30 ms (8.14%), Memory - 19.3 MB (100.00%)

--- my-folder/0283-move-zeroes/solution.cpp
+++ my-folder/0283-move-zeroes/solution.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        int lastOccurenceOfNonZero = 0;
-        int last = lastOccurenceOfNonZero;
-        
+        if(n==1)return;
+        int lastNonZero = 0;
+        int lastZero = 0;
         for(int i = 0;i<n;i++){
-            if(nums[i] != 0) {
-                nums[last] = nums[i];
-                last++;
+            if(nums[i] != 0){
+                nums[lastZero] = nums[i];
+                lastZero++;
             }
-             
-        }
-        for(int i = last;i<n;i++)nums[i] =0;
-        
+            
+        }for(int i = lastZero;i<n;i++){
+            nums[i]=0;
+        }return;
     }
 };
