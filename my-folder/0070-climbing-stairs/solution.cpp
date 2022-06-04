1fccd35eb1c51f5f6ac0cab8f2f5c8da6db97eb9
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-06-04 18:37:29 +0000

    [LeetCode Sync] Runtime - 2 ms (5.23%), Memory - 6.2 MB (100.00%)

--- my-folder/0070-climbing-stairs/solution.cpp
+++ my-folder/0070-climbing-stairs/solution.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-    
-    
     int f(int ind,vector<int> &dp){
         if(ind == 0)return 1;
-        if(ind == 1)return 1;
+        else if(ind == 1)return 1;
         if(dp[ind]!=-1)return dp[ind];
         
-        int left = f(ind-1,dp);
-        int right = f(ind-2,dp);
-        return dp[ind] = left+right;
+        int l = f(ind-2,dp);
+        int r = f(ind-1,dp);
+        return dp[ind] = r+l;
+        
     }
-    
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-        return f(n,dp);
+        int prev2 = 1,prev1 =1;
+        //int curr = 2;
+        for(int i = 2;i<=n;i++){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 };
