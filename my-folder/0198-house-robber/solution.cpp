89e787564ae5f1ea2285de2b9fb004abd0bbf420
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-06-05 07:20:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.8 MB (100.00%)

--- my-folder/0198-house-robber/solution.cpp
+++ my-folder/0198-house-robber/solution.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
+    
+    int f(int ind,vector<int> &dp,vector<int> &a){
+        if(ind == 0)return a[0];
+        else if(ind < 0)return 0;
+        if(dp[ind]!=-1)return dp[ind];
+        
+        // To maximise maxi, we will always go to ind -2 after picking index;
+        int pick = a[ind] + f(ind-2,dp,a);
+        // When you don't pick, go to previous index.
+        int notPick = 0 + f(ind-1,dp,a);
+        return dp[ind] = max(pick,notPick);
+        
+        
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        //we should return nums[0], if there only one house 
-
-        if(n==1) return nums[0];  
-        //1. Initialize an array dp sized according to state variable
-        int *dp = new int[n];
-        //2. Base cases to avoid index out of bounds
+        if(n == 1)return nums[0];
+        vector<int> dp(n+1,-1);
         dp[0] = nums[0];
         dp[1] = max(nums[0], nums[1]);
-
-        for(int i = 2; i<n; i++) {
-        //3. Recurrence Relation
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+        for(int i = 2;i<n;i++){
+            
+            int l = nums[i] + dp[i-2];
+            int r =  dp[i-1];
+            dp[i]= max(l,r);
+             
+            
         }
         return dp[n-1];
     }
