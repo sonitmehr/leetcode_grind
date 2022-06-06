ec4def6de7c1dcd3c864a2b4bb63d639dd327ad5
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-06-06 13:28:55 +0000

    [LeetCode Sync] Runtime - 341 ms (5.23%), Memory - 83 MB (100.00%)

--- my-folder/2387-partition-array-such-that-maximum-difference-is-k/solution.cpp
+++ my-folder/2387-partition-array-such-that-maximum-difference-is-k/solution.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int partitionArray(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());
+        
         int n = nums.size();
-        int cnt =1,prev = nums[0];
-        for(int i= 0;i<n;i++){
-            if((nums[i] - prev) > k){
+        int prev = nums[0],cnt=1;
+        for(int i = 0;i<n;i++){
+            if(nums[i] - prev > k){
                 cnt++;
                 prev = nums[i];
             }
-            
         }
         return cnt;
     }
