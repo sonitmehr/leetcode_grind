61130f08aaf34d57af0a5df7cab604ce9bc3b4a4
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-06-07 10:28:50 +0000

    [LeetCode Sync] Runtime - 3 ms (3.09%), Memory - 8.9 MB (99.99%)

--- my-folder/0088-merge-sorted-array/solution.cpp
+++ my-folder/0088-merge-sorted-array/solution.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int size1 = nums1.size(),size2 = nums2.size();
-        
-        for(int i = m;i<size1;i++){
-            nums1[i] = nums2[size2 - n];
-            n--;
+        int i = m-1,j =n-1,ptr =n+m-1;
+        if(m==0) nums1 = nums2;
+        while(i>=0 && j>=0){
+            if(nums2[j] >= nums1[i]){
+                nums1[ptr] = nums2[j];
+                j--;
+                ptr--;
+                
+            }
+            else if(nums2[j] < nums1[i]){
+                nums1[ptr] = nums1[i];
+                i--;
+                ptr--;
+                
+            }
             
-        }sort(nums1.begin(),nums1.end());
+        }
+        while(j >=0){
+            nums1[ptr] = nums2[j];
+            j--;
+            ptr--;
+        }
         
+            
     }
 };
