e4e14690f607850470a1ac35122697f7b7e1d6c0
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-06-13 09:37:46 +0000

    [LeetCode Sync] Runtime - 11 ms (4.35%), Memory - 8.9 MB (100.00%)

--- my-folder/0120-triangle/solution.cpp
+++ my-folder/0120-triangle/solution.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
+    
+    int f(int ind,vector<vector<int>> &dp,vector<vector<int>> &a,int row,int n){
+        if(ind == n-1)return a[ind][row];
+        if(dp[ind][row]!=-1)return dp[ind][row];
+        
+        int l = a[ind][row] + f(ind +1,dp,a,row,n);
+        int r = a[ind][row] + f(ind+1,dp,a,row+1,n);
+        return dp[ind][row] = min(l,r);
+    }
+    
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        for(int i=n-2;i>=0;i--){
-            for(int j=0;j<=i;j++){
-                triangle[i][j]+=min(triangle[i+1][j],triangle[i+1][j+1]);
-            }
-        }
-        return triangle[0][0];
+        int n =triangle.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        
+        return f(0,dp,triangle,0,triangle.size());
     }
 };
