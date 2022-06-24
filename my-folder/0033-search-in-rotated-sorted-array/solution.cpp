0f8d45ac7907cfcb9c61db664c37ab81020669cd
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-06-24 12:30:46 +0000

    [LeetCode Sync] Runtime - 9 ms (0.48%), Memory - 11.1 MB (100.00%)

--- my-folder/0033-search-in-rotated-sorted-array/solution.cpp
+++ my-folder/0033-search-in-rotated-sorted-array/solution.cpp
@@ -1,27 +1,41 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int i = 0,j=n-1;
-        while(i<=j){
-            int mid = i + (j-i)/2;
+    int search(vector<int>& v, int target) {
+        int n = v.size();
+        
+        int i = 0;
+        int j = n-1;
+        
+        while(i <= j){
             
-            int curr = nums[mid];
-            if(curr == target)return mid;
-            else if(curr >= nums[i]){
-                if(curr >= target && target >= nums[i]){
-                    j = mid-1;
+            int mid = i + (j - i)/2;
+            int ele = v[mid];
+            if( ele == target)return mid;
+            
+            // Check left.
+            // If mid >= leftmost, then mid is in left part of rotated array.
+            
+            else if(ele >= v[i]){ // v[i] is left most element. 
+                
+                // Is the leftmost element in left part >= target element?
+                if(target >= v[i] && ele >= target){
+                    j = mid -1;
                 }
-                else i = mid+1;
+                else i = mid + 1;
+                
             }
-            else{
-                if(target >= curr && nums[j] >=target){
-                    i = mid+1;
+            
+            // This is when mid is in right part of rotated array.
+            else { // v[j] is the right most element
+                if(target <= v[j] && ele <= target){
+                    i = mid +1;
                 }
-                else j = mid-1;
+                else j = mid - 1;
+                    
             }
             
         }
         return -1;
+        
     }
 };
