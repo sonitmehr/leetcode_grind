b279a0badc41ad941273c2730ad648cddb087200
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-06-25 16:47:51 +0000

    [LeetCode Sync] Runtime - 13 ms (5.53%), Memory - 9.6 MB (100.00%)

---
+++ my-folder/0137-single-number-ii/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int one = 0,two = 0;
+        
+        for (auto i : nums){
+            one = (one ^ i) & (~ two);
+            two = (two ^ i) & (~ one);
+        }
+        return one;
+    }
+};
