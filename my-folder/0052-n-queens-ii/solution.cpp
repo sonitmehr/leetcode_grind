a93d2fd6ab1a0587dd32e409828d9eaa10ab3b1d
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-06-28 10:55:43 +0000

    [LeetCode Sync] Runtime - 3 ms (48.75%), Memory - 6.3 MB (100.00%)

--- my-folder/0052-n-queens-ii/solution.cpp
+++ my-folder/0052-n-queens-ii/solution.cpp
@@ -1,28 +1,53 @@
 class Solution {
 public:
-    int totalNQueens(int n) {
-	vector<vector<bool>> board(n, vector<bool>(n, false));
-	return solve(board, 0);
-    }
-    bool check(vector<vector<bool>>& board, int row, int col) {
-        int n = size(board);
-        for(int i = 0; i <= row; i++) {
-            if(board[i][col]) return false; // checking if any queen already placed on same column previously
-            // checking if all diagonals are safe -
-            if(row - i >= 0 && col - i >= 0 && board[row - i][col - i]) return false;
-            if(row - i >= 0 && col + i <  n && board[row - i][col + i]) return false;
+    
+    int f(int col,vector<string> &a,vector<vector<string>> &ds, vector<int> &left,vector<int> &upperDiagnol,vector<int> &lowerDiagnol,int n){
+        if(col == n){
+            return 1;
         }
-        return true;
-    }    
-    int solve(vector<vector<bool>>& board, int row) {
-        if(row == size(board)) return 1;
-        int count = 0;
-        for(int col = 0; col < size(board); col++)           
-            if(check(board, row, col)){          // check if we can place at (row, col)
-                board[row][col] = true;          // place the queen at (row, col)
-                count += solve(board, row + 1);  // explore for the next row. The function will return 1 if all N queens get placed for current combination
-                board[row][col] = false;         // backtrack - remove previously placed queen and try for different columns
-            }                                
-        return count;
+        // Iterating theough each row.
+        int cnt = 0;
+        for(int i =0;i<n;i++){
+            // Checking Hash Table.
+            if(left[i] == 0 && lowerDiagnol[i + col] == 0 && upperDiagnol[n - 1 + col - i] == 0){
+                a[i][col] = 'Q';
+                
+                left[i] = 1;        // Checking if left side is not attacked by any other queen.
+                                    // If left[i] == 0, means no queen has entered current row.
+                lowerDiagnol[i + col] = 1;
+                upperDiagnol[n-1 + col - i] = 1;
+                
+                // Call for next column recursively.
+                cnt += f(col + 1,a,ds,left,upperDiagnol,lowerDiagnol,n);
+                
+                a[i][col] = '.';  // Back track to previous condition.
+                left[i] = 0;
+                lowerDiagnol[i + col] = 0;
+                upperDiagnol[n-1 + col -i] = 0;
+                
+                
+                
+            }
+            
+        }
+        return cnt;
+        
+        
+    }
+    
+    int totalNQueens(int n) {
+        vector<vector<string>> ds;
+        vector<string> board(n);
+        string s(n,'.');
+        
+        for(int i = 0;i<n;i++){
+            board[i] = s;
+        }
+        
+        vector<int> left(n + 1,0), upperDiagnol(2*n - 1,0),lowerDiagnol(2*n-1,0);
+        
+        int l = f(0,board,ds,left,upperDiagnol,lowerDiagnol,n);
+        
+        return l;
     }
 };
