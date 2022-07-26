bbd13c26c2322a53e05a7d1c7d889879e65cab18
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-07-26 11:44:04 +0000

    [LeetCode Sync] Runtime - 3 ms (1.22%), Memory - 6 MB (100.00%)

--- my-folder/0886-score-of-parentheses/solution.cpp
+++ my-folder/0886-score-of-parentheses/solution.cpp
@@ -4,10 +4,13 @@ public:
         int n = s.size();
         stack<int> stack;
         int count = 0;
+        //stack.push(0);
         for(auto i : s){
             if(i=='('){
                 stack.push(count);
                 count = 0;
+
+                cout << "stack.top() " << stack.top() << endl;
             }else{
                 count = stack.top() + max(2*count,1);
                 stack.pop();
