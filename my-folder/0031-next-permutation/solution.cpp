a10e67cb3e42e6ea44e51872328b2c6c8e5f7258
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-07-26 10:08:32 +0000

    [LeetCode Sync] Runtime - 9 ms (1.55%), Memory - 12.2 MB (100.00%)

--- my-folder/0031-next-permutation/solution.cpp
+++ my-folder/0031-next-permutation/solution.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    // find next permutation
-    void nextPermutation(vector<int> &nums)
-    {
-        // initialize variable:
-        int breakPoint = -1;
-
-        // find a breakpoint:
-        for (int i = nums.size() - 1; i > 0; i--)
-        {
-            if (nums[i] > nums[i - 1])
-            {
-                breakPoint = i - 1;
-                break;
+    int findEle(vector<int> &v,int ele,int end){
+        int n = v.size();
+        
+        for(int i= n-1;i>end;i--){
+            if(v[i] > ele)return i;
+        }
+        return -1;
+        
+    }
+    void nextPermutation(vector<int>& v) {
+        int n = v.size();
+        
+        for(int i= n-2;i>=0;i--){
+            if(v[i+1] > v[i] ){
+                //auto upperInd = upper_bound(v.begin(), v.end(),v[i]);
+                //auto it = std::find (v.begin(), v.end(), upperInd ));
+                int val = findEle(v,v[i],i);
+                cout << val;
+                swap(v[val],v[i]);
+                reverse(v.begin() + i + 1,v.end());
+                return;
+                
+                
+                
             }
         }
-
-        // if no breakpoint
-        if (breakPoint < 0)
-        {
-            reverse(nums.begin(), nums.end());
-            return;
-        }
-
-        // if found a breakpoint
-        for (int i = nums.size() - 1; i >= 0; i--)
-        {
-            if (nums[i] > nums[breakPoint])
-            {
-                swap(nums[breakPoint], nums[i]);
-                reverse(nums.begin() + breakPoint + 1, nums.end());
-                break;
-            }
-        }
+        reverse(v.begin(),v.end());
+        return;
+        
     }
 };
