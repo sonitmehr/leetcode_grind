586e5332f542bb608a7347b464b22ce887958cfb
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-07-27 08:17:19 +0000

    [LeetCode Sync] Runtime - 98 ms (17.61%), Memory - 48.4 MB (99.99%)

--- my-folder/0055-jump-game/solution.cpp
+++ my-folder/0055-jump-game/solution.cpp
@@ -3,16 +3,17 @@ public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
         
-        int goal = n - 1;
+        int goal = n - 1 ;
         
         for(int i = n-1;i>=0;i--){
             // when goal is reachable from some i;
             if(i + nums[i] >= goal){
                 // Change goal closer to i = 0
+                
                 goal = min(goal,i);
             }
         }
-        if(goal == 1-1)return true;
+        if(goal == 0)return true;
         return false;
     }
 };
