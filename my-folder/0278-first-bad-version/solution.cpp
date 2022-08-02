9a4466dfc839c503005359ad7153836536dcaa3e
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-08-02 17:40:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 5.9 MB (100.00%)

--- my-folder/0278-first-bad-version/solution.cpp
+++ my-folder/0278-first-bad-version/solution.cpp
@@ -4,21 +4,15 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int l =0,r = n;
-        
-        while(l<r){
-            int mid = l + (r-l)/2;
-            if(isBadVersion(mid)==true)r = mid;// not mid-1
-            else 
-            {
-               l=mid+1;
-            }
+        int l = 1, r  = n,mid;
+        if(n == 1)return 1;
+        while(l < r){
+            mid = l + (r-l)/2;
             
+            if(isBadVersion(mid))r = mid;
+            else l = mid + 1;
         }
-        if(l==r && isBadVersion(l)){// Returning left. 
-            return l;//                Not mid. 
-        }
+        if(r == l && isBadVersion(l))return l;
         return -1;
-        
     }
 };
