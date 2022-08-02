50e94f7ab9b600f082201a5e8926e7132b61f1b0
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-08-02 16:09:51 +0000

    [LeetCode Sync] Runtime - 33 ms (5.27%), Memory - 13.5 MB (100.00%)

--- my-folder/0801-is-graph-bipartite/solution.cpp
+++ my-folder/0801-is-graph-bipartite/solution.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-    vector<int>vis,col;
-    bool dfs(int v, int c, vector<vector<int>>& graph){
-        vis[v]=1;
-        col[v]=c;
-        for(int child:graph[v]){
-            if(vis[child]==0){
-                // here c^1 is for flipping 1 by 0 or 0 by 1, that is flip the current color
-                if(dfs(child,c^1,graph)==false) 
-                    return false;
+    
+    bool dfs(int node, vector<vector<int>>& adj,vector<int> &vis,vector<int> &col,int c){
+        vis[node] = 1;
+        col[node] = c;
+        for(auto i : adj[node]){
+            if(vis[i] == 0){
+                
+                
+                if(dfs(i,adj,vis,col,c^1) == false)return false;
             }
-            else{
-                if(col[v]==col[child])
-                    return false;
-            }
+            else if (col[node] == col[i])return false;
         }
         return true;
     }
     
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vis.resize(n);
-        col.resize(n);
-
-        for(int i=0;i<n;++i){
-            if(vis[i]==0 && dfs(i,0,graph)==false){ 
-                return false;
+    
+    
+    bool isBipartite(vector<vector<int>>& adj) {
+        int n = adj.size(); 
+        
+        vector<int> vis(n+1,0);
+        
+        vector<int> col(n+1,-1);
+        
+        for(int i = 0;i<n;i++){
+            if(vis[i] == 0){
+                if(dfs(i,adj,vis,col,0) == false)return false;
             }
         }
         
         return true;
+        
     }
 };
