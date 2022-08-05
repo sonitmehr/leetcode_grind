5b39bec1b33de6d9f0e49ac637b063c735237473
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-08-05 14:16:07 +0000

    [LeetCode Sync] Runtime - 40 ms (5.27%), Memory - 13.5 MB (100.00%)

--- my-folder/0801-is-graph-bipartite/solution.cpp
+++ my-folder/0801-is-graph-bipartite/solution.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
     
-    bool dfs(int node, vector<vector<int>>& adj,vector<int> &vis,vector<int> &col,int c){
-        vis[node] = 1;
-        col[node] = c;
-        for(auto i : adj[node]){
-            if(vis[i] == 0){
-                
-                
-                if(dfs(i,adj,vis,col,c^1) == false)return false;
-            }
-            else if (col[node] == col[i])return false;
+    bool dfs(int i,vector<vector<int>> &adj,vector<int> &vis,vector<int> &col,int c){
+        vis[i] = 1;
+        col[i] = c;
+        
+        for(auto ii : adj[i]){
+            if(vis[ii] == 0){
+                if(dfs(ii,adj,vis,col,c^1) == false)return false;
+            }else if(col[i] == col[ii])return false;
+            
         }
         return true;
     }
     
     
-    
-    bool isBipartite(vector<vector<int>>& adj) {
-        int n = adj.size(); 
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> vis(n + 1,0);
+        vector<int> col(n + 1,-1);
+        int c = 0;
         
-        vector<int> vis(n+1,0);
         
-        vector<int> col(n+1,-1);
-        
-        for(int i = 0;i<n;i++){
-            if(vis[i] == 0){
-                if(dfs(i,adj,vis,col,0) == false)return false;
+        for(int i = 0;i <n;i++){
+            if(vis[i]==0){
+                if(dfs(i,graph,vis,col,c) == false)return false;
             }
         }
-        
         return true;
-        
     }
 };
