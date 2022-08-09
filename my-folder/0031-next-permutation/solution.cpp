dad3e3c031361334ec52901f8039674710e5dc1d
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-08-09 05:30:07 +0000

    [LeetCode Sync] Runtime - 21 ms (1.55%), Memory - 12 MB (100.00%)

--- my-folder/0031-next-permutation/solution.cpp
+++ my-folder/0031-next-permutation/solution.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    int findEle(vector<int> &v,int ele,int end){
-        int n = v.size();
-        
-        for(int i= n-1;i>end;i--){
-            if(v[i] > ele)return i;
-        }
-        return -1;
-        
-    }
     void nextPermutation(vector<int>& v) {
         int n = v.size();
-        
-        for(int i= n-2;i>=0;i--){
-            if(v[i+1] > v[i] ){
-                //auto upperInd = upper_bound(v.begin(), v.end(),v[i]);
-                //auto it = std::find (v.begin(), v.end(), upperInd ));
-                int val = findEle(v,v[i],i);
-                cout << val;
-                swap(v[val],v[i]);
-                reverse(v.begin() + i + 1,v.end());
-                return;
-                
-                
-                
+        int i;
+        if(n == 1)return ;
+        for(i = n -2;i>=0;i--){
+            if(v[i] < v[i +1])break;
+        }
+        if(i < 0){
+            reverse(v.begin(),v.end());
+        }
+        else{
+            int j;
+            for(j = n - 1;j>=i+1;j--){
+                if(v[j] > v[i]){
+                    swap(v[i],v[j]);
+                    reverse(v.begin() + i + 1,v.end());
+                    return;
+                    
+                }
             }
         }
-        reverse(v.begin(),v.end());
-        return;
+        
+        
         
     }
 };
