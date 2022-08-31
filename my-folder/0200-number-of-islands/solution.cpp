da5e18beba8cf7f4afedde78abf0e51f27f4ad39
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-08-31 06:10:50 +0000

    [LeetCode Sync] Runtime - 75 ms (7.61%), Memory - 19.1 MB (41.93%)

--- my-folder/0200-number-of-islands/solution.cpp
+++ my-folder/0200-number-of-islands/solution.cpp
@@ -1,39 +1,46 @@
 class Solution {
 public:
-    void DFS(vector<vector<char>>& grid, int i, int j) {
-        // boundary checking
-        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size())
-            return;
-        // return if current position is of water or is already visited
-        if(grid[i][j] == '2' || grid[i][j] == '0')
-            return;
-        
-        // mark the current as visited
-        grid[i][j] = '2';
-        
-        // do DFS in all 4 directions
-        DFS(grid, i+1, j);
-        DFS(grid, i, j-1);
-        DFS(grid, i-1, j);
-        DFS(grid, i, j+1);
-    }
-    
-    int numIslands(vector<vector<char>>& grid) {
-        // We can treat the matrix grid as a grid. Each Island is a
-        // connected component. The task is to find no. of disconnectedd components
-        // in the graph.
-        
-        int islands = 0;
-        // We make each 1 as 2 in when it is visited
-        for(int i = 0; i < grid.size(); i++) {
-            for(int j = 0; j < grid[0].size(); j++) {
-                // do DFS in case has not been visited and there is land
-                if(grid[i][j] == '1') {
-                    DFS(grid, i, j);
-                    ++islands;
-                } 
+    int numIslands(vector<vector<char>>& v) {
+        int n = v.size();
+        int m = v[0].size();
+        vector<vector<int>> vis(n+1,vector<int> (m+1,0));
+        int cnt = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(vis[i][j] == 0 && v[i][j] == '1'){
+                    cnt++;
+                    queue<pair<int,int>> q;
+                    vis[i][j] = 1;
+                    q.push({i,j});
+                    while(!q.empty()){
+                        pair<int,int> node = q.front(); 
+                        q.pop();
+                        int I = node.first;
+                        int J = node.second;
+                        if(I < n && I >= 0 && J < m && J >= 0){
+                            if(I -1 >=0 && vis[I -1][J] == 0 && v[I -1][J] == '1'){
+                                q.push({I-1,J});
+                                vis[I -1][J] =1;
+                            }
+                            if(I+1 < n && vis[I + 1][J] == 0 && v[I + 1][J] == '1'){
+                                q.push({I +1,J});
+                                vis[I +1][J] =1;
+                            }
+                            if(J-1 >=0 && vis[I][J - 1] == 0 && v[I][J - 1] == '1'){
+                                q.push({I,J -1});
+                                vis[I][J -1] =1;
+                            }if(J+1 <m && vis[I][J + 1] == 0 && v[I][J + 1] == '1'){
+                                q.push({I,J+1});
+                                vis[I][J +1] =1;
+                            }
+                        }                       
+                        
+                    }
+                
+                    
+                }
             }
         }
-        return islands;
+        return cnt;
     }
 };
