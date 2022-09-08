90937721637e792f4b6cc27cdf60d5827130a100
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-09-08 12:35:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.7 MB (100.00%)

--- my-folder/0094-binary-tree-inorder-traversal/solution.cpp
+++ my-folder/0094-binary-tree-inorder-traversal/solution.cpp
@@ -13,12 +13,12 @@ class Solution {
 public:
     vector<int> v;
     vector<int> inorderTraversal(TreeNode* root) {
-        if(root != NULL){
-            inorderTraversal(root -> left);
-            v.push_back(root->val);
-            inorderTraversal(root -> right);
-            
-        }
+        if(root == NULL)return {};
+        
+        inorderTraversal(root -> left);
+        v.push_back(root -> val);
+        inorderTraversal(root -> right);
+        
         return v;
     }
 };
