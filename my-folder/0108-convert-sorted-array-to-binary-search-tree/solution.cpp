7fc72b65bce0130c286b66bf38e92f9514268db9
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-10-14 11:07:45 +0000

    [LeetCode Sync] Runtime - 36 ms (7.77%), Memory - 21.4 MB (99.99%)

--- my-folder/0108-convert-sorted-array-to-binary-search-tree/solution.cpp
+++ my-folder/0108-convert-sorted-array-to-binary-search-tree/solution.cpp
@@ -11,19 +11,22 @@
  */
 class Solution {
 public:
-    TreeNode * f(vector<int> & v,int l,int r){
+    vector<int> v;
+    TreeNode * f(int l,int r){
         if(l > r)return NULL;
         int mid = (l+r)/2;
         TreeNode * root = new TreeNode(v[mid]);
         
-        root -> left = f(v,l,mid - 1);
-        root -> right = f(v,mid + 1,r);
-        
+        root -> left = f(l,mid-1);
+        root -> right = f(mid+1,r);
         return root;
-        
     }
-    TreeNode* sortedArrayToBST(vector<int>& nums) {
-        
-        return f(nums,0,nums.size()-1);
+    
+    TreeNode* sortedArrayToBST(vector<int>& v) {
+        int n = v.size();
+        this->v =v;
+        int l = 0,r = n-1;
+        TreeNode* root = NULL;
+        return f(l,r);
     }
 };
