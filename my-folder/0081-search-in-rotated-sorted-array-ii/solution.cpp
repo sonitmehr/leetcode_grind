9e1345ee0cadbbce17577940ecd1287a21aa662d
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-10-19 17:39:56 +0000

    [LeetCode Sync] Runtime - 8 ms (0.96%), Memory - 13.9 MB (100.00%)

--- my-folder/0081-search-in-rotated-sorted-array-ii/solution.cpp
+++ my-folder/0081-search-in-rotated-sorted-array-ii/solution.cpp
@@ -2,37 +2,26 @@ class Solution {
 public:
     bool search(vector<int>& v, int target) {
         int n = v.size();
-        
-        int i  =0,j = n-1;
-        
-        while(i <=j){
-            
-            int mid = i + (j-i)/2;
-            
-            int curr = v[mid];
-            
+        int l= 0,r = n-1;
+        int mid,curr;
+        while(l <=r){
+            mid = l + (r-l)/2;
+            curr = v[mid];
             if(curr == target)return true;
-            else if(v[i] == curr  && curr == v[j]){
-                if(curr == target)return true;
-                i++;j--;
+            if(curr == v[l] && curr == v[r]){
+                l++;r--;
             }
-            // Left part.
-            else if(curr >= v[i]){
-                if(target >= v[i] && curr >= target){
-                    j = mid-1;
-                }
-                else i =mid + 1; 
+            else if(curr >= v[l]){
+                if(target >= v[l] && target < curr)r = mid -1;
+                else l = mid + 1;
+            }
+            else{
+                if(target <= v[r] && target > curr)l = mid + 1;
+                else r = mid - 1;
             }
             
-            else{
-                if(target <= v[j] && target >= curr){
-                    i = mid + 1;
-                }
-                else j = mid - 1;
-            }
+            
         }
         return false;
-        
     }
-}; // [1,0,1,1,1]
-
+};
