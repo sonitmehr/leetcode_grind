a8d9b37da8836c7c896323525262a7c494b6a5e1
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-10-20 11:27:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.8 MB (100.00%)

--- my-folder/0213-house-robber-ii/solution.cpp
+++ my-folder/0213-house-robber-ii/solution.cpp
@@ -1,44 +1,25 @@
 class Solution {
 public:
     
-    int f(int i,vector<int> &dp,vector<int> &a){
-        if(i == 0)return a[i];
-        if(i < 0)return 0;
+    int f(int i,int stop,vector<int> &dp,vector<int> &a){
+        if(i < stop)return 0;
+        if(i == stop)return a[i];
         if(dp[i]!=-1)return dp[i];
         
         
-        int l = a[i] + f(i-2,dp,a);
-        int r = f(i-1,dp,a);
+        int l = a[i] + f(i-2,stop,dp,a);
+        int r = f(i-1,stop,dp,a);
                 
         return dp[i]=max(l,r);
     }
-    
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        if(n==1)return nums[0];
+    int rob(vector<int>& a) {
+        int n = a.size();
+        
         vector<int> dp(n,-1);
-        vector<int> dp1(n,-1);
-        vector<int> exceptFirstEle(n-1);
-        vector<int> exceptLastEle(n-1);
-        
-        int ptr1=0,ptr2 = 0;
-        for(int i =0;i<n;i++){
-            if(i!=0){
-                exceptFirstEle[ptr1] = nums[i];
-                ptr1++;
-            }
-            if(i!=(n-1)){
-                exceptLastEle[ptr2] = nums[i];
-                ptr2++;
-            }
-        }
-        cout << "exceptLastEle " << endl;
-        for(auto i : exceptLastEle)cout << i << " ";
-        cout << "\n" << "exceptFirstEle " << endl;
-        for(auto i : exceptFirstEle)cout << i << " ";
-        
-        int r = f(n-2,dp,exceptLastEle);
-        int l = f(n-2,dp1,exceptFirstEle);
+        if (n < 2) return n ? a[0] : 0;
+        int l = f(n-1,1,dp,a);
+        fill(dp.begin(),dp.end(),-1);
+        int r = f(n-2,0,dp,a);
         return max(l,r);
     }
 };
