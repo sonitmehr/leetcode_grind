cf65d06f21e7c58e7b2c089c30cf6ba23ca08048
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-10-20 04:20:37 +0000

    [LeetCode Sync] Runtime - 70 ms (9.05%), Memory - 13.4 MB (38.96%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.cpp
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,26 +1,22 @@
-#include <iostream>
-#include <string>
-#include <unordered_set>
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
-        int l = 0,r=0,maxInt = 0;
-        if(n == 0)return 0;
-        unordered_set<char> set;
-        while(r<n){
-            if(set.find(s[r])!=set.end()){
-                set.erase(s[l]);
+        int l = 0,r = 0;
+        set<char> st;
+        int maxi = 0;
+        while(r < n){
+            if(st.find(s[r])!=st.end()){
+                st.erase(s[l]);
                 l++;
             }
             else{
-                set.insert(s[r]);
-                int size_set = set.size();
-                maxInt = max(size_set,maxInt);
+                st.insert(s[r]);
                 r++;
+                maxi = max(maxi,(int)st.size());
             }
-           
         }
-        return maxInt;
+        return maxi;
+        
     }
 };
