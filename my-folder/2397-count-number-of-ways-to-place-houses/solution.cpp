8bd0700627bc06d8657ea95b4b6ab8799b0f051c
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-10-20 11:47:36 +0000

    [LeetCode Sync] Runtime - 39 ms (10.73%), Memory - 13.6 MB (40.18%)

--- my-folder/2397-count-number-of-ways-to-place-houses/solution.cpp
+++ my-folder/2397-count-number-of-ways-to-place-houses/solution.cpp
@@ -3,22 +3,28 @@ public:
     
     int M = 1e9 + 7;
     long long f(int i,vector<long long> &dp){
-        if(i == 0)return 1;
         
-        if(i< 0)return 0;
+        if(i == 1)return dp[i] = 2;
+        if(i == 2)return dp[i] = 3;
+        
         
         if(dp[i] != -1)return dp[i];
         
-        int l = 1 + f(i-2,dp)%M;
+        int l =  f(i-2,dp)%M;
         int r = 0 + f(i-1,dp)%M;
         
         return dp[i] = (l%M + r%M)%M;
     }
     
     int countHousePlacements(int n) {
-        vector<long long> dp(n,-1);
-        long long sum = f(n-1,dp)%M + 1;
-        sum = sum% M;
-        return (sum * sum)%M;
+        vector<long long> dp(n+1,-1);
+        f(n,dp);
+        return dp[n]*dp[n]%M;
+        // dp[0] = 1;
+        // dp[1] = 1;
+        // for(int i = 2;i<n;i++){
+        //     dp[i] = dp[i-1] + 1 + dp[i-2];
+        // }
+        // return dp[n-1];
     }
 };
