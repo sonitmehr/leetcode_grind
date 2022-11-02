0af44a83d4b2915fcc3459fe2438f5451a2f3993
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-11-02 05:57:30 +0000

    [LeetCode Sync] Runtime - 1569 ms (18.31%), Memory - 174 MB (54.62%)

--- my-folder/0315-count-of-smaller-numbers-after-self/solution.cpp
+++ my-folder/0315-count-of-smaller-numbers-after-self/solution.cpp
@@ -1,73 +1,27 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+
+typedef tree <int, null_type, less_equal <int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
 class Solution {
 public:
-
-    void merge(int left, int mid, int right, vector<pair<int, int>>& arr,vector<int>& count)
-    {
-        vector<pair<int, int>> temp(right - left + 1);
-        
-        int i = left;
-        int j = mid + 1;
-        int k = 0;
-        
-        while(i <= mid && j <= right)
-        {
-            if(arr[i].first <= arr[j].first)
-            {
-                temp[k++] = arr[j++]; 
-            }
-            else
-            {
-                count[arr[i].second] += (right - j + 1);
-                
-                temp[k++] = arr[i++];
-            }
-        }
-        
-        while(i <= mid)
-        {
-            temp[k++] = arr[i++];
-        }
-        
-        while(j <= right)
-        {
-            temp[k++] = arr[j++];
-        }
-        
-        for(int l = left; l <= right; l++)
-        arr[l] = temp[l - left];
-        
-    }
-                
-    void mergeSort(int left, int right, vector<pair<int, int>>& arr, vector<int>& count)
-    {
-        if(left >= right)
-        {
-            return;
-        }
-
-        int mid = left + (right - left) / 2;
-        
-        mergeSort(left, mid, arr, count);
-        mergeSort(mid + 1, right, arr, count);
-        
-        merge(left, mid, right, arr, count);
-    }
     
-	vector<int> countSmaller(vector<int>& nums) {
-	    
-        int n=nums.size();
-	    vector<pair<int, int>> arr;
+    vector<int> countSmaller(vector<int>& nums) {
+        int n = nums.size();
+        pbds st;
         
-	    for(int i = 0; i < n; i++)
-	    {
-	        arr.push_back({nums[i], i});
-	    }
-	    
-	    vector<int> count(n, 0);
-	    
-	    mergeSort(0, n - 1, arr, count);
-	    
-	    return count;
-	}
-  
+        vector <int> vec;
+        
+        for(int i = n-1; i >= 0; i--){
+            
+            vec.push_back(st.order_of_key(nums[i]));
+            st.insert(nums[i]);
+            
+        }
+        
+        reverse(vec.begin(), vec.end());
+        return vec;
+    }
 };
