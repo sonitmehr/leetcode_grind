ac2cb91189ac3113b6a8675ecd4144fa6c2d2369
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2022-12-30 11:23:15 +0000

    [LeetCode Sync] Runtime - 13 ms (26.35%), Memory - 10.7 MB (100.00%)

--- my-folder/0813-all-paths-from-source-to-target/solution.cpp
+++ my-folder/0813-all-paths-from-source-to-target/solution.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     
-    void f(int i,int n,vector<int> &v,vector<vector<int>> &adj,vector<vector<int>> &ans){
-        if(i == n - 1){
-            //v.insert(v.begin(),0);
-            
-            ans.push_back(v);
-            return;
+    void dfs(int i,vector<vector<int>> &graph,vector<int>&path,vector<vector<int>> &ans){
+       
+
+
+        for(auto node : graph[i]){
+            if(node != -1){
+                int temp = node;
+                path.push_back(node);
+                node = -1;
+                dfs(temp,graph,path,ans);
+                path.pop_back();
+            }
         }
+         if(i == graph.size()-1)ans.push_back(path);
         
-        for(auto ii : adj[i]){
-            v.push_back(ii);
-            f(ii,n,v,adj,ans);
-            v.pop_back();
-        }
     }
-    
+
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<int> path;
         vector<vector<int>> ans;
-        vector<int> v;
-        v.push_back(0);
-        int n = graph.size();
-        f(0,n,v,graph,ans);
+        path.push_back(0);
+        dfs(0,graph,path,ans);
+
         return ans;
-    }
+    }   
 };
