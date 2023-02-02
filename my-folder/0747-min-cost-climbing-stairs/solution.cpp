78b5a876dce0db2abb07534ec4e2ad5e17965563
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-02-02 07:02:42 +0000

    [LeetCode Sync] Runtime - 22 ms (3.51%), Memory - 14.2 MB (100.00%)

--- my-folder/0747-min-cost-climbing-stairs/solution.cpp
+++ my-folder/0747-min-cost-climbing-stairs/solution.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    
-    int f(int i,vector<int> &dp,vector<int> &a){
-        if(i < 0 || i >= a.size())return 0;
+    int dp[100001];
+    int f(int i,vector<int> &a){
+        if(i >=a.size())return 0;
+        if(i == a.size())return a[i];
+        if(dp[i] != -1)return dp[i];
+        int one = a[i] + f(i+1,a);
+        int two = a[i] + f(i + 2,a);
+
+        return dp[i] = min(one,two);
         
-        if(i == a.size()-1)return a[i];
-        
-        if(dp[i]!=-1)return dp[i];
-        
-        int l = a[i] + f(i+1,dp,a);
-        int r = a[i] + f(i+2,dp,a);
-        
-        return dp[i] = min(l,r);
+
     }
-    
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int> dp(n+1,-1);
-        vector<int> dp1(n+1,-1);
-        int l = f(0,dp,cost);
-        
-        int r = f(1,dp1,cost);
-        return min(l,r);
+        memset(dp,-1,sizeof(dp));
+        int one = f(0,cost);
+        memset(dp,-1,sizeof(dp));
+        return min(one,f(1,cost));
     }
 };
