4fef74c9383ec3a32c75a87e909236bd7ef828ca
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-02-04 13:40:47 +0000

    [LeetCode Sync] Runtime - 33 ms (5.30%), Memory - 14.2 MB (99.16%)

--- my-folder/0967-minimum-falling-path-sum/solution.cpp
+++ my-folder/0967-minimum-falling-path-sum/solution.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    int n;
-    int f(int i,int j,vector<vector<int>>&a,vector<vector<int>>&dp){
-        if(j < 0 || j >= n)return 1e9;
-        if(i == n - 1)return a[i][j];
+    int dp[1001][1001];
+    int f(int i, int j,int n, int m,vector<vector<int>>& grid){
+       
         
-        if(dp[i][j]!=-1)return dp[i][j];
+        if(i >= n || i < 0 || j >=m || j < 0)return 1e9;
+        if(i == (n -1))return grid[i][j];
+
+        if(dp[i][j] !=-1)return dp[i][j];
         
-        int one = a[i][j] + f(i+1,j,a,dp);
-        int two = a[i][j] + f(i+1,j+1,a,dp);
-        int three = a[i][j] + f(i+1,j-1,a,dp);
-        
-        return dp[i][j] = min({one,two,three});
-        
+        return dp[i][j] = grid[i][j] + min({f(i+1,j,n,m,grid),f(i+1,j-1,n,m,grid),f(i+1,j+1,n,m,grid)});
     }
-    
-    
-    int minFallingPathSum(vector<vector<int>>& a) {
-        int n = a.size();
-        this->n = n;
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+
+
+    int minFallingPathSum(vector<vector<int>>& grid) {
         
+        int n = grid.size(),m = grid[0].size();
         int mini = 1e9;
-        for(int i = 0;i<n;i++){
-            mini = min(mini,f(0,i,a,dp));
-        }
+        memset(dp,-1,sizeof(dp));
+        //int mini = 1e9;
+        for(int k = 0;k<m;k++){
+            mini = min(mini,f(0,k,n,m,grid));
+        }    
         return mini;
-        
     }
 };
