8f7dd9fbd6a7ab007058a8a8e481662c82047799
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-02-04 13:01:00 +0000

    [LeetCode Sync] Runtime - 27 ms (0.84%), Memory - 10.4 MB (8.83%)

--- my-folder/0062-unique-paths/solution.cpp
+++ my-folder/0062-unique-paths/solution.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
-    
-    int f(int i,int j,vector<vector<int>> &dp,int m,int n){
-        if(i  < 0 || j  < 0)return 0;
+    int dp[1001][1001];
+    int f(int i,int j){
+        if(i < 0 || j < 0)return 0;
         if(i == 0 && j == 0)return 1;
-        
+
         if(dp[i][j] != -1)return dp[i][j];
-        
-        int l = f(i - 1,j,dp,m,n);
-        int r = f(i,j - 1,dp,m,n);
-        return dp[i][j] = l + r;
+        return dp[i][j] = f(i -1,j) + f(i,j -1);
+
     }
-    
+
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        dp[0][0] = 1;
-       // //dp[0][1] = 1;
-       // dp[1][0] = 1;
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j< n;j++){
-                if(i == 0 && j == 0)continue;
-                int l = 0;
-                if(i > 0)l = dp[i-1][j];
-                int r = 0;
-                if(j > 0) r = dp[i][j-1];
-                
-                dp[i][j] = l + r;
-            }
-        }
-        return dp[m-1][n-1];
+        memset(dp,-1,sizeof(dp));
+
+        return f(m-1,n-1);
     }
 };
