cffd651a8f85c4885fdb6eaef8cd6f501713dd82
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-02-04 06:17:08 +0000

    [LeetCode Sync] Runtime - 109 ms (30.97%), Memory - 7.6 MB (100.00%)

--- my-folder/0567-permutation-in-string/solution.cpp
+++ my-folder/0567-permutation-in-string/solution.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int n1 = s1.size();
-        int n2 = s2.size();
-        if(n2 < n1)return false;
-        vector<int> v1(26,0),v2(26,0);
-        for(int i = 0;i<n1;i++){
-            v1[s1[i]-'a']++;
-            v2[s2[i]-'a']++;
+        map<char,int> m1,m2;
+        for(auto i : s1)m1[i]++;
+        int n = s1.size();
+        if(n > s2.size())return false;
+        for(int i = 0;i<n;i++){
+            m2[s2[i]]++;
         }
-        int l = 0;
-        int r = n1-1;
-        while(r<n2){
-            if(v1 == v2)return true;
-            r++;
-            if(r <n2)v2[s2[r]-'a']++;
-            v2[s2[l]-'a']--;
-            l++;
+        int start = 0;
+        if(m1 == m2)return true;
+        for(int i = n;i<s2.size();i++){
+            m2[s2[start]]--;
+            m2[s2[i]]++;
+            start++;
+            bool flag = true;
+            for(char c = 'a';c<='z';c++){
+                if(m1[c] != m2[c])flag = false;
+            }
+            if(flag)return true;
         }
         return false;
+
     }
 };
