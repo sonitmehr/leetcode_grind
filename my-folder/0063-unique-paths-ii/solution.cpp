d6d005a81aa7881a8ed0244db8de8ca02b225b03
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-02-04 13:03:50 +0000

    [LeetCode Sync] Runtime - 12 ms (3.68%), Memory - 12.1 MB (6.30%)

--- my-folder/0063-unique-paths-ii/solution.cpp
+++ my-folder/0063-unique-paths-ii/solution.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
-    
-    int f(int i,int j,vector<vector<int>> &a,vector<vector<int>> &dp){
-        if(i>=0 && j>=0 && a[i][j] == 1)return 0;
-        if(i ==0 && j == 0)return 1;
-        if(i<0 || j<0)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        
-        int l = f(i-1,j,a,dp);
-        int r = f(i,j-1,a,dp);
-        return dp[i][j]= l+r;
+    int dp[1001][1001];
+    int f(int i ,int j,vector<vector<int>>& obstacleGrid){
+        if(i < 0 || j < 0)return 0;
+        if(obstacleGrid[i][j] == 1)return 0;
+        if(i == 0 && j == 0)return 1;
+
+        if(dp[i][j] != -1)return dp[i][j];
+
+        return dp[i][j] = f(i-1,j,obstacleGrid) + f(i,j-1,obstacleGrid);
+
     }
-    
-    
+
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        memset(dp,-1,sizeof(dp));
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1)); 
-        dp[0][0] = 1;
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(obstacleGrid[i][j] == 1)dp[i][j] = 0;
-                else if(i==0 && j ==0)continue;
-                else {
-                    int l=0,r=0;
-                    if(i>0) l = dp[i-1][j];
-                    if(j>0) r = dp[i][j-1];
-                    dp[i][j] = l + r;
-                }
-                
-            }
-        }
-        return dp[m-1][n-1];
+
+        return f(m-1,n-1,obstacleGrid);
     }
 };
