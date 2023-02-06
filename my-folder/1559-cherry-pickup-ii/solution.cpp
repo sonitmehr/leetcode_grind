c4d5acb08fdb5c0cd816090021e4ecca7dee6d74
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-02-06 17:37:07 +0000

    [LeetCode Sync] Runtime - 102 ms (6.89%), Memory - 13.1 MB (99.75%)

--- my-folder/1559-cherry-pickup-ii/solution.cpp
+++ my-folder/1559-cherry-pickup-ii/solution.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-    
-    int f(int i,int j1,int j2,vector<vector<int>> &a,vector<vector<vector<int>>>&dp){
-        if(j1 < 0 || j2 < 0 || j1 >= a[0].size() || j2 >=a[0].size() )return -1e8;
-        if(i == a.size()-1){
+    int dp[101][101][101];
+    int f(int i, int j1,int j2,vector<vector<int>> &a){
+        if(i < 0 || j1 < 0 || j2 < 0 || i >= a.size() || j1 >= a[0].size() || j2 >= a[0].size())return 0;
+
+        if(i == a.size() - 1){
             if(j1 == j2)return a[i][j1];
             return a[i][j1] + a[i][j2];
         }
-        if(dp[i][j1][j2]!=-1)return dp[i][j1][j2];
-        int maxi = -1e8;
-        for(int alice = -1;alice<=1;alice++){
-            for(int bob = -1;bob<=1;bob++){
-                int val;
-                if(j1 == j2)val = a[i][j1];
-                else val = a[i][j1] + a[i][j2];
-                
-                val += f(i+1,j1+alice,j2+bob,a,dp);
-                maxi = max(maxi,val);
-                
+        if(dp[i][j1][j2] != -1)return dp[i][j1][j2];
+        int maxi = 0;
+        for(int ptr1 = -1;ptr1 <= 1;ptr1++){
+            for(int ptr2 = -1;ptr2 <= 1;ptr2 ++){
+                if(j1 == j2)maxi =max(maxi,a[i][j1] + f(i + 1,j1 +ptr1,j2 + ptr2,a ));
+
+                else maxi = max(maxi,a[i][j1] + a[i][j2] + f(i+1,j1 + ptr1,j2 + ptr2,a));
             }
         }
         return dp[i][j1][j2] = maxi;
-        
+
+
     }
-    
-    int cherryPickup(vector<vector<int>>& a) {
-        int n = a.size();
-        int m = a[0].size();
-        
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(m+1,vector<int>(m+1,-1)));
-        
-        return f(0,0,m-1,a,dp);
-        
+
+    int cherryPickup(vector<vector<int>>& grid) {
+        memset(dp,-1,sizeof(dp));
+        int n = grid.size(),m = grid[0].size();
+        return f(0,0,m -1,grid);
     }
 };
