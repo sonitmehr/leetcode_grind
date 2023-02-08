30553ac94593b807f62b21f85732867d41fe4626
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-02-08 08:04:28 +0000

    [LeetCode Sync] Runtime - 294 ms (5.00%), Memory - 17.6 MB (100.00%)

--- my-folder/0045-jump-game-ii/solution.cpp
+++ my-folder/0045-jump-game-ii/solution.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     int dp[10001];
-    int minJumps;
-    int f(int i,vector<int>&a,int jumps){
-        if(i >= a.size()-1)return 0;
-        // if(i == a.size() -1){
-            
-        //     return jumps;
-        // }
+    int f(int i,vector<int>&a){
+        if(i >= a.size() - 1)return 0;
+        int mini = 1e9,one = 1e9;
         if(dp[i] != -1)return dp[i];
-        //cout << i << " " << jumps << endl;
-
-        int one = 1e9,mini = 1e9;
         for(int ind = 1;ind<=a[i];ind++){
-            one =  min(one,1 + f(i + ind,a,jumps + 1));
-            mini = min(one,mini);
+            mini = min(mini,1 + f(i + ind,a));
+            //mini = min(mini,one);
+            //cout << mini << " " << ind << endl;
         }
-        return dp[i] = one;
+        return dp[i] = mini;
     }
 
     int jump(vector<int>& a) {
         memset(dp,-1,sizeof(dp));
-        minJumps =1e9;
-        return f(0,a,0);
-        
+        return f(0,a);
     }
 };
