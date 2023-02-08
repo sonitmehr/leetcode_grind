79d86a73eefe25baa34d83ef5c98927da8f6a378
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-02-08 07:38:41 +0000

    [LeetCode Sync] Runtime - 26 ms (64.38%), Memory - 10.7 MB (88.38%)

--- my-folder/1250-longest-common-subsequence/solution.cpp
+++ my-folder/1250-longest-common-subsequence/solution.cpp
@@ -1,32 +1,43 @@
 class Solution {
 public:
+    int dp[1001][1001];
     string s1,s2;
-    int n1,n2;
-    int f(int i,int j,vector<vector<int>> &dp){
-        if(i < 0 || j < 0)return 0;
-        if(dp[i][j] != -1)return dp[i][j];
-        if(s1[i] == s2[j])return 1 + f(i - 1,j - 1,dp);
+    int f(int i,int j){
+        if(i <0 || j < 0)return 0;
 
-        return dp[i][j] = max(f(i - 1,j,dp),f(i,j-1,dp));
+        if(dp[i][j] != -1)return dp[i][j];
+        int one = 0;
+        if(s1[i] == s2[j])one = 1 + f(i - 1,j - 1);
+        return dp[i][j] = max({one,f(i - 1,j),f(i,j- 1)});
     }
 
     int longestCommonSubsequence(string text1, string text2) {
         s1 = text1;
         s2 = text2;
-        n1 = s1.size();
-        n2 = s2.size();
-        vector<vector<int>> dp(n1 + 1,vector<int>(n2 + 1,-1));
-        return f(n1-1,n2-1,dp);
-        // for(int i = 0;i<n1;i++)dp[i][0] = 0;
-        // for(int i = 0;i<n2;i++)dp[0][i] = 0;
-
-        // for(int i = 1;i<=n1;i++){
-        //     for(int j = 1;j<=n2;j++){
-        //         if(s1[i- 1] == s2[j- 1])dp[i][j] = 1 + dp[i-1][j-1];
-        //         else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        int n1 = s1.size(),n2 = s2.size();
+        // for(int i =0;i<n2;i++)dp[i][0] = 0;
+        // for(int i = 0;i<n1;i++)dp[0][i] = 0;
+        // for(int i= 0;i<=n1;i++){
+        //     for(int j= 0;j<=n2;j++){
+        //         cout << dp[i][j] << " ";
         //     }
+        //     cout << endl;
         // }
-        // return max(0,dp[n1][n2]);
         
+        for(int i = 1;i<=n1;i++){
+            for(int j = 1;j<=n2;j++){
+                if(s1[i - 1] == s2[j - 1])dp[i][j] = 1 + dp[i - 1][j - 1];
+                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+            
+        }
+        // cout << endl;
+        // for(int i= 0;i<=n1;i++){
+        //     for(int j= 0;j<=n2;j++){
+        //         cout << dp[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        return dp[n1][n2];
     }
 };
