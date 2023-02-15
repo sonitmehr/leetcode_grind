f274bc19187d8c483e9701f01e84c8a20d013440
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-02-15 13:16:06 +0000

    [LeetCode Sync] Runtime - 11 ms (77.59%), Memory - 10.5 MB (100.00%)

--- my-folder/0300-longest-increasing-subsequence/solution.cpp
+++ my-folder/0300-longest-increasing-subsequence/solution.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& v) {
+        vector<int> ans;
         int n = v.size();
-        
-        int ans = 0;
-        vector<int> arr;
-        arr.push_back(v[0]);
-        for(int i= 1;i<n;i++){
-            auto it = lower_bound(arr.begin(),arr.end(),v[i]);
-            
-            if(it == arr.end()){
-                
-                arr.push_back(v[i]);
+        ans.push_back(v[0]);
+        int maxi = 1;
+        for(int i = 1;i<n;i++){
+            auto it = lower_bound(ans.begin(),ans.end(),v[i]);
+            //cout <<v[i] << " " << it - ans.begin() << endl;
+            if(it == ans.end()){
+                ans.push_back(v[i]);
             }
             else{
-                arr[it - arr.begin()] = v[i];
+                //it--;
+                int ind = it - ans.begin();
+                ans[ind] = v[i];
             }
-            
+            maxi = max(maxi,(int)ans.size());
         }
-        return arr.size();
+        return maxi;
+
     }
 };
