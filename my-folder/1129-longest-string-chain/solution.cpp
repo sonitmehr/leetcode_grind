4413d9cd338fd8458da46953965cf06529d0f978
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-02-16 11:43:38 +0000

    [LeetCode Sync] Runtime - 130 ms (36.93%), Memory - 13.4 MB (100.00%)

--- my-folder/1129-longest-string-chain/solution.cpp
+++ my-folder/1129-longest-string-chain/solution.cpp
@@ -1,19 +1,39 @@
 class Solution {
-   public:
-    int longestStrChain(vector<string> &words) {
-        unordered_map<string, int> dp;
-        int res = 1;
-        sort(words.begin(), words.end(), [](const string &l, const string &r) { return l.size() < r.size(); });
-        for (string word : words) {
-            dp[word] = 1;
-            for (int i = 0; i < word.size(); i++) {
-                string prev = word.substr(0, i) + word.substr(i + 1);
-                if (dp.find(prev) != dp.end()) {
-                    dp[word] = dp[prev] + 1;
-                    res = max(res, dp[word]);
-                }
+public:
+    bool check(string &s1,string &s2){
+        if(s1.size() != s2.size() +1)return false;
+
+        int i = 0,j = 0;
+        while(i < s1.size() ){
+            if(j< s2.size() && s1[i] == s2[j]){
+                i++;
+                j++;
+            }
+            else{
+                i++;
             }
         }
-        return res;
+        if(i == s1.size() && j == s2.size())return true;
+        return false;
+
+
+    }
+    static bool comp(string &s1,string &s2){
+        return s1.size() < s2.size();
+    }
+    int longestStrChain(vector<string>& v) {
+        int n = v.size();
+        sort(v.begin(),v.end(),comp);
+        vector<int> dp(n + 1,1);
+        int maxi = 0;
+        for(int i = 0;i<n;i++){
+            for(int prev = 0;prev< i;prev++){
+                if(check(v[i],v[prev]) && dp[i] < dp[prev] + 1){
+                    dp[i] = dp[prev] + 1;
+                }
+            }
+            maxi = max(dp[i],maxi);
+        }
+        return maxi;
     }
 };
