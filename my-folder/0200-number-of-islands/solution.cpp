84b0e77d100cec4d9d4caffca4e1653b5308270b
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-02-16 13:35:05 +0000

    [LeetCode Sync] Runtime - 44 ms (12.62%), Memory - 19.1 MB (41.93%)

--- my-folder/0200-number-of-islands/solution.cpp
+++ my-folder/0200-number-of-islands/solution.cpp
@@ -1,46 +1,60 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& v) {
-        int n = v.size();
-        int m = v[0].size();
-        vector<vector<int>> vis(n+1,vector<int> (m+1,0));
-        int cnt = 0;
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(vis[i][j] == 0 && v[i][j] == '1'){
-                    cnt++;
-                    queue<pair<int,int>> q;
-                    vis[i][j] = 1;
-                    q.push({i,j});
-                    while(!q.empty()){
-                        pair<int,int> node = q.front(); 
-                        q.pop();
-                        int I = node.first;
-                        int J = node.second;
-                        if(I < n && I >= 0 && J < m && J >= 0){
-                            if(I -1 >=0 && vis[I -1][J] == 0 && v[I -1][J] == '1'){
-                                q.push({I-1,J});
-                                vis[I -1][J] =1;
-                            }
-                            if(I+1 < n && vis[I + 1][J] == 0 && v[I + 1][J] == '1'){
-                                q.push({I +1,J});
-                                vis[I +1][J] =1;
-                            }
-                            if(J-1 >=0 && vis[I][J - 1] == 0 && v[I][J - 1] == '1'){
-                                q.push({I,J -1});
-                                vis[I][J -1] =1;
-                            }if(J+1 <m && vis[I][J + 1] == 0 && v[I][J + 1] == '1'){
-                                q.push({I,J+1});
-                                vis[I][J +1] =1;
-                            }
-                        }                       
-                        
-                    }
-                
-                    
+    int vis[301][301];
+    bool valid(int i,int j,int n,int m){
+        if(i < 0 || j < 0 || i >= n || j >= m)return false;
+        return true;
+    }
+    void dfs(int i,int j,int n,int m,vector<vector<char>>& grid){
+        vector<int> a = {-1,1,0,0},
+                    b = {0,0,-1,1};
+        for(int k = 0;k<=3;k++){
+            int addI = a[k],addJ = b[k];
+            if(valid(i + addI,j + addJ,n,m) && grid[i + addI][j + addJ] == '1' && vis[i + addI][j + addJ] == 0 ){
+                //cout << i << " " << j << endl;
+                vis[i + addI][j + addJ] = 1;
+                dfs(i + addI,j + addJ,n,m,grid);
+            }
+            
+        }
+    }
+
+   void bfs(int i,int j,int n,int m,vector<vector<char>>& grid){
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        vector<int> a = {-1,1,0,0},b = {0,0,-1,1};
+        
+        while(!q.empty()){
+            auto p = q.front();
+            q.pop();
+            int newI = p.first,newJ = p.second;
+
+            for(int k = 0;k<=3;k++){
+                int addI = a[k], addJ = b[k];
+                if(valid(newI + addI,newJ + addJ,n,m) && grid[newI + addI][newJ + addJ] == '1' && vis[newI + addI][newJ + addJ] == 0 ){
+                    //cout << i << " " << j << endl;
+                    vis[newI + addI][newJ + addJ] = 1;
+                    q.push({newI + addI,newJ + addJ});
+
                 }
             }
         }
-        return cnt;
+
+   }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size(),m = grid[0].size();
+        int ans = 0;
+        memset(vis,0,sizeof(vis));
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(grid[i][j] == '1' && vis[i][j] == 0){
+                    vis[i][j] = 1;
+                    bfs(i,j,n,m,grid);
+                    ans++;
+                }
+            }
+        }
+        return ans;
     }
 };
