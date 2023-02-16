364a723c20d5afa41e7f20d049384b0b4a22d936
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-02-16 13:44:21 +0000

    [LeetCode Sync] Runtime - 11 ms (2.16%), Memory - 14.2 MB (100.00%)

--- my-folder/0733-flood-fill/solution.cpp
+++ my-folder/0733-flood-fill/solution.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    
-    void f(int i, int j,int startColor,int color,vector<vector<int>> &a,vector<vector<int>>&vis){
-        if(i < 0 || j < 0 || i >= a.size() || j >= a[0].size())return;
-        
-        
-        if(vis[i][j] == 0 && a[i][j] == startColor){
-            a[i][j] = color;
-            vis[i][j] = 1;
-            f(i-1,j,startColor,color,a,vis);
-            f(i+1,j,startColor,color,a,vis);
-            f(i,j-1,startColor,color,a,vis);
-            f(i,j+1,startColor,color,a,vis);
-        }
-        
-        //[[0,0,0],
-        // [0,0,0]]
-        
-        
+    int vis[51][51];
+    bool valid(int i,int j,int n,int m){
+        if(i < 0 || j < 0 || i >= n || j >= m)return false;
+        return true;
     }
-    
-    vector<vector<int>> floodFill(vector<vector<int>>& a, int sr, int sc, int newColor) {
-        vector<vector<int>> vis(a.size(),vector<int>(a[0].size(),0));
-        
-        f(sr,sc,a[sr][sc],newColor,a,vis);
-        return a;
+    void dfs(int i, int j,int n,int m,int oldCol,int newCol,vector<vector<int>>& image){
+        vector<int> a = {-1,1,0,0},
+                    b = {0,0,-1,1};
+        image[i][j] = newCol;
+        for(int k = 0;k<=3;k++){
+            int newI = i + a[k],newJ = j + b[k];
+            if(valid(newI,newJ,n,m) && image[newI][newJ] == oldCol && vis[newI][newJ] == 0){
+                vis[newI][newJ] = 1;
+                //image[newI][newJ] = newCol;
+                dfs(newI,newJ,n,m,oldCol,newCol,image);
+            }
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n = image.size(), m = image[0].size();
+        //image[sr][sc] = color;
+        dfs(sr,sc,n,m,image[sr][sc],color,image);
+        return image;
     }
 };
