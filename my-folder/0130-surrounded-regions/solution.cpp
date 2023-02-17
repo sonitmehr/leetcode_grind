3123143f116d3dd42a468083ca8bd3efd1432bd1
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-02-17 09:42:37 +0000

    [LeetCode Sync] Runtime - 11 ms (6.04%), Memory - 10.6 MB (100.00%)

---
+++ my-folder/0130-surrounded-regions/solution.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+bool valid(int i,int j,int n,int m){
+        if(i < 0 || j < 0 || i >= n || j >= m)return false;
+        return true;
+    }
+    bool border(int i,int j,int n,int m){
+        if(i == n - 1 || j == m - 1 || i == 0 || j == 0)return true;
+        return false;
+    }
+    void solve(vector<vector<char>>& mat) {
+        int n = mat.size(),m = mat[0].size();
+        queue<pair<int,int>> q;
+        vector<vector<int>> vis(n+1,vector<int>(m+1,0));
+        
+        for(int i = 0;i<n;i++){
+            if(mat[i][0] == 'O')q.push({i,0});
+            if(mat[i][m - 1] == 'O')q.push({i,m - 1});
+
+        }
+        for(int j = 1;j<m - 1;j++){
+            if(mat[0][j] == 'O')q.push({0,j});
+            if(mat[n - 1][j] == 'O')q.push({n-1,j});
+        }
+        vector<int> a = {-1,1,0,0},
+        b = {0,0,-1,1};
+        
+        
+        while(!q.empty()){
+            auto p = q.front();
+            q.pop();
+            int I = p.first,J = p.second;
+            if(vis[I][J] == 1)continue;
+            vis[I][J] = 1;
+            for(int k = 0;k<=3;k++){
+                int newI = I + a[k],newJ = J + b[k];
+                if(valid(newI,newJ,n,m) && mat[newI][newJ] == 'O' && vis[newI][newJ] == 0){
+                    q.push({newI,newJ});   
+                }
+            }
+        }
+            
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(mat[i][j] == 'O' && vis[i][j] == 0){
+                    mat[i][j] = 'X';
+                }
+            }
+        }
+            
+        
+    }
+};
