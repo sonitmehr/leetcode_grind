e13d835e343a0e36b23533d68dd4284d06b166ab
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-02-17 08:18:06 +0000

    [LeetCode Sync] Runtime - 362 ms (5.01%), Memory - 79.4 MB (5.08%)

--- my-folder/0542-01-matrix/solution.cpp
+++ my-folder/0542-01-matrix/solution.cpp
@@ -1,34 +1,52 @@
 class Solution {
 public:
+    /*
+    [[0,1,0],
+     [0,1,0],
+     [0,1,0],
+     [0,1,0],
+     [0,1,0]]
+    */
+    bool valid(int i,int j,int n,int m){
+        if(i < 0 || j < 0 || i >= n || j >= m)return false;
+        return true;
+    }
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-      int rs = mat.size();
-      int cs = mat[0].size();
-      vector<vector<int>> ans(mat.size(),vector<int>(mat[0].size(),-1));
-      queue<pair<int, int>>q;
-      for(int i=0; i<rs; i++){
-        for(int j=0; j<cs; j++){
-          if(mat[i][j] == 0){
-            q.push({i, j});
-            ans[i][j] = 0;
-          }
+        int n = mat.size(),m = mat[0].size();
+        vector<vector<int>> ans(n,vector<int>(m,1e9));
+        vector<vector<int>> vis(n+1,vector<int>(m+1,0));
+        queue<pair<pair<int,int>,int>> q;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(mat[i][j] == 0){
+                    q.push({{i,j},0});
+                    ans[i][j] = 0;
+                    vis[i][j] = 1;
+                }
+                //lse ans[i][j] = 0;
+            }
         }
-      }
-      int dx[4] = {0, 0, 1, -1};
-      int dy[4] = {1, -1, 0, 0};
-      
-      while(!q.empty()){
-        int x = q.front().first, y = q.front().second;
-        q.pop();
-        for(int i=0; i<4; i++){
-          int nx = x + dx[i], ny = y + dy[i];
-          if(nx<0 || nx>=rs || ny<0 || ny>=cs || ans[nx][ny]==0)continue;
-          if(ans[nx][ny] == -1){
-            ans[nx][ny] = ans[x][y]+1;
-            q.push({nx, ny});
-          }
+        while(!q.empty()){
+            auto p = q.front();
+            vector<int> a = {-1,1,0,0},b = {0,0,-1,1};
+            int i = p.first.first;
+            int j = p.first.second;
+            int dist = p.second;
+            q.pop();
+            vis[i][j] = 1;
+            bool flag = false;
+            if(mat[i][j] == 1)ans[i][j] = min(ans[i][j],dist);
+            for(int k = 0;k<=3;k++){
+                int newI = i + a[k],newJ = j + b[k];
+                if(valid(newI,newJ,n,m) && vis[newI][newJ] == 0){
+                    q.push({{newI,newJ},dist+1});
+                }
+            }
+            
+
         }
-        
-      }
-      return ans;
+        return ans;
+
+
     }
 };
