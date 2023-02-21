4efe157acc349b0bdfdd9f49afa97f7563b8e973
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-02-21 13:37:10 +0000

    [LeetCode Sync] Runtime - 26 ms (5.14%), Memory - 14.1 MB (100.00%)

--- my-folder/0207-course-schedule/solution.cpp
+++ my-folder/0207-course-schedule/solution.cpp
@@ -1,59 +1,36 @@
 class Solution {
 public:
-    
-    
-    bool canFinish(int n, vector<vector<int>>& pre) {
-        
-        int n1 = pre.size();
-        vector<vector<int>> adj(n + 3);
-        
-        vector<int> vis(n + 1,0);
-        vector<int> inDeg(n + 1,0);
-        //cnt = 0;
-        for(int i =0;i<n1;i++){
-            
-            int a = pre[i][0];
-            int b = pre[i][1];
-            
-            adj[a].push_back(b);
-            inDeg[b] ++;
+    int inDegree[2001] = {0};
+    bool canFinish(int n, vector<vector<int>>& v) {
+        vector<int> adj[n + 1];
+        for(auto i : v){
+            adj[i[0]].push_back(i[1]);
         }
         
+
+        for(auto i : adj){
+            for(auto j : i){
+                inDegree[j] ++;
+            }
+        }
         queue<int> q;
-        int cnt =0;
-        for(int i=0;i<n;i++){
-            if(inDeg[i] == 0){
-                q.push(i);
-                cnt++;
-                vis[i] = 1;
-            }
+        for(int i = 0;i<n;i++){
+            if(inDegree[i] == 0)q.push(i);
         }
-        
+        int cnt = 0;
         while(!q.empty()){
-            
-            int curr = q.front();
+            auto p = q.front();
+
             q.pop();
-            
-            for(auto i : adj[curr]){
-                if(vis[i] == 0){
-                    inDeg[i]--;
-                    if(inDeg[i] == 0){
-                        cnt++;
-                        vis[i] = 1;
-                        q.push(i);
-                    }
-                    
-                }
+            cnt++;
+            for(auto i : adj[p]){
+                inDegree[i]--;
+                if(inDegree[i] == 0)q.push(i);
+
             }
-                
-            
+
         }
-        
         if(cnt == n)return true;
         return false;
-        
-        
-        
-        
     }
 };
