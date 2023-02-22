493a529674231e124941bd28ec1b7bf6eb04c207
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-02-22 13:53:22 +0000

    [LeetCode Sync] Runtime - 60 ms (5.12%), Memory - 26.1 MB (100.00%)

---
+++ my-folder/1056-capacity-to-ship-packages-within-d-days/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& v, int days) {
+        int n = v.size();
+        int maxi = *max_element(v.begin(),v.end());
+        int low = maxi,high = 0;
+        for(auto i : v)high += i;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+            int currSum = 0;
+            int d = days;
+            for(int i = 0;i < n;i ++){
+
+                if(currSum + v[i] <= mid){
+                    currSum += v[i];
+                }
+                else{
+                    currSum = v[i];
+                    d--;
+                }
+            }
+            //if(d == 0)return mid;
+            if(d > 0)high = mid - 1;
+            else low = mid + 1;
+        }
+        return low;
+        
+    }
+};
