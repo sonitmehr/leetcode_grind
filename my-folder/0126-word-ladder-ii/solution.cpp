ec20fd3b4b1c9d78f0a0bdc02ee78e27eac59f73
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-02-23 05:52:38 +0000

    [LeetCode Sync] Runtime - 20 ms (19.96%), Memory - 9.1 MB (100.00%)

--- my-folder/0126-word-ladder-ii/solution.cpp
+++ my-folder/0126-word-ladder-ii/solution.cpp
@@ -1,71 +1,80 @@
 class Solution {
-    unordered_map<string,int> m;
-    string b;
-    vector<vector<string>> ans;
 public:
-    
-    void dfs(string s,vector<string> &seq){
-        if(s == b){
-            reverse(seq.begin(),seq.end());
-            ans.push_back(seq);
-            reverse(seq.begin(),seq.end());
+unordered_map<string,int> m;
+vector<vector<string>> ans;
+int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> st;
+        for(auto i : wordList)st.insert(i);
+        queue<pair<string,int>> q;
+        q.push({beginWord,1});
+        m[beginWord] = 1;
+        st.erase(beginWord); 
+        int mini = 1e9;
+        while(!q.empty()){
+            auto p = q.front();
+            q.pop();
+            string currWord = p.first;
+            int length = p.second;
+            if(currWord == endWord){
+                mini =min(mini,length);
+                break;
+            }
+            for(int k = 0;k<currWord.size();k++){
+                char ignore = currWord[k];
+                char store = currWord[k];
+                for(char c = 'a';c <= 'z';c++){
+                    if(c != ignore){
+                        
+                        currWord[k] = c;
+                        if(st.find(currWord) != st.end()){
+                            m[currWord] = length + 1;
+                            q.push({currWord,length + 1});
+                            st.erase(currWord);
+                        }
+                        
+                    }
+                }
+                currWord[k] = store;
+            }
+            
+        }
+        //if(mini == 1e9)return 0;
+        return mini;
+        
+
+    }
+
+    void dfs(string s,vector<string> &v){
+        int level = m[s];
+        if(level == 1){
+            reverse(v.begin(),v.end());
+            ans.push_back(v);
+            reverse(v.begin(),v.end());
             return;
         }
-        int steps = m[s];
         for(int i = 0;i<s.size();i++){
-            char currLetter = s[i];
-
-            for(int j = 0;j<26;j++){
-                s[i] = j + 'a';
-                if(m.find(s)!=m.end() && m[s] == steps - 1){
-                    seq.push_back(s);
-                    dfs(s,seq);
-                    seq.pop_back();                    
-                }                
-            }
-            s[i] = currLetter;
-        }
-        
-    }
-    
-    vector<vector<string>> findLadders(string bW, string eW, vector<string>& v) {
-        int n = v.size();
-        unordered_set<string> set;
-        for(auto i : v)set.insert(i);
-        b = bW;
-        queue<string> q;
-        
-        q.push(bW);
-        m[bW] = 1;
-        
-        set.erase(bW);
-        while(!q.empty()){
-            string s = q.front();
-            int steps = m[s];
-            q.pop();
-            if( s== eW)break;
-            
-            for(int i = 0;i<s.size();i++){
-                char currLetter = s[i];
-                
-                for(int j = 0;j<26;j++){
-                    s[i] = j + 'a';
-                    if(set.find(s)!=set.end()){
-                        q.push(s);
-                        set.erase(s);
-                        m[s] = steps + 1;
-                    }                
+            char store = s[i];
+            for(char c = 'a';c<='z';c++){
+                s[i] = c;
+                if(m.find(s) != m.end() && m[s] + 1 == level){
+                    v.push_back(s);
+                    dfs(s,v);
+                    v.pop_back();
                 }
-                s[i] = currLetter;
             }
-                        
+            s[i] = store;
         }
-        if(m.find(eW) != m.end()){
-            vector<string> sequence;
-            sequence.push_back(eW);
-            dfs(eW,sequence);            
-            
-        }
+    }
+
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        int mini= ladderLength(beginWord,endWord,wordList);
+        // for(auto i : m){
+        //     cout << i.first <<  " " << i.second << endl;
+        //  }
+        vector<string> v;
+        v.push_back(endWord);
+        dfs(endWord,v);
+
         return ans;
     }
 };
