68914cdb2da024cf34efe05de96646d31e18f2dd
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-03-01 06:43:39 +0000

    [LeetCode Sync] Runtime - 3 ms (0.46%), Memory - 5.9 MB (100.00%)

--- my-folder/0070-climbing-stairs/solution.cpp
+++ my-folder/0070-climbing-stairs/solution.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
-    int dp[50];
-    int f(int i,int n){
-        if(i > n)return 0;
-        if(i == n)return 1;
+    int dp[46];
+    int f(int i){
+        if(i  < 0)return 0;
+        if(i == 1) return 1;
+        if(i == 2) return 2;
         if(dp[i] != -1)return dp[i];
-        return dp[i] = f(i + 1,n) + f(i + 2,n);
+        int one = 0,two = 0;
+        
+        return dp[i] =  f(i - 1) + f(i-2);
     }
     int climbStairs(int n) {
-        memset(dp,-1,sizeof(dp));
-        return f(0,n);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        if(n == 1 || n == 2)return n;
+        int prev1 = 1,prev2 = 2;
+        int curr = 0;
+        for(int i = 3;i<=n;i++){
+            curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return curr;
+
+        
     }
 };
