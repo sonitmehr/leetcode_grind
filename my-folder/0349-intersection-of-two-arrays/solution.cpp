276bc530da63ac2aefb3b037d758a04cab5869dc
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-03-20 18:00:05 +0000

    [LeetCode Sync] Runtime - 3 ms (38.94%), Memory - 10.1 MB (100.00%)

--- my-folder/0349-intersection-of-two-arrays/solution.cpp
+++ my-folder/0349-intersection-of-two-arrays/solution.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        vector<int> v;
-        unordered_map<int,int>m;
-        
-        for(int i = 0;i<n1;i++){
-            m[nums1[i]]++;
-        }
-        for(int i = 0;i<n2;i++){
-            if(m[nums2[i]]>0){
-                v.push_back(nums2[i]);
-                m[nums2[i]]=0;
+    vector<int> intersection(vector<int>& v1, vector<int>& v2) {
+        sort(v1.begin(),v1.end());
+        sort(v2.begin(),v2.end());
+        // 4 4 8 9 9
+        // 4 5 9
+        vector<int> ans;
+        int i =0,j = 0;
+        while( i < v1.size() && j < v2.size()){
+            if(v1[i] < v2[j]){
+                i++;
+            }
+            else if(v2[j] < v1[i]){
+                j++;
+            }
+            else{
+                if(ans.size() == 0)ans.push_back(v1[i]);
+                else if(ans.size() > 0 && ans.back() != v1[i])ans.push_back(v1[i]);
+                i++;
+                j++;
             }
         }
-        return v;
+        return ans;
     }
 };
