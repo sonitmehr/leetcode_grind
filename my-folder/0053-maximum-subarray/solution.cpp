272bfb216c22a887283d0eb34286015cae377933
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-03-30 16:52:29 +0000

    [LeetCode Sync] Runtime - 154 ms (5.19%), Memory - 67.8 MB (99.98%)

--- my-folder/0053-maximum-subarray/solution.cpp
+++ my-folder/0053-maximum-subarray/solution.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
-        int sum = 0,maxSum = INT_MIN;
-        for(int i = 0;i<n;i++){
-            if(sum < 0){
-                sum = 0;
-            }
+
+    int maxCrossingSum(vector<int>& nums,int l,int mid,int r){
+        int sum = 0;
+        int left_sum = -1e9,right_sum = -1e9;
+        for(int i = mid;i>= l;i--){
             sum += nums[i];
-            maxSum = max(sum,maxSum);
+            left_sum = max(left_sum,sum);
         }
-        return maxSum;
+        sum = 0;
+        for(int i = mid;i<=r;i++){
+            sum += nums[i];
+            right_sum = max(right_sum,sum);
+        }
+        return max({left_sum,right_sum,left_sum + right_sum - nums[mid]});
     }
+
+    int f(vector<int>&nums,int l,int r){
+        if(l > r)return -1e9;
+        if(l == r)return nums[l];
+
+        int m = (l + r)/2;
+
+        return max({f(nums,l,m - 1),f(nums,m + 1,r),maxCrossingSum(nums,l,m,r)});
+
+    }
+
+
+
+    int maxSubArray(vector<int>& nums) {
+        return f(nums,0,nums.size() - 1);
+    }
+    
 };
