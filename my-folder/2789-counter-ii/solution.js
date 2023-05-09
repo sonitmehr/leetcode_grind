17a98afaf81c616adec1e942952319b1db10f4ae
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-05-09 06:31:18 +0000

    [LeetCode Sync] Runtime - 69 ms (5.10%), Memory - 44.6 MB (100.00%)

---
+++ my-folder/2789-counter-ii/solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+
+var createCounter = function(init) {
+    let temp = init;
+    function increment (){
+        return ++temp; 
+    }
+    function decrement(){
+        return --temp;
+    }
+    function reset (){
+        temp = init;
+        return init;
+    }
+    return { increment, decrement, reset };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
