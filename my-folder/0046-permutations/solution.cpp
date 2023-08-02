cc2720387cc131761a97c66713b6b131ed42bd14
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-08-02 16:49:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.6 MB (100.00%)

--- my-folder/0046-permutations/solution.cpp
+++ my-folder/0046-permutations/solution.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    
-    void f(int ind,vector<int> &a,vector<vector<int>> &ds){
-        
-        if(ind >=a.size()){
-            ds.push_back(a);
+
+    void f(int ind,vector<int>& v,vector<vector<int>> & ds){
+        if(ind == v.size()){
+            ds.push_back(v);
             return;
         }
-        
-        for(int i = ind;i<a.size();i++){
-            swap(a[i],a[ind]);
-            f(ind+1,a,ds);
-            swap(a[i],a[ind]);
+
+        for(int i = ind;i<v.size();i++){
+            swap(v[i],v[ind]);
+            f(ind + 1,v,ds);
+            swap(v[i],v[ind]);
         }
-        
+
     }
-    
+
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ds;
-        //vector<int> v;
-        int n = nums.size();
-        
-        //map<int,bool> m;
-        
-        
-        f(0,nums,ds);
-        
-        return ds;
+        vector<vector<int>> ans;
+
+        f(0,nums,ans);
+        return ans;
     }
 };
