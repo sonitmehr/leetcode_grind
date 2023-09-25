64270767de05450c4681ec85c42884ed9942ddeb
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2023-09-25 19:32:09 +0000

    [LeetCode Sync] Runtime - 7 ms (45.85%), Memory - 12.4 MB (99.99%)

--- my-folder/0001-two-sum/solution.cpp
+++ my-folder/0001-two-sum/solution.cpp
@@ -1,18 +1,45 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> m;
-        vector<int> v;
+
+  vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        int n = nums.size();
         
-        for(int i =0;i<nums.size();i++){
-            int diff = target - nums[i];
-            if(m.find(diff)==m.end())m[nums[i]]=i;
-            else{
-                v.push_back(m.find(diff)->second);
-                v.push_back(i);
-            }
+        // [1,3,1,3,2,3,2] target = 6
+        // 1 -> 2
+        // 2 -> 6
+        // 3 -> 5
+
+        //
+        unordered_map<int,int> mp;
+//      3,2,4
+        for(int i = 0;i<n;i++){
+            mp[nums[i]] = i;
         }
-        return v;
+
+        for(int i = 0;i<n;i++){
+            int difference = target - nums[i]; // Element to be found
+
+            // Check if that element is in the array
+           std::unordered_map<int,int>::iterator it = mp.find(difference);
             
+            // If exists then we have the found the answer.
+            if (it != mp.end() && mp[difference] != i){
+                ans.push_back(i);
+                ans.push_back(mp[difference]);
+                return ans;
+            }
+
+
+        }
+        return ans;
+
+        
+
+
+
+        return ans;
+
+
     }
 };
