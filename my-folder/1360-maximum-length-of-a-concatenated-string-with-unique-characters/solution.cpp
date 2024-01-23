665c97ac78d3b9e917520d17d3caa92535f4bb39
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-01-23 07:28:46 +0000

    [LeetCode Sync] Runtime - 2 ms (91.04%), Memory - 9.6 MB (100.00%)

--- my-folder/1360-maximum-length-of-a-concatenated-string-with-unique-characters/solution.cpp
+++ my-folder/1360-maximum-length-of-a-concatenated-string-with-unique-characters/solution.cpp
@@ -1,56 +1,67 @@
 class Solution {
 public:
-    bool check(string s1,string s2){
-        map <char,int> m1,m2;
-        for(auto i : s1){
-            if(m1[i] > 0)return false;
-            m1[i]++;
-        }
-        for(auto i : s2){
-            if(m2[i] > 0)return false;
-            m2[i]++;
-        }
-        for(int i = 0;i<26;i++){
-            //cout << i << " ";
-            if(m1[i + 'a'] > 0 && m2[i + 'a'] > 0 )return false;
-        }
-        return true;
-    }
-    int maxi = 0;
-    void f(int i,vector<string>&a,string s,int size){
-        //if(i >= a.size())return;
-        
-        if(i >= a.size()){
-            //cout << s << " "<< size << endl;
-            maxi = max(size,maxi);
-            return;
-        }
-        
-        for(int ind = i;ind <a.size();ind++){
-            if(check(s,a[ind])== false){
-                maxi = max(maxi,size);
-                continue;
+    void convertToBits(unordered_set<int>& uniqueBits, vector<string>& arr) {
+
+        // This function will convert each string to its corresponding version
+        // in bits form
+        for (auto& i : arr) {
+            int stringToBit = 0;
+            bool flag = true;
+            for (auto& c : i) {
+                int charToBit = (1 << (c - 'a'));
+                // If the string itself has a repeating character then dont take
+                // it in unordered_set
+                if (stringToBit & charToBit) {
+                    flag = false;
+                    continue;
+                }
+                // If no repeating character create its bit mapping
+                stringToBit |= charToBit;
             }
-            //cout << s << " " << a[ind] << endl;
-            s = s + a[ind];
-            //cout << s << " hi ";
-            f(ind+1,a,s,size + a[ind].size());
-            for(int k = 0;k<a[ind].size();k++)s.pop_back();
+            // If we reach here then means that the string is good
+            // Store the length of this string after the 26 characters.
+            if (flag) {
+
+                int stringToBitLen = i.size() << 26;
+                stringToBit |= stringToBitLen;
+                uniqueBits.insert(stringToBit);
+            }
         }
-        
-        
-        
     }
-    
-    int maxLength(vector<string>& a) {
-        int n = a.size();
-        //int maxi = 0;
-        map<char,int> m;
-        string s ;
-        f(0,a,s,0);
-        return maxi;
-        
-        
-        
+
+    int solve(vector<int>& uniqueVec, int i, int result) {
+        // oldResult stores the first 26 characters from result
+        int oldResult = result & ((1 << 26) - 1);
+        int oldResultLen = result >> 26;
+        int best = oldResultLen;
+        if (i >= uniqueVec.size())
+            return best;
+
+        // newResult stores the first 26 characters of the new element.
+        int newResult = uniqueVec[i] & ((1 << 26) - 1);
+        int newResultLen = uniqueVec[i] >> 26;
+
+        // Check if any repetition after merging
+        int mergedResult = (oldResult & newResult);
+
+        if (mergedResult == 0) { // Means there is no repetition.
+            int newValue = oldResult | newResult;
+            int newValueLength = oldResultLen + newResultLen;
+
+            int newValueWithLength = newValue | (newValueLength << 26);
+
+            best = solve(uniqueVec, i + 1, newValueWithLength);
+        }
+        best = max(best, solve(uniqueVec, i + 1, result));
+        return best;
+    }
+
+    int maxLength(vector<string>& arr) {
+        unordered_set<int> uniqueBits;
+        convertToBits(uniqueBits, arr);
+        vector<int> uniqueVec(uniqueBits.begin(), uniqueBits.end());
+
+        // result = 0 initially as our string is empty. with length = 0
+        return solve(uniqueVec, 0, 0);
     }
 };
