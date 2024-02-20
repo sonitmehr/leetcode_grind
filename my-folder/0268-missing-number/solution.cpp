39f5539e0ca9be40f1306355e3fb98650fe44b4a
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-02-20 11:43:43 +0000

    [LeetCode Sync] Runtime - 18 ms (9.01%), Memory - 20.3 MB (99.95%)

--- my-folder/0268-missing-number/solution.cpp
+++ my-folder/0268-missing-number/solution.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        //sort(nums.begin(),nums.end());
+        
         int n = nums.size();
-        int a[10001] = {0};
-        int maxi = 0;
-        
-        for(int i =0;i<n;i++){
-            a[nums[i]]++;
-            maxi = max(maxi,nums[i]);
+        int xori = n;
+        for(int i = 0;i<n;i++){
+            xori = (xori ^ nums[i]);
+            xori = (xori ^ i);
         }
-        for(int i = 0;i <=maxi;i++){
-            if(a[i] == 0)return i;
-        }
-        return maxi + 1;
-        
-        
+        return xori;
     }
-    
 };
