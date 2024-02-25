99494137b68fabbef455a305a89124a5bb82b573
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-02-25 16:05:56 +0000

    [LeetCode Sync] Runtime - 242 ms (20.63%), Memory - 176.9 MB (100.00%)

--- my-folder/1568-pseudo-palindromic-paths-in-a-binary-tree/solution.cpp
+++ my-folder/1568-pseudo-palindromic-paths-in-a-binary-tree/solution.cpp
@@ -6,30 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     int ans = 0;
-    void solve(TreeNode* root, int result) {
-        if (root == NULL)
-            return;
-        int val = root->val;
-        int newFrequency = result ^ (1 << val);
+    void solve(TreeNode* root,int val){
+        if(root == NULL)return;
+        
+        
+        int xorVal = 1 << root->val;
+        
+        val = val ^ xorVal;
 
-        if (root->left == NULL && root->right == NULL) {
-            if ((newFrequency & (newFrequency - 1)) == 0) {
-                ans++;
-            }
+        if(root -> left == NULL && root->right == NULL){
+            int check = val & -val;
+            if(check == 0 || check == val)ans++;
+            
+            return;
         }
-        solve(root->left, newFrequency);
-        solve(root->right, newFrequency);
+        solve(root->left,val);
+        solve(root->right,val);
+
     }
 
-    int pseudoPalindromicPaths(TreeNode* root) {
-        solve(root, 0);
+    int pseudoPalindromicPaths (TreeNode* root) {
+        ans = 0;
+        solve(root,0);
         return ans;
     }
 };
