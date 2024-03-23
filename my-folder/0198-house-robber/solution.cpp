595c03c8a0d10d45bbc1a5bb65c1b1bd2c405242
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-03-23 14:19:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.1 MB (100.00%)

--- my-folder/0198-house-robber/solution.cpp
+++ my-folder/0198-house-robber/solution.cpp
@@ -2,17 +2,29 @@ class Solution {
 public:
     int dp[101];
     int f(int i,vector<int> &a){
-        if(i >= a.size())return 0;
+        if(i < 0)return 0;
         
         if(dp[i] != -1)return dp[i];
-        int pick = a[i] + f(i+2,a);
-        int noPick = f(i+1,a);
+        int pick = a[i] + f(i-2,a);
+        int noPick = f(i-1,a);
 
         return dp[i]=max(pick,noPick); 
     }
 
     int rob(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return f(0,nums);
+        memset(dp,0,sizeof(dp));
+        int n = nums.size();
+        if(n == 1)return nums[0];
+        dp[0] = nums[0];
+        dp[1] = max(dp[0],nums[1]);
+        for(int i = 2;i<n;i++){
+            int pick = nums[i] + dp[i - 2];
+            // if(i-2 >= 0){
+            //     pick += dp[i -2];
+            // }
+            int noPick = dp[i - 1];
+            dp[i] = max(pick,noPick);
+        }
+        return dp[n - 1];
     }
 };
