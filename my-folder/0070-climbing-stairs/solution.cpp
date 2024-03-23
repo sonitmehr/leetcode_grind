83728c99e6f8402f20c62e12143ce5fbfcaf6741
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-03-23 14:03:42 +0000

    [LeetCode Sync] Runtime - 2 ms (5.23%), Memory - 7.2 MB (100.00%)

--- my-folder/0070-climbing-stairs/solution.cpp
+++ my-folder/0070-climbing-stairs/solution.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     int dp[46];
-    int f(int i){
-        if(i  < 0)return 0;
-        if(i == 1) return 1;
-        if(i == 2) return 2;
+    int solve(int i,int n){
+        if(i > n)return 0;
+        if(i == n)return 1;
         if(dp[i] != -1)return dp[i];
-        int one = 0,two = 0;
-        
-        return dp[i] =  f(i - 1) + f(i-2);
-    }
-    int climbStairs(int n) {
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-        if(n == 1 || n == 2)return n;
-        int prev1 = 1,prev2 = 2;
-        int curr = 0;
-        for(int i = 3;i<=n;i++){
-            curr = prev1 + prev2;
-            prev1 = prev2;
-            prev2 = curr;
-        }
-        return curr;
+        int one = solve(i + 1,n);
+        int two = solve(i + 2,n);
 
+        return dp[i] = one + two;
+    }
+
+    int climbStairs(int n) {
         
+
+
+        memset(dp,0,sizeof(dp));
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i= 2;i<=n;i++){
+            
+            int one = dp[i - 1];
+            int two = dp[i - 2];
+
+            dp[i] = one + two;
+        }
+
+        return dp[n];
     }
 };
