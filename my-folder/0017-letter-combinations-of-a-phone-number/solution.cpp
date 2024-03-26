739072573442421db0cc2b22f514aa1726f48190
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-03-26 16:50:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.2 MB (100.00%)

--- my-folder/0017-letter-combinations-of-a-phone-number/solution.cpp
+++ my-folder/0017-letter-combinations-of-a-phone-number/solution.cpp
@@ -1,15 +1,34 @@
 class Solution {
 public:
+
+    void solve(int i,string digits,string currString,vector<string> &ans,vector<string> &keys){
+        if(i > digits.size())return ;
+
+        if(i == digits.size()){
+            ans.push_back(currString);
+            return;
+        }
+
+        int currDigit = digits[i] - '0';
+        
+        for(int k = 0;k<keys[currDigit].size();k ++){
+            currString.push_back(keys[currDigit][k]);
+            // cout << currString << " " << i << " " << keys[currDigit].size() << endl;
+            solve(i + 1,digits,currString,ans,keys);
+            currString.pop_back();
+        }
+
+
+    }
+
     vector<string> letterCombinations(string digits) {
-	if(digits == "") return {};
-	vector<string> mappings{"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}, ans{""};
-	for(auto digit : digits){
-		vector<string> extendCombination;
-		for(auto& currentCombination : ans)
-			for(auto newChar : mappings[digit - '2'])
-				extendCombination.push_back(currentCombination + newChar);                            
-		ans = move(extendCombination); // same as ans = extendCombination, just avoids copying each value. You Can also use swap(ans,extendCombination)
-	}
-	return ans;
-}
+        if(digits.size () == 0)return {};
+        vector<string> keys = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        vector<string> ans;
+        solve(0,digits,"",ans,keys);
+        return ans;
+        
+
+
+    }
 };
