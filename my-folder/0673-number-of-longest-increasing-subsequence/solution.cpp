b12af8e2e6f25c3a7205368220c011fb8a727502
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-03-29 03:44:42 +0000

    [LeetCode Sync] Runtime - 33 ms (43.20%), Memory - 15.7 MB (100.00%)

---
+++ my-folder/0673-number-of-longest-increasing-subsequence/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n,1);
+        vector<int> cnt(n,1);
+        int maxi = 0;
+        for(int i = 0;i<n;i++){
+            for(int prev = 0;prev < i;prev++){
+                
+                if(nums[i] > nums[prev] && 1 + dp[prev] > dp[i]){
+                    dp[i] = 1 + dp[prev];
+                    cnt[i] = cnt[prev];
+                }
+                else if(nums[i] > nums[prev] && 1 + dp[prev] == dp[i]){
+                    
+                    cnt[i] += cnt[prev];
+
+                }
+            }
+                maxi = max(maxi,dp[i]);
+        }
+
+        int total = 0;
+        for(int i = 0;i<n;i++){
+            if(dp[i] == maxi)total+= cnt[i];
+        }
+        return total;
+    }
+};
