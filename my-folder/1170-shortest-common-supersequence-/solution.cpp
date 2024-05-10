e5f8949eccb223cf77d079f9ad0ce6b04d560a70
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-05-10 02:40:23 +0000

    [LeetCode Sync] Runtime - 23 ms (88.57%), Memory - 25.6 MB (96.05%)

--- my-folder/1170-shortest-common-supersequence-/solution.cpp
+++ my-folder/1170-shortest-common-supersequence-/solution.cpp
@@ -1,50 +1,57 @@
 class Solution {
 public:
-    int dp[1001][1001];
     string shortestCommonSupersequence(string s1, string s2) {
-        int n1 = s1.size(),n2 = s2.size();
+        int n = s1.size(),m  =s2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
 
-        for(int i = 1;i<=n1;i++){
-            for(int j = 1;j<=n2;j++){
-                if(s1[i - 1] == s2[j - 1])dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 0;
+        for (int i = 0; i <= m; i++)
+            dp[0][i] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+                }
             }
         }
-        // for(int i = 0;i<=n1;i++){
-        //     for(int j = 0;j<=n2;j++){
-        //         cout << dp[i][j] << " ";
-        //     }
-        //     cout << endl;
-        // }
-        int i = n1,j = n2;
         string ans;
-        while(i > 0 && j > 0){
-            if(s1[i -1] == s2[j - 1]){
-                ans = s1[i-1] + ans;
+        int len = s1.size() + s2.size() - dp[n][m];
+        for (int i = 0; i < len; i++)
+            ans += '$';
+        int i = n, j = m;
+        int index = len - 1;
+        while (i > 0 && j > 0) {
+            if (s1[i - 1] == s2[j - 1]) {
+                ans[index] = s1[i - 1];
+                index--;
                 i--;
                 j--;
-            }
-            else if(dp[i-1][j] > dp[i][j-1]){
-                ans = s1[i-1] + ans;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                ans[index] = s1[i - 1];
+                index--;
                 i--;
-                
-            }
-            else{
-                ans =s2[j - 1] + ans;
+            } else
+            {
+                ans[index] = s2[j - 1];
+                index--;
                 j--;
             }
-
-
         }
         while(i > 0){
-            ans =s1[i -1] + ans;
+            ans[index] = s1[i - 1];
+            index--;
             i--;
         }
         while(j > 0){
-            ans = s2[j -1 ] + ans;
+            ans[index] = s2[j - 1];
+            index--;
             j--;
         }
-        
         return ans;
     }
 };
