76a53d03e3e7add44978f1258f0bbfa32bffb00a
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-05 14:07:29 +0000

    [LeetCode Sync] Runtime - 91 ms (76.85%), Memory - 13.7 MB (83.22%)

--- my-folder/0416-partition-equal-subset-sum/solution.cpp
+++ my-folder/0416-partition-equal-subset-sum/solution.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
-    int dp[201][20001];
-    bool f(int i ,int k,vector<int>&a){
-        if(k == 0)return true;
-        if(i >= a.size())return false;
-        if(k < 0)return false;
-        
-        
-        if(dp[i][k] != -1)return dp[i][k];
-        bool one = false;
-        if(k - a[i] >= 0)one = f(i + 1,k - a[i],a);
-        return dp[i][k] = one | f(i + 1,k,a);
-
-    }
-    bool canPartition(vector<int>& nums) {
-        int n = nums.size();
+    bool canPartition(vector<int>& arr) {
+        int n = arr.size();
         int sum = 0;
-        for(int i = 0;i<n;i++)sum += nums[i];
-        if(sum%2 == 1)return false;
-        memset(dp,-1,sizeof(dp));
+        for(int i =0;i<n;i++){
+            sum +=arr[i];
+        }
+        if(sum % 2== 1)return false;
+        sum/=2;
+        vector<int> prev(sum + 1,0);
+        vector<int> curr(sum + 1,0);
+        prev[0] = 1;
+        if(arr[0] <= sum){
+            
+        prev[arr[0]] = 1;
+        }
 
-        return f(0,sum/2,nums);
+        for(int i = 1;i<n;i++){
+            for(int k = 0;k<=sum;k++){
+                bool one = prev[k];
+                bool two = false;
+                if(k - arr[i] >= 0){
+                    two = prev[k - arr[i]];
+                }
+                curr[k] = one | two;
+            }
+                prev = curr;
+        }
+        
+        return prev[sum];
     }
 };
