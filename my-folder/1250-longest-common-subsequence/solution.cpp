cccf7acf068e6ac315cdc191fe1da33c3c417d8c
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-06 14:45:21 +0000

    [LeetCode Sync] Runtime - 29 ms (39.92%), Memory - 25.6 MB (82.17%)

--- my-folder/1250-longest-common-subsequence/solution.cpp
+++ my-folder/1250-longest-common-subsequence/solution.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    string s1,s2;
-    int f(int i,int j){
-        if(i <0 || j < 0)return 0;
 
+    int solve(int i,int j,string &s1,string &s2,vector<vector<int>> &dp){
+        if(i < 0 || j < 0)return 0;
+
+        if(i == 0 && j == 0){
+            return s1[i] == s2[j];
+        }
         if(dp[i][j] != -1)return dp[i][j];
         int one = 0;
-        if(s1[i] == s2[j])one = 1 + f(i - 1,j - 1);
-        return dp[i][j] = max({one,f(i - 1,j),f(i,j- 1)});
+
+        if(s1[i] == s2[j]){
+            one = 1 + solve(i - 1,j - 1,s1,s2,dp);
+        }
+        int two = solve(i-1,j,s1,s2,dp);
+        int three = solve(i,j - 1,s1,s2,dp);
+
+        return dp[i][j] = max({one ,two,three});
     }
 
-    int longestCommonSubsequence(string text1, string text2) {
-        s1 = text1;
-        s2 = text2;
+    int longestCommonSubsequence(string s1, string s2) {
         int n1 = s1.size(),n2 = s2.size();
-        // for(int i =0;i<n2;i++)dp[i][0] = 0;
-        // for(int i = 0;i<n1;i++)dp[0][i] = 0;
-        // for(int i= 0;i<=n1;i++){
-        //     for(int j= 0;j<=n2;j++){
-        //         cout << dp[i][j] << " ";
-        //     }
-        //     cout << endl;
-        // }
-        
-        for(int i = 1;i<=n1;i++){
-            for(int j = 1;j<=n2;j++){
-                if(s1[i - 1] == s2[j - 1])dp[i][j] = 1 + dp[i - 1][j - 1];
-                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        vector<vector<int>> dp(n1 + 1,vector<int> (n2 + 1));
+
+        for(int i = 0;i<n1;i++)dp[i][0] = 0;
+        for(int i = 0;i<n2;i++)dp[0][i] = 0;
+
+            for(int i= 1;i<=n1;i++){
+                for(int j = 1;j<=n2;j++){
+                    int one = 0;
+                    if(s1[i - 1] == s2[j - 1]) one =  1  + dp[i - 1][j - 1];
+                    int two = max(dp[i - 1][j],dp[i][j - 1]);
+
+                    dp[i][j] = max(one,two);
+                }
+
             }
+
+            return dp[n1][n2];
             
         }
-        // cout << endl;
-        // for(int i= 0;i<=n1;i++){
-        //     for(int j= 0;j<=n2;j++){
-        //         cout << dp[i][j] << " ";
-        //     }
-        //     cout << endl;
-        // }
-        return dp[n1][n2];
-    }
-};
+    };
