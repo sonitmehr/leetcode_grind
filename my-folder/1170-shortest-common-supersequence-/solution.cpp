c370607a916c3173ce619e4eddcdad074a54721b
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-08 07:46:58 +0000

    [LeetCode Sync] Runtime - 23 ms (88.57%), Memory - 25.7 MB (96.05%)

--- my-folder/1170-shortest-common-supersequence-/solution.cpp
+++ my-folder/1170-shortest-common-supersequence-/solution.cpp
@@ -1,57 +1,52 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string s1, string s2) {
-        int n = s1.size(),m  =s2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        int n = s1.size(),m = s2.size();
 
-        for (int i = 0; i <= n; i++)
-            dp[i][0] = 0;
-        for (int i = 0; i <= m; i++)
-            dp[0][i] = 0;
+        vector<vector<int>> dp(n + 1,vector<int>(m + 1, 0));
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
 
-                if (s1[i - 1] == s2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                if(s1[i - 1] == s2[j - 1]){
+                    dp[i][j] = 1 + dp[i -1 ][j - 1]; 
+                }
+                else{
+                    dp[i][j] = max(dp[i - 1][j],dp[i][j - 1]);
                 }
             }
         }
-        string ans;
-        int len = s1.size() + s2.size() - dp[n][m];
-        for (int i = 0; i < len; i++)
-            ans += '$';
-        int i = n, j = m;
-        int index = len - 1;
-        while (i > 0 && j > 0) {
-            if (s1[i - 1] == s2[j - 1]) {
-                ans[index] = s1[i - 1];
-                index--;
-                i--;
-                j--;
-            } else if (dp[i - 1][j] > dp[i][j - 1]) {
-                ans[index] = s1[i - 1];
-                index--;
-                i--;
-            } else
-            {
-                ans[index] = s2[j - 1];
-                index--;
-                j--;
+        
+        int i = n,j = m ;
+string ans;
+        while(i > 0 && j > 0){
+            if(s1[i - 1] == s2[j - 1]){
+                ans += s1[i-1];
+                i --;
+                j --;
             }
+            else{
+                if(dp[i - 1][j] >= dp[i][j - 1]){
+                    ans += s1[i-1];
+                    i--;
+                }
+                else{
+                    ans += s2[j - 1];
+                    j --;
+                }
+            }
+            
         }
         while(i > 0){
-            ans[index] = s1[i - 1];
-            index--;
-            i--;
+            ans += s1[i-1];
+                    i--;
         }
-        while(j > 0){
-            ans[index] = s2[j - 1];
-            index--;
-            j--;
+        while( j > 0){
+            ans += s2[j - 1];
+            j -- ;
         }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
