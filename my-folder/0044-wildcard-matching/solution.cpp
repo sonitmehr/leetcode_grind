d5b3572d4672468b1e17cc81826b298404399639
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-08 13:12:08 +0000

    [LeetCode Sync] Runtime - 22 ms (87.66%), Memory - 31.5 MB (35.93%)

--- my-folder/0044-wildcard-matching/solution.cpp
+++ my-folder/0044-wildcard-matching/solution.cpp
@@ -1,30 +1,68 @@
 class Solution {
 public:
-    int dp[2001][2001];
-    bool solve(int i, int j, string &pattern,string &text){
 
-        if(i < 0 && j < 0)return true;
-        if(i < 0 && j >= 0)return false;
+    bool solve(int i,int j,string &s,string &p,vector<vector<int>> &dp){
 
-        if(i >=0 && j < 0){
-            for(int ii = 0;ii<=i;ii++){
-                if(pattern[ii] != '*')return false;
+        // if()
+        if(i < 0 && j>=0){
+            for(int k = j;k>=0;k--){
+                if(p[k] !='*')return false;
             }
             return true;
         }
+        if(j < 0){
+            if(i < 0)return true;
+            return false;
+        }
         if(dp[i][j] != -1)return dp[i][j];
 
-        if(pattern[i] == text[j] || pattern[i] == '?')return dp[i][j] = solve(i - 1,j - 1,pattern,text);
-
-        if(pattern[i] == '*'){
-            return dp[i][j] = solve(i - 1,j,pattern,text) | solve(i,j - 1,pattern,text);
+        if(s[i] == p[j] || p[j] == '?'){
+            return dp[i][j] = solve(i - 1,j - 1,s,p,dp);
         }
-        return dp[i][j] = false;
+        if(p[j] == '*'){
+            return  dp[i][j] =solve(i - 1,j,s,p,dp) | solve(i,j - 1,s,p,dp);
+        }
+
+        return dp[i][j] =false;
     }
 
-    bool isMatch(string text, string pattern) {
-        int n = pattern.size(),m = text.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(n- 1,m-1,pattern,text);
+    bool isMatch(string s, string p) {
+        int n = s.size(),m = p.size();
+        vector<vector<int>> dp(n + 1,vector<int>(m + 1,0));
+        
+        dp[0][0] = 1;
+
+        int k = 1;
+        while(p[k - 1] == '*' && k <= m){
+            dp[0][k] = 1;
+            k++;
+        }
+
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                if(s[i - 1] == p[j - 1] || p[j - 1] == '?'){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+
+                else{
+                    if(p[j - 1] == '*'){
+                        dp[i][j] = dp[i - 1][j] | dp[i][j - 1];
+                    }
+                    else dp[i][j] = 0;
+
+                }
+            }
+        }
+
+        // for(int i= 0;i<=n;i++){
+        //     for(int j = 0;j<=m;j++){
+        //         cout << dp[i][j] << ' ';
+        //     }
+        //     cout << endl;
+        // }
+        return dp[n][m];
+
+
+
     }
 };
