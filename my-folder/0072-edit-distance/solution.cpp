5bd9d10f597b74b4908d883d00c5f22f48e4cd27
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-08 08:22:57 +0000

    [LeetCode Sync] Runtime - 3 ms (90.36%), Memory - 12 MB (85.06%)

--- my-folder/0072-edit-distance/solution.cpp
+++ my-folder/0072-edit-distance/solution.cpp
@@ -1,21 +1,51 @@
 class Solution {
 public:
-    int dp[501][501];
-    int solve(int i,int j, string &s1,string &s2){
+
+    int solve(int i ,int j,string &s1,string &s2,vector<vector<int>> &dp){
         
-        if(i < 0)return j + 1;
-        if(j < 0)return i + 1;
+
+        if(i < 0){
+            return j + 1;
+        }
+        if(j < 0){
+            return i + 1;
+        }
 
         if(dp[i][j] != -1)return dp[i][j];
-        if(s1[i] == s2[j]) return dp[i][j] = solve(i - 1,j - 1,s1,s2);
+        int one = 1e9;
 
-        return dp[i][j] = 1 + min(solve(i,j - 1,s1,s2),min(solve(i - 1,j,s1,s2),solve(i - 1,j - 1,s1,s2)));
+        if(s1[i] == s2[j]){
+            one = solve(i - 1,j - 1,s1,s2,dp);
+        }
+
+        int two = 1 +  solve(i,j -1,s1,s2,dp);
+        int three = 1 + solve(i - 1,j,s1,s2,dp);
+        int four = 1 + solve(i - 1,j - 1,s1,s2,dp);
+        
+        return dp[i][j] = min({one,two,three,four});
 
     }
 
-    int minDistance(string word1, string word2) {
-        int n = word1.size(),m = word2.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(n - 1,m - 1,word1,word2);
+    int minDistance(string s1, string s2) {
+        int n = s1.size(),m = s2.size();
+        vector<vector<int>> dp(n +1,vector<int> (m + 1,0));
+        
+        for(int i = 0;i<=n;i++)dp[i][0] = i;
+        for(int i = 0;i<=m;i++)dp[0][i] = i;
+
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                
+                if(s1[i - 1] == s2[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else{
+                    dp[i][j] = 1 + min({dp[i - 1][j],dp[i][j - 1],dp[i - 1][j - 1]});
+                }
+
+            }
+        }
+
+        return dp[n][m];
     }
 };
