d305ab385c8276209002df7345493434fa2ed9b3
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-11 12:10:53 +0000

    [LeetCode Sync] Runtime - 14 ms (85.28%), Memory - 19.8 MB (78.30%)

--- my-folder/0115-distinct-subsequences/solution.cpp
+++ my-folder/0115-distinct-subsequences/solution.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
-    string s1,s2;
 
-    int dp[1001][1001];
-    int f(int i,int j){ 
+    int solve(int i,int j,string &s,string &t,vector<vector<int>> &dp){
+
+        // if(i < 0 && j < 0)return 1;
         if(j < 0)return 1;
-        if( i < 0)return 0;
-        
-        if(dp[i][j] != -1)return dp[i][j];
-        if(s1[i] == s2[j]){
-            return dp[i][j] =f(i-1,j-1) + f(i-1,j);
+        if(i < 0)return 0;
+
+        if( dp[i][j] != - 1)return dp[i][j];    
+        int one = 0,two = 0;
+        if(s[i] == t[j]){
+            one += (solve(i - 1,j - 1,s,t,dp) + solve(i -1,j,s,t,dp));
         }
-        return dp[i][j] = f(i - 1,j);
+        else{
+            two += solve(i - 1,j,s,t,dp);
+        }
+        return dp[i][j] = one + two;
     }
+
     int numDistinct(string s, string t) {
-        s1 = s,s2 = t;
-        int n1 = s1.size(),n2 = s2.size();
-        memset(dp,-1,sizeof(dp));
-        return f(n1-1,n2-1);
+        int n = s.size(),m =t.size();
+        vector<vector<int>> dp(n + 1,vector<int> (m + 1,0));
+        cout << s.size() << endl;
+        for(int i= 0;i<=n;i++)dp[i][0] = 1;
+        int M = 1e9 + 7;
+        for(int i = 1;i<=n;i++){
+            for(int j= 1;j<=m;j++){
+                if(s[i - 1] == t[j - 1]){
+                    dp[i][j] =( dp[i - 1][j - 1] + dp[i - 1][j]) % M;
+                }
+                else{
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
