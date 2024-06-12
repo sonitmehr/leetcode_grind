7207ccd1b9cbc74094e10bcd7b5710ed8a0faf15
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-12 14:22:43 +0000

    [LeetCode Sync] Runtime - 50 ms (54.07%), Memory - 16.9 MB (100.00%)

--- my-folder/1129-longest-string-chain/solution.cpp
+++ my-folder/1129-longest-string-chain/solution.cpp
@@ -1,39 +1,47 @@
 class Solution {
 public:
-    bool check(string &s1,string &s2){
-        if(s1.size() != s2.size() +1)return false;
 
+    bool compare (string &s1,string &s2){
         int i = 0,j = 0;
-        while(i < s1.size() ){
-            if(j< s2.size() && s1[i] == s2[j]){
-                i++;
-                j++;
+        if(s1.size() != s2.size() + 1)return false;
+        while(i < s1.size()){
+            if(s1[i] == s2[j]){
+                i ++ ;
+                j ++ ;
             }
             else{
-                i++;
+                i ++;
             }
         }
         if(i == s1.size() && j == s2.size())return true;
         return false;
 
+    }
 
-    }
-    static bool comp(string &s1,string &s2){
-        return s1.size() < s2.size();
-    }
-    int longestStrChain(vector<string>& v) {
-        int n = v.size();
-        sort(v.begin(),v.end(),comp);
-        vector<int> dp(n + 1,1);
-        int maxi = 0;
-        for(int i = 0;i<n;i++){
-            for(int prev = 0;prev< i;prev++){
-                if(check(v[i],v[prev]) && dp[i] < dp[prev] + 1){
-                    dp[i] = dp[prev] + 1;
-                }
-            }
-            maxi = max(dp[i],maxi);
+    static bool cmp  (string &s1,string &s2){
+           return s1.size() < s2.size();
         }
-        return maxi;
+
+    int longestStrChain(vector<string>& words) {
+        int n = words.size();
+        sort(words.begin(),words.end(),cmp);
+        vector<int> dp (n,1);
+        // for(auto &i : words)cout << i << " ";
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<i;j++){
+                if(compare(words[i],words[j]) && dp[j] + 1 > dp[i]){
+                    dp[i] = dp[j] + 1;
+                    // cout << i << " " << j << dp[i] << endl;
+                 }
+            }
+            if(ans < dp[i]){
+                ans = dp[i];
+
+            }
+            // cout << ans << endl;
+        }
+
+        return ans;
     }
 };
