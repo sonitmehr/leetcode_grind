254bdd065470e76886b196adbf4a64ddacc1418f
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-13 20:19:52 +0000

    [LeetCode Sync] Runtime - 14 ms (2.16%), Memory - 16.9 MB (100.00%)

--- my-folder/0733-flood-fill/solution.cpp
+++ my-folder/0733-flood-fill/solution.cpp
@@ -1,41 +1,41 @@
+#define pi pair<int,int>
 class Solution {
 public:
 
-    bool check(int x,int y,int n,int m){
-        if( x >= n || y >= m || x < 0 || y< 0)return false;
+    bool check( int i,int j,int n,int m){
+        if(i < 0 || j < 0 || i >= n || j >= m)return false;
         return true;
     }
-
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n = image.size(),m = image[0].size();
-
-        queue<pair<int,int>> q;
-        if(color == image[sr][sc])return image;
-        q.push({sr,sc});
-        int initialColor = image[sr][sc];
-        vector<int> dx = {1,-1,0,0};
+        queue<pi> q;if(color == image[sr][sc])return image;
+        vector<int> dx = {-1,1,0,0};
         vector<int> dy = {0,0,1,-1};
-
+        int initialColor = image[sr][sc];
+        q.push({sr,sc});
+        image[sr][sc] = color;
         while(!q.empty()){
-            auto curr = q.front();
+            auto top = q.front();
             q.pop();
 
-            int currX = curr.first;
-            int currY = curr.second;
-            int currColor = image[currX][currY];
-            image[currX][currY] = color;
+            int currX = top.first;
+            int currY = top.second;
 
-            for(int k = 0;k<4;k++){
+           
+
+                // cout << currX << " : " << currY << endl;
+            for(int k = 0;k<=3;k++){
                 int newX = currX + dx[k];
                 int newY = currY + dy[k];
                 if(check(newX,newY,n,m) && image[newX][newY] == initialColor){
+                // cout << newX << " " << newY << endl;
                     q.push({newX,newY});
                     image[newX][newY] = color;
                 }
-
-            }   
+            }
 
         }
         return image;
+
     }
 };
