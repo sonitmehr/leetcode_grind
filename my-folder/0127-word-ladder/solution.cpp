067cac97c658aa1392ada48a239c192f1d7539d9
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-14 20:39:49 +0000

    [LeetCode Sync] Runtime - 60 ms (57.44%), Memory - 19.5 MB (96.01%)

--- my-folder/0127-word-ladder/solution.cpp
+++ my-folder/0127-word-ladder/solution.cpp
@@ -1,38 +1,42 @@
+#define pi pair<string,int>
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
+        queue<pi> q;
         unordered_set<string> st;
-        for(auto i : wordList)st.insert(i);
-        queue<pair<string,int>> q;
+        for(auto &i : wordList)st.insert(i);
         q.push({beginWord,1});
         int mini = 1e9;
         while(!q.empty()){
+
             auto p = q.front();
             q.pop();
+
             string currWord = p.first;
             int length = p.second;
-            if(currWord == endWord)mini =min(mini,length);
+
+            if(currWord == endWord)mini = min(mini,length);
+
             for(int k = 0;k<currWord.size();k++){
                 char ignore = currWord[k];
-                char store = currWord[k];
-                for(char c = 'a';c <= 'z';c++){
+
+                for(char c = 'a';c<='z';c++){
                     if(c != ignore){
-                        
                         currWord[k] = c;
                         if(st.find(currWord) != st.end()){
-                            q.push({currWord,length + 1});
+                            q.push({currWord,length+1});
                             st.erase(currWord);
                         }
-                        
                     }
+                    currWord[k] = ignore;
                 }
-                currWord[k] = store;
             }
-            
+
         }
-        if(mini == 1e9)return 0;
-        return mini;
+            return mini == 1e9 ? 0 : mini;
         
+
 
     }
 };
