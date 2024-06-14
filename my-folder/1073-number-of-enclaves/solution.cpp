1d6c3e78a9ed22af7f3719f7f04a784b94f8ec43
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-14 14:02:11 +0000

    [LeetCode Sync] Runtime - 61 ms (11.45%), Memory - 37.4 MB (29.69%)

--- my-folder/1073-number-of-enclaves/solution.cpp
+++ my-folder/1073-number-of-enclaves/solution.cpp
@@ -1,68 +1,61 @@
 class Solution {
 public:
-bool valid(int i,int j,int n,int m){
-        if(i < 0 || j < 0 || i >= n || j >= m)return false;
+int n,m;
+    bool check(int i,int j){
+        if(i < 0 || j <0 || i >=n || j >=m)return false;
         return true;
     }
-    bool border(int i,int j,int n,int m){
-        if(i == n - 1 || j == m - 1 || i == 0 || j == 0)return true;
-        return false;
-    }
-    int numEnclaves(vector<vector<int>>& mat) {
-        int n = mat.size(),m = mat[0].size();
-        queue<pair<int,int>> q;
-        vector<vector<int>> vis(n+1,vector<int>(m+1,0));
-        
-        // for(int i = 0;i<n;i++){
-        //     if(mat[i][0] == 'O')q.push({i,0});
-        //     if(mat[i][m - 1] == 'O')q.push({i,m - 1});
+vector<int> dx = {-1,1,0,0};
+    vector<int> dy = {0,0,-1,1};
+    int ans = 0;
+    void dfs(int i,int j,vector<vector<int>> &vis,int c,vector<vector<int>>  &mat){
+        // cout << i<< " " << j << endl;
+        vis[i][j] = 1;
 
-        // }
-        // for(int j = 0;j<m;j++){
-        //     if(mat[0][j] == 'O')q.push({0,j});
-        //     if(mat[n - 1][j] == 'O')q.push({n-1,j});
-        // }
-        vector<int> a = {-1,1,0,0},
-        b = {0,0,-1,1};
-        
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(border(i,j,n,m) && mat[i][j] == 1){
-                    q.push({i,j});
-                    //cout << i << " " << j << endl;
-                    while(!q.empty()){
-                        auto p = q.front();
-                        q.pop();
-                        int I = p.first,J = p.second;
-                        if(vis[I][J] == 1)continue;
-                        vis[I][J] = 1;
-                        for(int k = 0;k<=3;k++){
-                            int newI = I + a[k],newJ = J + b[k];
-                            if(valid(newI,newJ,n,m) && mat[newI][newJ] == 1 && vis[newI][newJ] == 0){
-                                
-                                q.push({newI,newJ});   
-                            }
-                        }
-                    }
-                    // for(int i = 0;i<n;i++){
-                    //     for(int j = 0;j<m;j++){
-                    //         cout << vis[i][j] << " ";
-                    //     }
-                    //     cout << endl;
-                    // }
-                }
+        for(int k = 0;k<=3;k++){
+                int newX = i + dx[k];
+                int newY = j + dy[k];
+
+            if(check(newX,newY) && vis[newX][newY] == 0 && mat[newX][newY] == 1){
+                mat[newX][newY] = c;
+                ans ++;
+                dfs(newX,newY,vis,c,mat);
+                // q.push({{newX,newY},distance + 1});
             }
         }
-        int ans= 0;
+    }
+    int numEnclaves(vector<vector<int>>& mat) {
+        n = mat.size(),m = mat[0].size();
+
+        vector<vector<int>> vis(n,vector<int>(m,0));
+
+        for(int i = 0;i<n;i++){
+            if(mat[i][0] == 1){
+                dfs(i,0,vis,1,mat);
+            }
+            if(mat[i][m - 1] == 1){
+                dfs(i,m-1,vis,1,mat);
+            }
+        }
+        for(int i = 0;i<m;i++){
+            if(mat[0][i] == 1){
+                dfs(0,i,vis,1,mat);
+            }
+            if(mat[n - 1][i] == 1){
+                dfs(n - 1,i,vis,1,mat);
+            }
+        }
+        ans = 0;
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
                 if(mat[i][j] == 1 && vis[i][j] == 0){
+                    mat[i][j] = 0;
+                    dfs(i,j,vis,1,mat);
                     ans++;
                 }
-                //cout << vis[i][j] << " ";
             }
-            //cout << endl;
         }
+        
         return ans;
     }
 };
