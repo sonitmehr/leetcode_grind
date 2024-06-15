88e3bfb5e306b54fa43db58a77b2a15a43fb8fa3
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-15 20:30:37 +0000

    [LeetCode Sync] Runtime - 19 ms (8.29%), Memory - 16.5 MB (100.00%)

--- my-folder/0803-cheapest-flights-within-k-stops/solution.cpp
+++ my-folder/0803-cheapest-flights-within-k-stops/solution.cpp
@@ -1,49 +1,55 @@
-#define pii pair<int,pair<int,int>>
+#define pi pair<int,pair<int,int>>
+#define pii pair<int,int>
 class Solution {
 public:
-int vis[101];
-vector<pair<int,int>> adj[101];
-bool valid(int i, int j,int n,int m){
-        if(i < 0 || i >=n || j < 0 || j>=m)return false;
-        return true;
-    }
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
 
-        for(auto i : flights){
-            adj[i[0]].push_back({i[1],i[2]});
+
+bool check(int i,int j,int n,int m){
+    if(i < 0 || j < 0 || i >= n || j >= m)return false;
+    return true;
+}
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
+        vector<pii> adj[n];
+        for(int i = 0;i<flights.size();i++){
+            int a = flights[i][0];
+            int b = flights[i][1];
+            int d = flights[i][2];
+            adj[a].push_back({b,d});
         }
-        vector<int> distArray(n,1e9); 
-        //priority_queue<pii,vector<pii>,greater<pii>> q;
-        queue<pii> q;
-        int mini = 1e9;
-        q.push({0,{src,k}});
-        distArray[src] = 0;
-        //int mini = 1e9;
+        // priority_queue<pi,vector<pi>,greater<pi>> q;
+        queue<pi> q;
+        k++;
+        int ans = 1e9;
+        vector<int> dist(n,1e9);
+        q.push({0,{src,0}});
         while(!q.empty()){
             auto p = q.front();
             q.pop();
 
-            int dist = p.first;
+
+            int dis = p.first;
             int node = p.second.first;
             int currK = p.second.second;
-            //cout << node << " " << dist << " " <<currK <<endl;
-            // if(node == dst && currK >= 0){
-            //     mini = min(mini,dist);
-            // }
-            if(currK < 0)break;
-           
 
-            for(auto i : adj[node]){
-                int addDist = i.second;
+            if(currK <= k && node == dst){
+                ans = min(ans,dis);
+            }
+
+            for(auto &i : adj[node]){
                 int adjNode = i.first;
-                if(currK >= 0 && distArray[adjNode] > (dist + addDist)){
-                    distArray[adjNode] = dist + addDist;
-                    q.push({dist + addDist,{adjNode,currK -1}});
+                int adjDist = i.second;
+                // cout << dis << " " << adjDist << ' ' << adjNode << endl;
+                if(dist[adjNode] > dis + adjDist){
+                    dist[adjNode] = dis + adjDist;
+                    q.push({dis + adjDist,{adjNode,currK + 1}});
                 }
             }
+
+
+
             
         }
-        if(distArray[dst] == 1e9)return -1;
-        return distArray[dst];
+        return ans == 1e9 ? -1 : ans;
     }
 };
