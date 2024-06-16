f105287845831bd08ef93e76719f963668f37db2
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-16 11:36:26 +0000

    [LeetCode Sync] Runtime - 21 ms (5.04%), Memory - 18.1 MB (100.00%)

--- my-folder/0547-number-of-provinces/solution.cpp
+++ my-folder/0547-number-of-provinces/solution.cpp
@@ -1,39 +1,58 @@
-class Solution {
-public:
-
-    void dfs(int i,vector<int> &vis,vector<int> adj[]){
-
-        vis[i] = 1;
-
-        for(auto adjEle : adj[i]){
-            if(vis[adjEle] == 0){
-                dfs(adjEle,vis,adj);
-            }
+class DSU{
+    vector<int> rank,parent;
+    public:
+    
+    DSU(int n){
+        rank.resize(n + 1,0);
+        parent.resize(n + 1,0);
+        for(int i = 0;i<=n;i++)parent[i] = i;
+    }
+    
+    int findPar(int u){
+        if(u == parent[u])return u;
+        
+        return parent[u] = findPar(parent[u]);
+    }
+    
+    void Union(int u,int v){
+        int par_u = findPar(u);
+        int par_v = findPar(v);
+        if(par_u == par_v)return;
+        
+        if(rank[par_u] < rank[par_v]){
+            parent[par_u] = par_v;  
+        }
+        else if(rank[par_v] < rank[par_u]){
+            parent[par_v] = par_u;
+        }
+        else {
+            parent[par_u] = par_v;  
+            rank[par_v] ++;
         }
     }
-
+    
+    
+};
+class Solution {
+public:
     int findCircleNum(vector<vector<int>>& mat) {
         int n = mat.size();
-        vector<int> vis(n + 1,0);
-        vector<int> adj[n + 1];
+        DSU dsu(n);
 
         for(int i = 0;i<n;i++){
-            for(int j = 0;j < n;j++){
+            for(int j = 0;j<n;j++){
                 if(mat[i][j] == 1){
-                    adj[i + 1].push_back(j + 1);
+                    dsu.Union(i,j);
                 }
             }
         }
-        int ans = 0;
-        for(int i = 1;i<=n;i++){
-            if(vis[i] == 0){
-                dfs(i,vis,adj);
-                ans++;
-            }
+        // unordered_set<int> st;
+        int cnt = 0;
+        for(int i = 0;i<n;i++){
+            int par = dsu.findPar(i);
+            if(par == i)cnt++;
         }
-
-        return ans;
-
+        return cnt;
 
     }
 };
