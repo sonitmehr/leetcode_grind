53fcfae44485ca0dfe9e085bd2a47224bd285fa5
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-17 17:06:22 +0000

    [LeetCode Sync] Runtime - 453 ms (5.03%), Memory - 177 MB (70.14%)

--- my-folder/1300-critical-connections-in-a-network/solution.cpp
+++ my-folder/1300-critical-connections-in-a-network/solution.cpp
@@ -1,30 +1,43 @@
 class Solution {
 public:
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        disc = vector<int>(n);
-        low = vector<int>(n);
-        for (auto conn : connections) {
-            edgeMap[conn[0]].push_back(conn[1]);
-            edgeMap[conn[1]].push_back(conn[0]);
+    vector<vector<int>> ans;
+    void dfs(int i, int parent,int stp,vector<int>&vis,vector<int>&step,vector<int> &low,vector<int> adj[]){
+
+        vis[i] = 1;
+        step[i] = stp;
+        low[i] = stp;
+        for(auto &adjNode : adj[i]){
+            if(adjNode == parent)continue;
+            if(vis[adjNode] == 0){
+                dfs(adjNode,i,stp + 1,vis,step,low,adj);
+                low[i] = min(low[i],low[adjNode]);
+                if(low[adjNode] > step[i]){
+                    ans.push_back({i,adjNode});
+                }
+
+            }
+            else{
+                int adjLow = low[adjNode];
+                low[i] = min(low[i],adjLow);
+            }
         }
-        dfs(0, -1);
+    }
+
+
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& edges) {
+        vector<int> adj[n];
+        ans.clear();
+        for(int i = 0;i<edges.size();i++){
+            int a = edges[i][0];
+            int b = edges[i][1];
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }
+        vector<int> step(n);
+        vector<int> low(n);
+        vector<int> vis(n,0);
+
+        dfs(0,-1,0,vis,step,low,adj);
         return ans;
     }
-    void dfs(int curr, int prev) {
-        disc[curr] = low[curr] = time++;
-        for (int next : edgeMap[curr]) {
-            if (disc[next] == 0) {
-                dfs(next, curr);
-                low[curr] = min(low[curr], low[next]);
-            } else if (next != prev)
-                low[curr] = min(low[curr], disc[next]);
-            if (low[next] > disc[curr]) 
-                ans.push_back({curr, next});
-        }
-    }
-private:
-    vector<int> disc{0}, low{0};
-    int time = 1;
-    vector<vector<int>> ans;
-    unordered_map<int, vector<int>> edgeMap;
 };
