68c8d97f1cd9ceb30c7846bcfd48fad383162a55
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-18 13:34:55 +0000

    [LeetCode Sync] Runtime - 23 ms (55.09%), Memory - 16.1 MB (100.00%)

--- my-folder/0018-4sum/solution.cpp
+++ my-folder/0018-4sum/solution.cpp
@@ -1,54 +1,44 @@
 class Solution {
 public:
-    vector<pair<int,int>> twoSum(vector<int>& v, long long target,int l) {
-        
-        int n = v.size();
-        vector<pair<int,int>> p;
-        int i =l, r = n-1;
-        while(l < r){
-            if(v[l] + v[r] == target){
-                p.push_back({l,r});
-                
-                int L = v[l],R = v[r];
-                
-                while(l < r && v[l] == L){   
-                        l++;
-                }
-                while(l < r && v[r] == R){
-                        r--;
-                }
-                //l++;r--;
-            
+    vector<vector<int>> twoSum(vector<int> &nums,long long target,int l){
+        int n = nums.size();
+        int i = l,j = n - 1;
+        vector<vector<int>> ans;
+        while(i < j){
+            long long sum = nums[i] + nums[j];
+
+            if(sum == target){
+                ans.push_back({i,j});
+                int left = nums[i],right = nums[j];
+                while(i < j && nums[i] == left)i++;
+                while(i < j && nums[j] == right)j--;
+
             }
-            else if(v[l] + v[r] < target)l++;
-            else r--;
+            else if(sum < target){
+                i++;
+            }
+            else j --;
+
         }
-        return p; 
-            
+        return ans;
     }
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        
         int n = nums.size();
-        // int curr = -1e9 - 1;
-        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-        for(int i =0;i<n;i++){
-            if(i > 0 && nums[i] == nums[i - 1])continue;
-            for(int j= i + 1;j < n;j ++) {
-                if(j != (i + 1) && nums[j] == nums[j - 1])continue;
-                long long sum = nums[i] + nums[j];
+        sort(nums.begin(),nums.end());
+        for(int i= 0;i<n - 1;i++){
+            if(i > 0 && nums[i] == nums[i -1 ])continue;
+            for(int j = i + 1;j<n;j++){
+                if(j > i + 1 && nums[j] == nums[j -1 ])continue;
 
-                long long tar = target - sum;
-                auto p = twoSum(nums,tar,j + 1);
-
+                long long rem = (long long)target - nums[i] - nums[j];
+                auto p = twoSum(nums,rem,j + 1);
+                
                 for(auto &k : p){
-                    ans.push_back({nums[i],nums[j],nums[k.first],nums[k.second]});
-                }
+                    ans.push_back({nums[i],nums[j],nums[k[0]],nums[k[1]]});
+                }  
             }
         }
         return ans;
-        // [1,0,-1,0,-2,2]
-        // [-2,-1,0,0,1,2]
-
     }
 };
