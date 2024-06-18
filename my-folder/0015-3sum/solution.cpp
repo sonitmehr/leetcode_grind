dc86211fd0fc82d39c31cd7d23da291df643e0dd
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-18 13:23:45 +0000

    [LeetCode Sync] Runtime - 56 ms (29.11%), Memory - 33.4 MB (16.92%)

--- my-folder/0015-3sum/solution.cpp
+++ my-folder/0015-3sum/solution.cpp
@@ -1,49 +1,46 @@
 class Solution {
 public:
 
-    vector<pair<int,int>> twoSum(vector<int> &v,int target,int l){
-        int n = v.size();
+    vector<vector<int>> twoSum(vector<int> &nums,int target,int l){
+        int n = nums.size();
         int i = l,j = n - 1;
-        vector<pair<int,int>> p;
+        vector<vector<int>> ans;
         while(i < j){
-            int sum = v[i] + v[j];
-            if(sum == target){
-                p.push_back({i,j});
-                int L = v[i],R = v[j];
-                while(i < j && v[i] == L)i++;
-                while(i < j && v[j] == R)j--;
-            }
-            else if(sum < target)i++;
-            else j--;
-        }
-        return p;
+            int sum = nums[i] + nums[j];
 
+            if(sum == target){
+                ans.push_back({i,j});
+                int left = nums[i],right = nums[j];
+                while(i < j && nums[i] == left)i++;
+                while(i < j && nums[j] == right)j--;
+
+            }
+            else if(sum < target){
+                i++;
+            }
+            else j --;
+
+        }
+        return ans;
     }
 
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
+        int n = nums.size();
         sort(nums.begin(),nums.end());
-        int curr = -1e6;
-         int n = nums.size();
+        int sum = 0;
+        //[-1,0,1,2,-1,-4]
+        //[-4,-1,-1,0,1,2]
         vector<vector<int>> ans;
         for(int i= 0;i<n;i++){
-            if(curr == nums[i])continue;
-            curr = nums[i];
+            if(i > 0 && nums[i] == nums[i - 1])continue;
+            int curr = nums[i];
 
-            auto p = twoSum(nums,-curr,i+1);
-
-            for(int j = 0;j<p.size();j++){
-                auto pp = p[j];
-                ans.push_back({curr,nums[pp.first],nums[pp.second]});
+            auto vec = twoSum(nums,-curr,i + 1);
+            for(auto &i : vec){
+                ans.push_back({curr,nums[i[0]],nums[i[1]]});
             }
-
-            
         }
+
         return ans;
-
-
-
-
-
     }
 };
