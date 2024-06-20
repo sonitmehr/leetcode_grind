8d3be53fbebba5ebaef130bb652fe0fd995013e3
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-20 13:04:17 +0000

    [LeetCode Sync] Runtime - 91 ms (5.19%), Memory - 70.3 MB (99.98%)

--- my-folder/0053-maximum-subarray/solution.cpp
+++ my-folder/0053-maximum-subarray/solution.cpp
@@ -1,35 +1,15 @@
 class Solution {
 public:
-
-    int crossingSum(vector<int> &v,int l,int m,int r){
-        int left_sum = -1e9;
-        int sum = 0;
-        for(int i = m;i>=l;i--){
-            sum += v[i];
-            left_sum = max(sum,left_sum);
-        }
-        sum = 0;
-        int right_sum = -1e9;
-        for(int i = m;i<=r;i++){
-            sum += v[i];
-            right_sum = max(sum,right_sum);
-        }
-        return max({left_sum,right_sum,left_sum + right_sum - v[m]});
-    }
-
-    int f(vector<int> &v,int l,int r){
-        if(r < l) return -1e9;
-        if(r == l) return v[l];
-
-        int m = (l + r)/2;
-
-        return max({f(v,l,m-1),f(v,m + 1,r),crossingSum(v,l,m,r)});
-
-
-    }
-
-
     int maxSubArray(vector<int>& nums) {
-        return f(nums,0,nums.size()-1);
+        int n = nums.size();
+        int sum = 0;
+        int maxi = INT_MIN;
+        for(int i = 0;i<n;i++){
+            sum += nums[i];
+            maxi = max(maxi,sum);
+            if(sum < 0)sum = 0;
+        }
+        return maxi;
+
     }
 };
