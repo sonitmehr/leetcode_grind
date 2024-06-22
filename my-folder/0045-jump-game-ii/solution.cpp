c7d8470433e2f144d115c1384fdf0c863ed272b9
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-22 12:23:20 +0000

    [LeetCode Sync] Runtime - 62 ms (22.60%), Memory - 19.4 MB (100.00%)

--- my-folder/0045-jump-game-ii/solution.cpp
+++ my-folder/0045-jump-game-ii/solution.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-    int dp[10001];
-    int f(int i,vector<int>&a){
-        if(i >= a.size() - 1)return 0;
-        int mini = 1e9,one = 1e9;
-        if(dp[i] != -1)return dp[i];
-        for(int ind = 1;ind<=a[i];ind++){
-            mini = min(mini,1 + f(i + ind,a));
-            //mini = min(mini,one);
-            //cout << mini << " " << ind << endl;
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n,1e9);
+        dp[0] = 0;
+        
+        for(int i = 1;i<=nums[0];i++){
+            if(i >= n)break;
+            dp[i] = 1;
         }
-        return dp[i] = mini;
-    }
 
-    int jump(vector<int>& a) {
-        memset(dp,-1,sizeof(dp));
-        return f(0,a);
+        for(int i = 1;i<n;i++){
+            int val = i + nums[i] ;
+            if(val < n){
+                for(int j = i;j<=val;j++){
+
+                    dp[j] = min(1 + dp[i],dp[j]);
+                }
+            }
+            else {
+                dp[n - 1] = min(1 + dp[i],dp[n-1]);
+
+            }
+        }
+        return dp[n -1 ];
     }
 };
