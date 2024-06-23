05dfef9615320f698998d447608fa3b50a28e740
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-23 07:59:21 +0000

    [LeetCode Sync] Runtime - 3 ms (1.60%), Memory - 16 MB (100.00%)

--- my-folder/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ my-folder/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -1,50 +1,42 @@
 class Solution {
 public:
-    
-    void f(vector<int> &v,int target,int& left,int& right,string s){
-        int n = v.size();
-        int l = 0, r = n - 1,mid,curr;
-        while(l <= r){
-            mid = l + (-l+r)/2;
-            
-            curr = v[mid];
-            
-            if(curr == target){
-                
-                if(s == "first"){
-                    left = mid;
-                    r = mid - 1;
-                    
-                }
-                else{
-                    right = mid;
-                    l = mid + 1;
-                }
-                
-            }          
-            
-            else if(curr < target)l = mid+1;
-            else r= mid-1;
-            
-            
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+        int i = 0,j = n - 1;
+        int low = -1,high = -1;
+        
+        while(i <= j){
+            int mid = (i + j)/2;
+
+            if(nums[mid] == target){
+                low = mid;
+                j = mid - 1;
+            }
+            else if(nums[mid] > target){
+                j = mid - 1;
+            }
+            else {
+                i = mid + 1;
+            }
         }
-    }
-    
-    vector<int> searchRange(vector<int>& v, int target) {
-        int n = v.size();
-        int left = -1,right =-1;
-        //int l = 0, r = n - 1,mid,curr,left = -1,right =-1;
-        
-        vector<int> ans;
-        
-        f(v,target,left,right,"first");
-        f(v,target,left,right,"second");
-        
-        ans.push_back(left);
-        ans.push_back(right);
-        
-        return ans;
-        
-        
+        i = 0,j = n -1;
+        while(i <= j){
+            int mid = (i + j)/2;
+            // cout << mid << " " << nums[mid] << endl;
+            if(nums[mid] == target){
+
+                high = mid;
+                i = mid + 1;
+            }
+            else if(nums[mid] > target){
+                j = mid - 1;
+            }
+            else {
+                i = mid + 1;
+            }
+        }
+        // if(low == -1)high = -1;
+        // else high--;
+        return {low,high};
     }
 };
