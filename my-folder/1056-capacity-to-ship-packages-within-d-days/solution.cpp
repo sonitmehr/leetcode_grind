7bdfdd8b379ad18c6a00ce300b9c00194d0a3253
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-25 12:33:07 +0000

    [LeetCode Sync] Runtime - 40 ms (5.12%), Memory - 33.5 MB (100.00%)

--- my-folder/1056-capacity-to-ship-packages-within-d-days/solution.cpp
+++ my-folder/1056-capacity-to-ship-packages-within-d-days/solution.cpp
@@ -2,29 +2,29 @@ class Solution {
 public:
     int shipWithinDays(vector<int>& v, int days) {
         int n = v.size();
-        int maxi = *max_element(v.begin(),v.end());
-        int low = maxi,high = 0;
-        for(auto i : v)high += i;
-
-        while(low <= high){
-            int mid = (low + high)/2;
-            int currSum = 0;
-            int d = days;
-            for(int i = 0;i < n;i ++){
-
-                if(currSum + v[i] <= mid){
-                    currSum += v[i];
+        int i = *max_element(v.begin(),v.end()),j = 1e9;
+        
+        while(i <= j){
+            int mid = i + (j - i)/2;
+        cout <<i << " " << j << " "  << mid ;
+            int temp = mid;
+            int d = 1;
+            for(int i = 0;i<n;i++){
+                if(temp >= v[i] && v[i] < mid){
+                    
+                    temp -= v[i];
                 }
-                else{
-                    currSum = v[i];
-                    d--;
+                else {
+                    temp = mid - v[i];
+                    d++;
                 }
             }
-            //if(d == 0)return mid;
-            if(d > 0)high = mid - 1;
-            else low = mid + 1;
+            cout <<  " days :" << d <<endl;
+            if(d <= days){
+                j = mid - 1;
+            }
+            else i = mid +1;
         }
-        return low;
-        
+        return i;
     }
 };
