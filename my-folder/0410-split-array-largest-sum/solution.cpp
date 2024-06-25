4fa857ac633f14b164b2c03678afae54e7bf7113
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-25 19:19:44 +0000

    [LeetCode Sync] Runtime - 2 ms (9.55%), Memory - 9.1 MB (100.00%)

--- my-folder/0410-split-array-largest-sum/solution.cpp
+++ my-folder/0410-split-array-largest-sum/solution.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
-    
-    bool f(vector<int>&v,int k,int maxi){
-        int subArray = 0;
-        int n = v.size();
-        int currSum = 0;
-        
-        for(int i = 0;i<n;i++){
-            currSum += v[i];
-            if(currSum > maxi) currSum = v[i], subArray++;
-        }
-        subArray++;
-        return subArray <= k;
-        
-        
-    }
-    
     int splitArray(vector<int>& v, int k) {
-        int l = 0,r = 0;
-        int n =v.size();
-        for(int i= 0;i<n;i++){
-            l = max(l,v[i]);
-            r += v[i];
-        }
-        int mid;
-        int ans = r;
-        while(l<=r){
-            mid = l + (r - l)/2;
-            if(f(v,k,mid)){
-                ans = mid;
-                r = mid - 1;
+        int n = v.size();
+        int sum = 0;
+        for(auto &i : v)sum += i;
+        int i = *max_element(v.begin(),v.end()),j = sum;
+
+        while(i <= j){
+            int mid = (i + j)/2;
+
+            int cnt = 1,currSum = 0;
+            for(int i = 0;i<n;i++){
+                if(currSum + v[i] <= mid){
+                    currSum +=v[i];
+                }
+                else{
+                    currSum = v[i];
+                    cnt++;
+                }
             }
-            else l = mid + 1;
-            
+            cout << mid << " " << cnt << endl;
+
+            if(cnt <= k){
+                j = mid - 1;
+            }
+            else i = mid + 1;
         }
-        return ans;
-        
+
+        return i;
     }
+    
 };
