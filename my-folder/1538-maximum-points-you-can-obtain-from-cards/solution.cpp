1e6d8e09eff7ad597b0515b39b1356ba081a6626
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-06-28 19:37:03 +0000

    [LeetCode Sync] Runtime - 48 ms (3.69%), Memory - 44.8 MB (100.00%)

--- my-folder/1538-maximum-points-you-can-obtain-from-cards/solution.cpp
+++ my-folder/1538-maximum-points-you-can-obtain-from-cards/solution.cpp
@@ -2,16 +2,23 @@ class Solution {
 public:
     int maxScore(vector<int>& v, int k) {
         int n = v.size();
-        if(n==1)return v[0];
-        int sum = 0;
+        int i = 0,j = n -1;
+        int ans = 0;
         
-        for(int i = 0;i<k;i++)sum += v[i];
-        int ptr = k-1,r = n-1,maxi = sum;
-        while(ptr >= 0){
-            sum = sum - v[ptr] + v[r];
-            maxi = max(sum,maxi);
-            ptr--;r--;
+        int lsum = 0,rsum = 0;
+
+        for(int i = 0;i<k;i++){
+            lsum += v[i];
         }
-        return maxi;
+        int ptr = k - 1;
+        ans = lsum;
+        for(int i = n - 1;i >= n - k;i--){
+            rsum += v[i];
+            ans = max(ans,rsum + lsum - v[ptr]);
+            lsum -= v[ptr];
+            ptr--;
+        }
+
+        return ans;
     }
 };
