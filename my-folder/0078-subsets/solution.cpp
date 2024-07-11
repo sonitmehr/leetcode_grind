d0288a3a5af184e99e414d7712ea159b10d26927
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-07-11 19:06:29 +0000

    [LeetCode Sync] Runtime - 2 ms (40.35%), Memory - 8.6 MB (100.00%)

--- my-folder/0078-subsets/solution.cpp
+++ my-folder/0078-subsets/solution.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void f(int i,vector<int> &a,vector<int> &v){
-        if(i > a.size())return;
-        if(i== a.size()){
-           
-            ans.push_back(v);
-            return;
-        }
-        
-        v.push_back(a[i]);
-        f(i+1,a,v);
-        v.pop_back();
-        f(i + 1,a,v);
-    }
+    vector<vector<int>> subsets(vector<int>& v) {
+        int n = v.size();
 
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> v;
-        f(0,nums,v);
+        vector<vector<int>> ans;
+        for(int i = 0;i<(1 << n);i++){
+                vector<int> vec;
+            for(int j = 0;j<n;j++){
+                int set = (i & (1 << j));
+                // cout << set << " "<< i << " " << j << endl;
+                if(set){
+                    vec.push_back(v[j]);
+                }
+            }
+            ans.push_back(vec);
+        }
         return ans;
     }
 };
