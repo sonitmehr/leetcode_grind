215ac93f5083c864a9c22388c8de88c204e48b7f
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-07-11 19:00:58 +0000

    [LeetCode Sync] Runtime - 2 ms (5.38%), Memory - 7.5 MB (99.92%)

--- my-folder/0231-power-of-two/solution.cpp
+++ my-folder/0231-power-of-two/solution.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        long x = n;
+        
         if(n == 0)return false;
-        if(n == 1)return true;
-        // Removing the last set bit.
-        // (x & -x) will have the last set bit from left.
-        // x - (x & -x) is us removing this last set bit 
-        return x - (x & -(x - 1)) == 0;
+        if(n == INT_MIN) return false;
+        return n - (n & -n) == 0;
     }
 };
