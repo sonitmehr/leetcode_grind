e1440ec3aeded5cd55f9ccacbd9ec1f421513999
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-07-12 08:33:06 +0000

    [LeetCode Sync] Runtime - 4 ms (13.99%), Memory - 15.3 MB (99.92%)

--- my-folder/0102-binary-tree-level-order-traversal/solution.cpp
+++ my-folder/0102-binary-tree-level-order-traversal/solution.cpp
@@ -6,47 +6,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    
-    void print(vector<int> &v){
-        for(int i = 0;i<v.size();i++)cout << v[i] << " ";
-        cout << endl;
-    }
-    void f(TreeNode* root,int height,vector<vector<int>> &ds){
-        if(root == NULL)return;
-        
-        cout << "Height " << height << " ds.size() == " << ds.size() << endl;
-        //print(v);
-        if(height == ds.size()){            
-            ds.push_back({});            
-            //v.clear();
-        }
-        ds[height].push_back(root->val);
-        
-        
-        f(root->left,height+1,ds);
-        f(root->right,height+1,ds);
-        
-        
-    }
-    
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> ds;
-        vector<int> v;
-        
-        if(root == NULL)return {};
-        
-        int height = 0,val = root->val;
-        ds.push_back({});
-        
-        f(root,height,ds);
-        
-        //f(root->right,v,ds,0);
-        
-        return ds;
+        queue<TreeNode*> q;
+        if (root == NULL)
+            return {};
+        q.push(root);
+        vector<vector<int>> ans;
+        while (!q.empty()) {
+
+            vector<int> vec;
+            int sz = q.size();
+            for (int i =0;i<sz;i++) {
+                auto p = q.front();
+                vec.push_back(p->val);
+                if (p->left != NULL) {
+                    q.push(p->left);
+                }
+                if (p->right != NULL) {
+                    q.push(p->right);
+                }
+                q.pop();
+            }
+            ans.push_back(vec);
+        }
+
+        return ans;
     }
 };
