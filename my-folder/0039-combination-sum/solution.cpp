6604524b323d74215ae7cab4be319b628bd8f9d7
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-07-15 18:24:05 +0000

    [LeetCode Sync] Runtime - 8 ms (28.25%), Memory - 12.7 MB (99.99%)

--- my-folder/0039-combination-sum/solution.cpp
+++ my-folder/0039-combination-sum/solution.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    void solve(int i,vector<vector<int>> &ans,vector<int> &vec, vector<int> &candidates,int target){
-        if(i < 0)return;
 
-        if(target == 0){
-            ans.push_back(vec);
-            return ;
+    void solve(int i,int tar,vector<int> &v,vector<int> &a,vector<vector<int>> &ans){
+        if(i == a.size()){
+            
+            
+            return;
+        }
+        if(tar == 0){
+            ans.push_back(v);
+            return;
         }
 
-        if(target - candidates[i] >= 0){
-            vec.push_back(candidates[i]);
-            solve(i,ans,vec,candidates,target - candidates[i]);
-            vec.pop_back();
+        if(tar - a[i] >= 0){
+            v.push_back(a[i]);
+            solve(i,tar-a[i],v,a,ans);
+            v.pop_back();
         }
-            solve(i - 1,ans,vec,candidates,target);
+        solve(i +1,tar,v,a,ans);
     }
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
-        vector<int> vec;
-        int n = candidates.size();
-        solve(n - 1,ans,vec,candidates,target);
+        vector<int> v;
+        solve(0,target,v,candidates,ans);
         return ans;
     }
 };
