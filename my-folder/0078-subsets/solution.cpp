c5ffa1deac80f5b091e2a955caf46edaea6313cf
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-07-15 17:40:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.6 MB (100.00%)

--- my-folder/0078-subsets/solution.cpp
+++ my-folder/0078-subsets/solution.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& v) {
-        int n = v.size();
 
-        vector<vector<int>> ans;
-        for(int i = 0;i<(1 << n);i++){
-                vector<int> vec;
-            for(int j = 0;j<n;j++){
-                int set = (i & (1 << j));
-                // cout << set << " "<< i << " " << j << endl;
-                if(set){
-                    vec.push_back(v[j]);
-                }
-            }
+    void solve(int i,vector<int>& v,vector<int> &vec,vector<vector<int>> &ans){
+        if(i == v.size()){
             ans.push_back(vec);
+            return;
         }
+
+
+        vec.push_back(v[i]);
+        solve(i + 1,v,vec,ans);
+        vec.pop_back();
+        solve(i + 1,v,vec,ans);
+
+    }
+
+    vector<vector<int>> subsets(vector<int>& v) {
+        vector<vector<int>> ans;
+        vector<int> vec;
+
+        solve(0,v,vec,ans);
         return ans;
     }
 };
