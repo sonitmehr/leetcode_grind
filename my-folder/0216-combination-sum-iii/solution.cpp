fba9b306b3c2027e2bfab85bba589c685052ab02
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-07-16 09:41:21 +0000

    [LeetCode Sync] Runtime - 3 ms (15.64%), Memory - 8.2 MB (100.00%)

--- my-folder/0216-combination-sum-iii/solution.cpp
+++ my-folder/0216-combination-sum-iii/solution.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
-    
-    void f(int i,int k,int n,int sum,vector<int> &v,vector<vector<int>> &ans){
-        
-        if(k == 0){
-            if(n == sum){                
-                ans.push_back(v);
-            }
+
+
+    vector<vector<int>> ans;
+
+    void solve(int i,vector<int>&v,int n,int k){
+        if(n < 0 || k < 0)return;
+        if(k == 0 && n == 0){
+            ans.push_back(v);
             return;
         }
-        if(i > 9)return ;
-        
-        v.push_back(i);
-        f(i+1,k-1,n,sum + i,v,ans);
-        v.pop_back();
-        f(i+1,k,n,sum,v,ans);
-        
-        /*
-        else if(i == a.size()){
-            return;
-        }
-        
-        
-        if(a[i] < n){
-            v.push_back(a[i]);
-            f(i+1,k-1,n-a[i],a,v,ans);
+
+        for(int ind = i;ind<=9;ind ++){
+            v.push_back(ind);
+            solve(ind + 1,v,n - ind,k-1);
             v.pop_back();
         }
-        f(i+1,k,n,a,v,ans);
-        */
+
         
     }
-    
+
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> ans;
         vector<int> v;
-        vector<int> a = {1,2,3,4,5,6,7,8,9};
-        f(1,k,n,0,v,ans);
+        solve(1,v,n,k);
         return ans;
     }
 };
