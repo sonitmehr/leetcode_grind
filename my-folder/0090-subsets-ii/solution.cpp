981a9903339ccd10cfac2189bb88473198f47104
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-07-16 09:30:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.1 MB (100.00%)

--- my-folder/0090-subsets-ii/solution.cpp
+++ my-folder/0090-subsets-ii/solution.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void f(int i,vector<int>&a,vector<int>&v){
-        //if(i > a.size())return;
-        
-        
-        ans.push_back(v);
-           
-            
-        
+    void solve(int i,vector<int> &v,vector<int> &a){
 
-        for(int ind = i;ind< a.size();ind++){
+        ans.push_back(v);
+
+        for(int ind = i;ind<a.size();ind++){
             if(ind > i && a[ind] == a[ind - 1])continue;
             v.push_back(a[ind]);
-            f(ind+1,a,v);
+            solve(ind + 1,v,a);
             v.pop_back();
-
         }
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<int> v;
-        sort(nums.begin(),nums.end());
-        f(0,nums,v);
+        sort(nums.begin(), nums.end());
+        solve(0,v,nums);
+
         return ans;
     }
 };
