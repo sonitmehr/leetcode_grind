bc03d157a141559afa56e8374bb49b1af7e60181
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-07-19 06:05:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.8 MB (100.00%)

--- my-folder/0120-triangle/solution.cpp
+++ my-folder/0120-triangle/solution.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-    int dp[201][201];
-    int f(int i,int j,vector<vector<int>>&a){
-        if(i >= a.size() || j >= a[i].size())return 1e9;
 
-        if(i == a.size() -1)return a[i][j];
-        if(dp[i][j] != -1)return dp[i][j];
-        int one = a[i][j] + f(i + 1,j,a);
-        int two = a[i][j] + f(i + 1,j + 1,a);
+   
 
-        return dp[i][j] = min(one,two);
+    int minimumTotal(vector<vector<int>>& a) {
+        int n = a.size();
 
 
+        vector<vector<int>> dp(n,vector<int> (n,1e9));
 
-    }
+        dp[0][0] = a[0][0];
 
-    int minimumTotal(vector<vector<int>>& triangle) {
-        memset(dp,-1,sizeof(dp));
-        return f(0,0,triangle);
+        for(int i = 1;i<n;i++){
+            for(int j = 0;j<=i;j++){
+                int top = 1e9,left = 1e9;
+                top = dp[i - 1][j];
+                if(j - 1 >= 0)left = dp[i - 1][j - 1];
+
+                dp[i][j] = a[i][j] + min(top,left);
+            }
+        }
+        // for(auto &i : dp[n - 2])cout << i << " ";
+        // cout << endl;
+        // for(auto &i : dp[n - 1])cout << i << " ";
+        return *min_element(dp[n - 1].begin(),dp[n - 1].end());
+
+
     }
 };
