74873e42a93293a1d62f70fb2fe0d702481b443b
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-07-20 06:59:08 +0000

    [LeetCode Sync] Runtime - 62 ms (12.39%), Memory - 47.1 MB (48.99%)

--- my-folder/0322-coin-change/solution.cpp
+++ my-folder/0322-coin-change/solution.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int dp[20][10001];
-    int f(int i,int sum,vector<int>&a){
-        if(i >= a.size())return 1e9;
-        if(i == a.size() - 1){
-            if(sum%a[i] == 0)return sum/a[i];
-            return 1e9;
+    int coinChange(vector<int>& v, int amount) {
+        int n = v.size();
+
+        vector<vector<int>> dp(n,vector<int>(amount+ 1,1e9));
+
+        for(int am = 0;am<=amount;am++){
+            if(am % v[0] == 0) dp[0][am] = am/v[0];
         }
-        if(dp[i][sum] != -1)return dp[i][sum];
 
-        int one = 1e9;
-        if(sum - a[i] >= 0)one = 1 + f(i,sum - a[i],a);
-        return dp[i][sum] = min(one,f(i+1,sum,a));
+        for(int i= 1;i<n;i++){
+            for(int j = 0;j<=amount;j++){
+                if(j - v[i] >= 0)dp[i][j] = min(dp[i][j],1 + dp[i][j - v[i]]);
+                dp[i][j] = min(dp[i][j],dp[i - 1][j]);
+            }
+        }
 
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        memset(dp,-1,sizeof(dp));
-       int ans =f(0,amount,coins);
-       if(ans == 1e9)return -1;
-       return ans;
+        return dp[n - 1][amount]  == 1e9 ? -1 : dp[n-1][amount];
+
     }
 };
