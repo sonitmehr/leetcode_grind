c263e0fc4b84c7f848472175f98197afcabb5905
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-07-20 17:55:35 +0000

    [LeetCode Sync] Runtime - 4 ms (11.58%), Memory - 8.5 MB (100.00%)

--- my-folder/0078-subsets/solution.cpp
+++ my-folder/0078-subsets/solution.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
 
     void solve(int i,vector<int>& v,vector<int> &vec,vector<vector<int>> &ans){
-        if(i == v.size()){
+        // if(i == v.size()){
             ans.push_back(vec);
-            return;
+            // return;
+        // }
+
+
+        for(int k = i;k<v.size();k++){
+            vec.push_back(v[k]);
+            solve(k + 1,v,vec,ans);
+            vec.pop_back();
         }
-
-
-        vec.push_back(v[i]);
-        solve(i + 1,v,vec,ans);
-        vec.pop_back();
-        solve(i + 1,v,vec,ans);
 
     }
 
