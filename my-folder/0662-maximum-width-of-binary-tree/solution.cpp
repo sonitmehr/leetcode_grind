4ee5f55148273fbe50bfdb737806e64f510f891f
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-07-20 18:22:58 +0000

    [LeetCode Sync] Runtime - 6 ms (3.07%), Memory - 18.4 MB (99.97%)

--- my-folder/0662-maximum-width-of-binary-tree/solution.cpp
+++ my-folder/0662-maximum-width-of-binary-tree/solution.cpp
@@ -11,30 +11,37 @@
  */
 class Solution {
 public:
-    
     int widthOfBinaryTree(TreeNode* root) {
-        if(root == NULL)return 0;
+        
         queue<pair<TreeNode*,int>> q;
+
         q.push({root,0});
-        int ans = 0;
+        long long ans = 0;
         while(!q.empty()){
-            int n = q.size();
-            long long mini = q.front().second;
-            int first,last;
-            for(int i = 0;i<n;i++){
-                //first = 1e9,last = -1;
+
+            int sz = q.size();
+            long long mini = 1e9,maxi = -1e9;
+            long long minus = q.front().second;
+            // cout << minus << endl;
+            for(int k = 0;k<sz;k++){
                 auto p = q.front();
                 q.pop();
-                long long ind = p.second - mini;
-                auto node = p.first;
-                if(i == 0)first = ind;
-                if(i == n - 1)last =ind;
-                if(node->left != NULL)q.push({node->left,2*ind + 1});
-                if(node -> right != NULL)q.push({node->right,2*ind + 2});
+                
+                TreeNode * node = p.first;
+                long long ind = p.second - minus;
+                
+                mini = min(mini,ind);
+                maxi = max(maxi,ind);
+                
+                if(node->left != NULL){
+                    q.push({node->left,2*ind + 1});
+                }
+                if(node->right != NULL){
+                    q.push({node->right,2*ind + 2});
+                }
 
             }
-            cout << first << " " << last << endl;
-            ans = max(ans,last - first +  1);
+            ans = max(ans,maxi - mini + 1);
         }
         return ans;
     }
