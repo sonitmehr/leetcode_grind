fcc97eb853ee3ca2c6b98b79f16a47c64b147eb1
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-07-21 17:38:11 +0000

    [LeetCode Sync] Runtime - 15 ms (6.22%), Memory - 16.1 MB (100.00%)

--- my-folder/1036-rotting-oranges/solution.cpp
+++ my-folder/1036-rotting-oranges/solution.cpp
@@ -1,50 +1,62 @@
-#define pi pair<pair<int,int>,int>
 class Solution {
 public:
-bool check( int i,int j,int n,int m){
-        if(i < 0 || j < 0 || i >= n || j >= m)return false;
-        return true;
-    }
-    int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size(),m = grid[0].size();
 
-        int cnt = 0;
-        queue<pi> q;
-vector<int> dx = {-1,1,0,0};
-        vector<int> dy = {0,0,1,-1};
-        for(int i= 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(grid[i][j] == 2){
+    vector<int> dx = {1,-1,0,0};
+    vector<int> dy = {0,0,-1,1};
+
+    bool check(int i, int j,int n,int m ){
+        return i >= 0 && j >= 0 && i < n && j < m;
+    }
+    int rotten = 0,ans = 0;
+    void solve(int i, int j,vector<vector<int>> &mat,int n,int m){
+        
+        
+    }
+
+    int orangesRotting(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        int orange = 0;
+
+        
+        queue<pair<pair<int,int>,int>> q;
+        for(int i = 0;i<n;i++){
+            for(int j= 0;j<m;j++){
+                if(mat[i][j] == 2){
                     q.push({{i,j},0});
+                    orange++;
                 }
-                if(grid[i][j] == 1)cnt++;
+                if(mat[i][j] == 1)orange++;
             }
         }
-        int maxTime = 0;
+
         while(!q.empty()){
-            pi top = q.front();
+            auto p = q.front();
             q.pop();
 
-            int currX = top.first.first;
-            int currY = top.first.second;
-            int time = top.second;
-            maxTime = max(maxTime,time);
+            int x = p.first.first;
+            int y = p.first.second;
+            int time = p.second;
+            ans=max(ans,time);
+            cout << x << " " << y << " " << time << endl;
+            rotten ++;
             for(int k = 0;k<=3;k++){
-                int newX = currX + dx[k];
-                int newY = currY + dy[k];
-                if(check(newX,newY,n,m) && grid[newX][newY] == 1){
-                // cout << newX << " " << newY << endl;
-                    q.push({{newX,newY},time+ 1});
-                    grid[newX][newY] = 2;
-                    cnt--;
+                int newX = x + dx[k];
+                int newY = y + dy[k];
+
+                if(check(newX,newY,n,m) && mat[newX][newY] == 1){
+                    mat[newX][newY] = 3;
+                    q.push({{newX,newY},time + 1});
                 }
-            
-            
+
             }
+
+
+
         }
-        return cnt == 0 ? maxTime : -1;
-        
-        
+        cout << rotten << " " << orange << endl;
+        return rotten == orange ? ans : -1;
+
 
     }
 };
