93d21bdf7199c02434ba53136f5e7d9230a93181
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-07-21 18:18:10 +0000

    [LeetCode Sync] Runtime - 9 ms (6.45%), Memory - 14.2 MB (78.16%)

--- my-folder/0130-surrounded-regions/solution.cpp
+++ my-folder/0130-surrounded-regions/solution.cpp
@@ -1,55 +1,65 @@
 class Solution {
 public:
-    int n,m;
-    bool check(int i,int j){
-        if(i < 0 || j <0 || i >=n || j >=m)return false;
-        return true;
+
+    vector<int> dx = {-1,1,0,0};
+    vector<int> dy = {0,0,1,-1};
+
+    bool check(int i,int j,int m,int n){
+        return i>=0 && j >= 0 && i< n  &&j<m;
     }
-vector<int> dx = {-1,1,0,0};
-    vector<int> dy = {0,0,-1,1};
-    void dfs(int i,int j,vector<vector<int>> &vis,char c,vector<vector<char>>  &mat){
-        cout << i<< " " << j << endl;
+
+    void dfs(int i, int j,vector<vector<char>> &mat,vector<vector<int>> &vis,int n,int m){
         vis[i][j] = 1;
 
+        mat[i][j] = 'X';
         for(int k = 0;k<=3;k++){
-                int newX = i + dx[k];
-                int newY = j + dy[k];
+            int x = i + dx[k];
+            int y = j + dy[k];
 
-            if(check(newX,newY) && vis[newX][newY] == 0 && mat[newX][newY] == 'O'){
-                mat[newX][newY] = c;
-                dfs(newX,newY,vis,c,mat);
-                // q.push({{newX,newY},distance + 1});
-            }
+            if(check(x,y,m,n) && mat[x][y] == 'O'){
+               dfs(x,y,mat,vis,n,m);
+                mat[x][y] = 'X';
+                
+            } 
         }
     }
+
     void solve(vector<vector<char>>& mat) {
-        n = mat.size(),m = mat[0].size();
+        int n = mat.size();
+        int m = mat[0].size();
 
         vector<vector<int>> vis(n,vector<int>(m,0));
 
-        for(int i = 0;i<n;i++){
-            if(mat[i][0] == 'O'){
-                dfs(i,0,vis,'O',mat);
-            }
-            if(mat[i][m - 1] == 'O'){
-                dfs(i,m-1,vis,'O',mat);
-            }
-        }
         for(int i = 0;i<m;i++){
             if(mat[0][i] == 'O'){
-                dfs(0,i,vis,'O',mat);
+                dfs(0,i,mat,vis,n,m);
             }
             if(mat[n - 1][i] == 'O'){
-                dfs(n - 1,i,vis,'O',mat);
+                dfs(n - 1,i,mat,vis,n,m);
+
             }
         }
+
+        for(int i = 0;i<n;i++){
+            if(mat[i][0] == 'O'){
+                dfs(i,0,mat,vis,n,m);
+            }
+            if(mat[i][m - 1] == 'O'){
+                dfs(i,m-1,mat,vis,n,m);
+            }
+        }
+
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
-                if(mat[i][j] == 'O' && vis[i][j] == 0){
-                    mat[i][j] ='X';
-                    dfs(i,j,vis,'X',mat);
+                if(vis[i][j] == 1){
+                    mat[i][j] = 'O';
+                }
+                if(vis[i][j] == 0 && mat[i][j] == 'O'){
+                    mat[i][j] = 'X';
                 }
             }
         }
+
+
     }
 };
