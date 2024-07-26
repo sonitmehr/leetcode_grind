dc90f255c8f3f87e05662661113c28998ca993ff
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-07-26 07:10:07 +0000

    [LeetCode Sync] Runtime - 48 ms (3.06%), Memory - 16.7 MB (100.00%)

--- my-folder/0073-set-matrix-zeroes/solution.cpp
+++ my-folder/0073-set-matrix-zeroes/solution.cpp
@@ -1,45 +1,73 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& mat) {
-        int n = mat.size(),m = mat[0].size();
+        int n = mat.size();
+        int m = mat[0].size();
+
         int col0 = 1;
-        if(mat[0][0] == 0)col0 = 0;
-        for(int i= 0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(mat[i][j] == 0 ){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0) {
                     mat[i][0] = 0;
-                    if(j == 0){
+                    if (j == 0) {
                         col0 = 0;
-                    }
-                    else{  
+                    } else {
+
                         mat[0][j] = 0;
                     }
-                    
                 }
             }
         }
+
         cout << col0 << endl;
-        for(int i= 1;i<n;i++){
-            for(int j =1;j<m;j++){
-                
-                if(mat[i][0] == 0 || mat[0][j] == 0){
-                    cout << i << " " << j << endl;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cout << mat[i][j] << " ";
+            }
+            cout << endl;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (mat[i][0] == 0) {
                     mat[i][j] = 0;
                 }
+                if (j != 0 && mat[0][j] == 0) {
+                    mat[i][j] = 0;
+                }
+                // cout << i << " " << j << endl;
+                // cout << endl;
+                // for (int k = 0; k < n; k++) {
+                //     for (int g = 0; g < m; g++) {
+                //         cout << mat[k][g] << " ";
+                //     }
+                //     cout << endl;
+                // }
             }
         }
-        if(mat[0][0] == 0){
-            for(int i = 0;i<m;i++)mat[0][i] = 0;
-        }
-        if(col0== 0){
-            for(int i = 0;i<n;i++)mat[i][0] = 0;
 
+        cout << endl;
+        for (int k = 0; k < n; k++) {
+            for (int g = 0; g < m; g++) {
+                cout << mat[k][g] << " ";
+            }
+            cout << endl;
         }
-    /*
-    [1,2,3,4],
-    [5,0,7,8],
-    [0,10,11,12],
-    [13,14,15,0]
-    */
+        if (mat[0][0] == 0) {
+            for (int i = 0; i < m; i++) {
+                mat[0][i] = 0;
+            }
+        }
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                mat[i][0] = 0;
+            }
+        }
+        cout << endl;
+        for (int k = 0; k < n; k++) {
+            for (int g = 0; g < m; g++) {
+                cout << mat[k][g] << " ";
+            }
+            cout << endl;
+        }
     }
 };
