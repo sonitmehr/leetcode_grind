547efa2f5a6dfd72d38e01073509a639d2826c90
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-07-28 14:57:48 +0000

    [LeetCode Sync] Runtime - 12 ms (10.24%), Memory - 27.6 MB (37.86%)

--- my-folder/0106-construct-binary-tree-from-inorder-and-postorder-traversal/solution.cpp
+++ my-folder/0106-construct-binary-tree-from-inorder-and-postorder-traversal/solution.cpp
@@ -11,32 +11,32 @@
  */
 class Solution {
 public:
-TreeNode* solve(vector<int>& post, vector<int>& in,
-                    unordered_map<int, int>& mp, int postStart, int postEnd,
-                    int inStart, int inEnd) {
 
+     TreeNode* solve(vector<int>& post, vector<int>& in, unordered_map<int,int> &mp, int postStart, int postEnd,
+                    int inStart, int inEnd) {
+        
         if(postStart > postEnd || inStart > inEnd)return NULL;
 
         int newRoot = post[postEnd];
         int ind = mp[newRoot];
-        int right = inEnd -ind;
-        
+        int right = inEnd - ind;
 
         TreeNode* root = new TreeNode(newRoot);
 
-        root->right = solve(post,in,mp,postEnd - right,postEnd - 1,ind + 1,inEnd);
-        root->left = solve(post,in,mp,postStart,postEnd-right-1,inStart,ind - 1);
+        root->right = solve(post,in,mp,postEnd - right,postEnd - 1,ind + 1,inEnd); 
         
-        
+        root->left = solve(post,in,mp,postStart,postEnd - right - 1,inStart,ind - 1);
+
         return root;
+
     }
+
     TreeNode* buildTree(vector<int>& in, vector<int>& post) {
-         int n = post.size();
-        unordered_map<int, int> mp;
-        for (int i = 0; i < n; i++) {
+        unordered_map<int,int> mp;
+        int n = in.size();
+        for(int i = 0;i<n;i++){
             mp[in[i]] = i;
         }
-
-        return solve(post, in, mp,0,n - 1,0,n - 1);
+        return solve(post, in,mp,0,n- 1,0,n-1);
     }
 };
