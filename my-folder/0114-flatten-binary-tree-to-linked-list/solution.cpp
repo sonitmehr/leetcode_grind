f55be23a0e85e9527d9e9388156bbab72df6ecd7
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-07-28 16:31:26 +0000

    [LeetCode Sync] Runtime - 7 ms (0.89%), Memory - 16.9 MB (100.00%)

--- my-folder/0114-flatten-binary-tree-to-linked-list/solution.cpp
+++ my-folder/0114-flatten-binary-tree-to-linked-list/solution.cpp
@@ -11,17 +11,16 @@
  */
 class Solution {
 public:
-
     TreeNode* prev = NULL;
     void flatten(TreeNode* root) {
+        
         if(root == NULL)return;
 
         flatten(root->right);
         flatten(root->left);
-        
         root->left = NULL;
         root->right = prev;
-        
         prev = root;
+
     }
 };
