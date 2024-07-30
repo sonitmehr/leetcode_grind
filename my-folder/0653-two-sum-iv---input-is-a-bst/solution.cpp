186618b47c317a5680048338f7eb2f62c6531980
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-07-30 06:21:29 +0000

    [LeetCode Sync] Runtime - 30 ms (5.68%), Memory - 37.3 MB (44.44%)

--- my-folder/0653-two-sum-iv---input-is-a-bst/solution.cpp
+++ my-folder/0653-two-sum-iv---input-is-a-bst/solution.cpp
@@ -1,66 +1,73 @@
-class right_it{
-private:
-    TreeNode* root;
-    stack<TreeNode*> s;
-public:
-    right_it(TreeNode* cur){
-        root=cur;
-        precompute(root);
-    }
-    
-    void precompute(TreeNode* root){
-        while(root){
-            s.push(root);
-            root=root->right;
-        }
-    }
-    
-    int next(){
-        int res;
-        root=s.top(); s.pop();
-        res=root->val;
-        root=root->left;
-        precompute(root);
-        return res;
-    }
-};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 
-class left_it{
-private:
-    TreeNode* root;
-    stack<TreeNode*> s;
-public:
-    left_it(TreeNode* cur){
-        root=cur;
-        precompute(root);
-    }
-    
-    void precompute(TreeNode* root){
-        while(root){
-            s.push(root);
-            root=root->left;
+class BSTIterator {
+private : 
+    void insert(TreeNode* root, stack<TreeNode*>& st) {
+
+        while (root != NULL) {
+            st.push(root);
+            if (reverse) {
+                root = root->right;
+            } else {
+                root = root->left;
+            }
         }
     }
-    
-    int next(){
-        int res;
-        root=s.top(); s.pop();
-        res=root->val;
-        root=root->right;
-        precompute(root);
-        return res;
+public:
+    stack<TreeNode*> st;
+    bool reverse;
+
+
+    BSTIterator(TreeNode* root, bool reverse) {
+        this->reverse = reverse;
+        insert(root, st);
+    }
+
+    int next() {
+
+        auto top = st.top();
+        int val = top->val;
+        st.pop();
+        if (reverse) {
+            insert(top->left, st);
+        } else {
+            insert(top->right, st);
+        }
+
+        return val;
     }
 };
 
 class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
-        left_it l(root); right_it r(root);
-        int low=l.next(); int high=r.next();
-        while(low<high){
-            if(low+high==k) return true;
-            if(low+high<k) low=l.next();
-            else high=r.next();
+        if(root == NULL)return false;
+        BSTIterator l(root, false);
+        BSTIterator r(root, true);
+
+        int left = l.next();
+        int right = r.next();
+
+        while (left < right) {
+            int sum = left + right;
+            if (k == sum)
+                return true;
+            else if (sum > k) {
+                right = r.next();
+            } else {
+                left = l.next();
+            }
         }
         return false;
     }
