abaa6edb9fe28214885a0d7619ca3f4452ae6565
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-07-30 07:30:18 +0000

    [LeetCode Sync] Runtime - 31 ms (5.19%), Memory - 60.8 MB (56.27%)

--- my-folder/0099-recover-binary-search-tree/solution.cpp
+++ my-folder/0099-recover-binary-search-tree/solution.cpp
@@ -10,22 +10,42 @@
  * };
  */
 class Solution {
+private:
+    TreeNode* prev;
+    TreeNode* first;
+    TreeNode* middle;
+    TreeNode* last;
+
 public:
-    vector<pair<TreeNode*,TreeNode*>> vec;
-    TreeNode* prev = NULL;
-    void inorder(TreeNode* root)
-    {
-        if(!root) return;
+
+    void inorder(TreeNode* root){
+        if(root==NULL)return;
+
         inorder(root->left);
-        if(prev && prev->val>root->val) vec.push_back({prev,root});
+        
+        if(prev != NULL && root->val < prev -> val){
+            if(first == NULL){
+                first = prev;
+                last = root;
+            }
+            else{
+                last = root;
+            }
+        }
         prev = root;
+
         inorder(root->right);
     }
+   
+
     void recoverTree(TreeNode* root) {
+        first = last = middle = NULL;
+        prev = new TreeNode(INT_MIN);
         inorder(root);
-        if(vec.size()==1)
-            swap(vec[0].first->val,vec[0].second->val);
-        if(vec.size()==2)
-            swap(vec[0].first->val,vec[1].second->val);
+
+       swap(first->val,last->val);
+        
+
+
     }
 };
