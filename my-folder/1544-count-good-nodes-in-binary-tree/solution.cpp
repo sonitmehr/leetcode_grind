bbd2112f219efc7b92448a8de3103414b043868d
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-08-01 13:44:09 +0000

    [LeetCode Sync] Runtime - 96 ms (66.51%), Memory - 85 MB (99.84%)

--- my-folder/1544-count-good-nodes-in-binary-tree/solution.cpp
+++ my-folder/1544-count-good-nodes-in-binary-tree/solution.cpp
@@ -11,17 +11,20 @@
  */
 class Solution {
 public:
-    
-    int goodNodes(TreeNode* root,int val=INT_MIN) {
-        int ans=0;
-        if(root == NULL)
-            return 0;
-        if(root->val>=val)
-        {
-            val=root->val;
-            ans++;
-        }
-        ans+=goodNodes(root->left,val)+goodNodes(root->right,val);
-        return ans; 
+
+    void solve(TreeNode* root,int &cnt,int maxi){
+        if(root == NULL)return;
+
+        maxi = max(maxi,root->val);
+        if(root->val >= maxi)cnt++;
+
+        solve(root->left,cnt,maxi);
+        solve(root->right,cnt,maxi);
+    }
+
+    int goodNodes(TreeNode* root) {
+        int cnt =0;
+        solve(root,cnt,-1e4-1);
+        return cnt;
     }
 };
