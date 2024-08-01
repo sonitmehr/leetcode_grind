8906ca39addc165de093c7f3f7d4dc1f967ba692
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-08-01 13:24:29 +0000

    [LeetCode Sync] Runtime - 23 ms (2.30%), Memory - 20.5 MB (99.91%)

--- my-folder/0098-validate-binary-search-tree/solution.cpp
+++ my-folder/0098-validate-binary-search-tree/solution.cpp
@@ -13,18 +13,23 @@ class Solution {
 public:
 
     bool solve(TreeNode* root,long long mini,long long maxi){
-        if(root == NULL)return true;
-
+        if(root==NULL)return true;
+        cout << mini << " " << maxi << endl;
         if(root->val <= mini || root->val >= maxi)return false;
 
         bool left = solve(root->left,mini,root->val);
         bool right = solve(root->right,root->val,maxi);
 
         if(left == false)return false;
-        return right;
+        if(right == false)return false;
+
+        return true;
+
+
     }
 
     bool isValidBST(TreeNode* root) {
+        
         return solve(root,LLONG_MIN,LLONG_MAX);
     }
 };
