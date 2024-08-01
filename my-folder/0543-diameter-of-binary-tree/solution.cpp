b964c18814907f762d453fe6ffc1acd16d2fb93e
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-08-01 13:39:53 +0000

    [LeetCode Sync] Runtime - 10 ms (20.53%), Memory - 22.2 MB (99.94%)

--- my-folder/0543-diameter-of-binary-tree/solution.cpp
+++ my-folder/0543-diameter-of-binary-tree/solution.cpp
@@ -12,22 +12,22 @@
 class Solution {
 public:
 
-    int solve(TreeNode * root,int &diameter){
+
+    int solve(TreeNode* root,int &d){
         if(root == NULL)return 0;
 
-
-        int left=  solve(root->left,diameter);
-        int right=  solve(root->right,diameter);
-
-        diameter = max(diameter,left + right);
+        int left = solve(root->left,d);
+        int right = solve(root->right,d);   
+        d = max(d,right + left);
 
         return 1 + max(left,right);
+
 
     }
 
     int diameterOfBinaryTree(TreeNode* root) {
-        int diameter = 0;
-        solve(root,diameter);
-        return diameter;
+        int d = 0;
+        solve(root,d);
+        return d;   
     }
 };
