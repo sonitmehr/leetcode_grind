3f642a73ad0d973fca6e8fd5562997a1f5cbf07f
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-08-02 16:14:43 +0000

    [LeetCode Sync] Runtime - 1388 ms (30.57%), Memory - 0.0B (100.00%)

--- my-folder/1153-product-sales-analysis-i/solution.sql
+++ my-folder/1153-product-sales-analysis-i/solution.sql
@@ -1,8 +1,9 @@
 # Write your MySQL query statement below
-SELECT p.product_name, s.year, s.price
--- SELECT *
+select 
+p.product_name
+, s.year
+, s.price
 from Sales s
 JOIN Product p
 ON s.product_id = p.product_id
--- GROUP BY s.sale_id
-ORDER BY s.product_id ASC
+GROUP BY s.sale_id
