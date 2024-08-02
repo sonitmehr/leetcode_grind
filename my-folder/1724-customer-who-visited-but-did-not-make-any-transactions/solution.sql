2e41ca4fc02169375c4865b3b7069bb5a5b6a74f
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-08-02 16:09:28 +0000

    [LeetCode Sync] Runtime - 1314 ms (62.49%), Memory - 0.0B (100.00%)

--- my-folder/1724-customer-who-visited-but-did-not-make-any-transactions/solution.sql
+++ my-folder/1724-customer-who-visited-but-did-not-make-any-transactions/solution.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
-
-SELECT v.customer_id,count(v.visit_id) as count_no_trans 
+select 
+v.customer_id
+,COUNT(v.visit_id) as count_no_trans
 from Visits v
 LEFT JOIN Transactions t
 ON v.visit_id = t.visit_id
