a28b5621c06f46901e8546cc06e92666596ed0d5
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-08-03 12:38:01 +0000

    [LeetCode Sync] Runtime - 523 ms (32.04%), Memory - 0.0B (100.00%)

--- my-folder/0197-rising-temperature/solution.sql
+++ my-folder/0197-rising-temperature/solution.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-SELECT w1.id
-FROM Weather w1
+select w2.id
+from Weather w1
 JOIN Weather w2
-ON datediff(w1.recordDate,w2.recordDate) = 1
-AND w1.temperature > w2.temperature
+ON datediff(w2.recordDate,w1.recordDate) = 1
+AND w1.temperature < w2.temperature 
