53c954e21d694ab72a6581197e27d2b39c2ad8fe
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-08-05 18:35:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.9 MB (100.00%)

--- my-folder/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.cpp
+++ my-folder/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.cpp
@@ -1,15 +1,60 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int n = haystack.size(),m = needle.size();
-        int i = 0,j=0;
-        if(needle == "")
-            return 0;
-        int found = haystack.find(needle);
-        if(found!= string::npos){ // If found
-            return found;
+vector<int> computeLPS(string &pat){
+            
+            
+            int i = 1;
+            int len = 0;
+            int n = pat.size();
+            vector<int> lps(n,0);
+            lps[0] = 0;
+            
+            while(i < n){
+                if(pat[i] == pat[len]){
+                    len++;
+                    lps[i] = len;
+                    // cout << "Entered1" << endl;
+                    i++;
+                }
+                else{
+                    // cout << "Entered2" << endl;
+                    if(len > 0){
+                        len = lps[len - 1];
+                    }
+                    else{
+                        lps[i] = 0;
+                        i++;
+                    }
+                }
+            }
+            return lps;
         }
-        return -1;
-        
+    int strStr(string txt, string pat) {
+        vector<int> lps = computeLPS(pat);
+            int M = pat.size(),N = txt.size();
+            int i = 0,j = 0;
+            
+            while((N - i) >= (M - j)){
+                if(pat[j] == txt[i]){
+                    
+                    j++;
+                    i++;
+                }
+                
+                if(j == M){
+                    return (i - j);
+                    j = lps[j - 1];
+                }
+                else if(i < N && pat[j] != txt[i]){
+                    
+                    if(j > 0){
+                        j = lps[j - 1];
+                    }
+                    else{
+                        i ++;
+                    }
+                }
+            }
+            return -1;
     }
 };
