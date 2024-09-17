977354c83008594d5d87e730f165d5a416d215b0
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-09-17 08:30:54 +0000

    [LeetCode Sync] Runtime - 471 ms (43.52%), Memory - 12.1 MB (8.71%)

--- my-folder/0079-word-search/solution.cpp
+++ my-folder/0079-word-search/solution.cpp
@@ -9,12 +9,13 @@ public:
         return true;
     }
 
-    bool solve(int i,int j,int k,vector<vector<char>>& mat,string word){
+    bool solve(int i,int j,int k,vector<vector<char>>& mat,string word,vector<vector<vector<int>>> &dp){
         int n = mat.size();
         int m = mat[0].size();
         if(k == word.size()){
             return true;
         }
+        if(dp[i][j][k] != -1)return dp[i][j][k];
 
         for(int ii = 0;ii<=3;ii++){
             int x = i + dx[ii];           
@@ -24,25 +25,31 @@ public:
                 char t = mat[x][y];
                 // cout << t << endl;
                 mat[x][y] = '*';
-                if(solve(x,y,k +1,mat,word)==true)return true;
+                if(solve(x,y,k +1,mat,word,dp)==true)return dp[x][y][k] = true;
                 mat[x][y] = t;
             }
         }
         return false;
 
     }
-
+/*
+[["A","B","C","E"],
+ ["S","F","E","S"],
+ ["A","D","E","E"]]
+    "ABCESEEEFS"
+*/
     bool exist(vector<vector<char>>& mat, string word) {
             int n = mat.size();
             int m = mat[0].size();
             bool ans = false;
+            vector<vector<vector<int>>> dp(n,vector<vector<int>> (m,vector<int>(word.size(),-1)));
             for(int i = 0;i<n;i++){
                 for(int j = 0;j<m;j++){
                     if(mat[i][j] == word[0]){
                         char t = mat[i][j];
                         mat[i][j] = '*';
                         // cout <<i << " " << j <<endl;
-                        ans |= solve(i,j,1,mat,word);
+                        ans |= solve(i,j,1,mat,word,dp);
                         mat[i][j] = t;
                     }
                 }
