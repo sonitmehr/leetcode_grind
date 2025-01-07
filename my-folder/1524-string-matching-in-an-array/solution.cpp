28ff3d583075382e6c989e7f47e5b1749e2b2677
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2025-01-07 16:04:46 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.1 MB (99.53%)

--- my-folder/1524-string-matching-in-an-array/solution.cpp
+++ my-folder/1524-string-matching-in-an-array/solution.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    
-    static bool compare(string a,string b){
-        if(a.size()>b.size()){
-            return true;
-            
-        }
-        return false;
-            
-    }
-//      ["superhero","^","mass","as","ero"]  
     vector<string> stringMatching(vector<string>& words) {
         int n = words.size();
-        sort(words.begin(),words.end(),compare);
-        vector<string>v;
-        for(int i = 0;i<n-1;i++){
-            if(words[i]!="^"){
-                for(int j =i+1;j<words.size();j++){
-                    if(words[i].find(words[j])!=string::npos){
-                        v.push_back(words[j]);
-                        words[j] = "^";
-                    }
+        vector<string> ans;
 
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i != j && words[j].find(words[i]) != string::npos) {
+                    ans.push_back(words[i]);
+                    break;
                 }
             }
         }
-        return v;
+
+        return ans;
     }
 };
