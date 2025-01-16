bc7d711ed52e23a3220f209a624a136f8f3e2690
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2025-01-16 05:36:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 64.2 MB (72.56%)

--- my-folder/2533-bitwise-xor-of-all-pairings/solution.cpp
+++ my-folder/2533-bitwise-xor-of-all-pairings/solution.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int xorAllNums(vector<int>& v1, vector<int>& v2) {
-        int xor1 = 0,xor2 = 0;
-        for(int i = 0;i<v1.size();i++)xor1 = xor1 ^ v1[i];
-        for(int i = 0;i<v2.size();i++)xor2 = xor2 ^ v2[i];
-        
-        if(v1.size()%2==0){
-            
-            xor2 = 0;
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+
+        int xori = 0;
+
+        if(n % 2 == 1){
+            for(auto &i : nums2)xori = xori ^ i;
         }
-        if(v2.size()%2==0)xor1= 0;
-        
-        
-        return xor1 ^ xor2;
+
+        if(m % 2 == 1){
+            for(auto &i : nums1)xori = xori ^ i;
+        }
+
+        return xori;
     }
 };
