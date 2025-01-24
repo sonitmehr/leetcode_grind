69b394ff930e55d19cf8a0dd0a38cb93e4a75e65
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2025-01-24 07:08:23 +0000

    [LeetCode Sync] Runtime - 49 ms (48.37%), Memory - 66.1 MB (50.32%)

--- my-folder/0820-find-eventual-safe-states/solution.cpp
+++ my-folder/0820-find-eventual-safe-states/solution.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
+
+    
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int V = graph.size();
-        vector<int> adj[V];
-        for(int i = 0;i<V;i++){
-            int from = i;
-            for(int &j : graph[i]){
-                int to = j;
-                adj[to].push_back(from);
+        int n = graph.size();
+
+        vector<vector<int>> adj(n);
+        for(int i = 0;i<n;i++){
+            for(auto &node: graph[i]){
+                adj[node].push_back(i);
             }
         }
         vector<int> ans;
-	    vector<int> inDeg(V,0);
-	   
-	    queue<int> q;
-	       
-	   for(int i = 0;i<V;i++){
-	       for(int &node : adj[i]){
-	           inDeg[node] ++;
-	       }
-	   }
-	   
-	   for(int i = 0;i<V;i++){
-	       if(inDeg[i] == 0)q.push(i);
-	   }
-	   
-	   
-	    while(!q.empty()){
-	        int node = q.front();
-	        q.pop();
-	        ans.push_back(node);
-	        for(int &k : adj[node]){
-	            inDeg[k]--;
-	            if(inDeg[k] == 0)q.push(k);
-	            
-	        }
-	        
-	    }
+        vector<int> inDegree(n,0);
+
+        queue<int> q;
+
+        for(int i= 0;i<n;i++){
+            for(auto &node : adj[i]){
+                inDegree[node]++;
+            }
+        }
+        for(int i = 0;i<n;i++){
+            if(inDegree[i] == 0)q.push(i);
+        }
+        while(!q.empty()){
+            auto p = q.front();
+            q.pop();
+            ans.push_back(p);
+            for(auto &node : adj[p]){
+                inDegree[node]--;
+                if(inDegree[node] == 0){
+                    q.push(node);
+                }
+            }
+        }
         sort(ans.begin(),ans.end());
-	    return ans;
+        return ans;
+
     }
 };
+// 6 4 0 1 3 2 5 
+
