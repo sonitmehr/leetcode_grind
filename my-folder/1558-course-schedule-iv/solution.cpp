0fbb840f9edadc0735e4213c68997b44ceac5621
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2025-01-27 09:10:20 +0000

    [LeetCode Sync] Runtime - 54 ms (56.79%), Memory - 74.3 MB (34.56%)

--- my-folder/1558-course-schedule-iv/solution.cpp
+++ my-folder/1558-course-schedule-iv/solution.cpp
@@ -1,41 +1,41 @@
+
 class Solution {
 public:
-    stack<int> st;
-    int vis[101];
-    vector<int> adj[101];
-    void f(int node){
-        vis[node] = 1;
 
-        for(auto i : adj[node]){
-            if(vis[i] == 0){
-                f(i);
+    void topo(int i,int start,vector<int>&vis, unordered_map<int,unordered_set<int>> &mp
+,vector<int> adj[]){
+        vis[i] = 1;
+        for(auto &node : adj[i]){
+            if(vis[node] == 0){
+                mp[start].insert(node);
+                topo(node,start,vis,mp,adj);
             }
         }
-        //st.push(node);
-        
     }
 
-    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& v, vector<vector<int>>& queries) {
-        if(v.size() == 0){
-            vector<bool> anss;
-            for(auto i : queries){
-                anss.push_back(0);
-            }
-            return anss;
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& pre, vector<vector<int>>& queries) {
+        const int n = numCourses;
+        vector<int> adj[101];
+        vector<bool> ans(queries.size(),false);
+        for(auto &i : pre){
+            int a = i[0],b = i[1];
+
+            adj[a].push_back(b);
         }
-        for(auto i : v){
-            adj[i[0]].push_back(i[1]);
+        unordered_map<int,unordered_set<int>> mp;
+
+        for(int i = 0;i<n;i++){
+            vector<int> vis(n,0);
+            topo(i,i,vis,mp,adj);
         }
-        vector<bool> ans;
-        for(auto q : queries){
+        int ind = 0;
+        for(auto &q : queries){
             int a = q[0],b = q[1];
-            memset(vis,0,sizeof(vis));
-            f(a);
-            if(vis[b] == 1)ans.push_back(1);
-            else ans.push_back(0);
+            if((mp[a]).find(b) != mp[a].end()){
+                ans[ind] = true;
+            }
+            ind++;
         }
         return ans;
-        
-
     }
 };
